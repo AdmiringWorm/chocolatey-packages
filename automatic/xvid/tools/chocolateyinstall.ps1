29eebcd82f892e817e53df4cc8999b7552a0a46d
AdmiringWorm <kim.nordmo@gmail.com>
2016-09-03 15:38:27 +0200

    Added Xvid

---
+++ automatic/xvid/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'xvid'
+$url        = ''
+$checksum = ''
+$checksumType = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'xvid*'
+
+  checksum      = $checksum
+  checksumType  = $checksumType
+
+  silentArgs    = "--unattendedmodeui none --AutoUpdater no --mode unattended --decode_divx yes --decode_3ivx yes --decode_other yes"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/xvid/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+cd "$PSScriptRoot"
+
+$releases = "https://www.xvid.com/download/"
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing;
+
+    $re = "Xvid.*\.exe";
+    $url = $download_page.links | ? href -Match $re | select -First 1 -ExpandProperty href;
+
+    $version = $url -split '-' | select -Last 1 -Skip 1;
+
+    $Latest = @{
+      Version = $version
+      URL = $url
+    }
+
+    return $Latest;
+}
+
+update -ChecksumFor 32
