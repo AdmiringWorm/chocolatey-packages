f34291d340743ea6f0643b0fafd278f0fb3a32e5
AdmiringWorm <kim.nordmo@gmail.com>
2016-10-08 15:57:04 +0200

    Added Gobby Package

---
+++ automatic/gobby/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'gobby'
+
+$url32          = ''
+
+$checksum32     = ''
+$checksumType32 = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  softwareName  = "Gobby*"
+
+  checksum      = $checksum32
+  checksumType  = $checksumType32
+  url           = $url32
+
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
