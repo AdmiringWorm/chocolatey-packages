702cc06518153889757c96bfb0ef308c03539c52
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-08 13:43:52 +0100

    Updated to use AU from github tag

--- update_all.ps1
+++ update_all.ps1
@@ -1,11 +1,12 @@
 # AU Packages Template: https://github.com/majkinetor/au-packages-template
 
-param($Name = $null)
+param([string] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\automatic")
 
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
     Timeout    = 100                                        #Connection timeout in seconds
+    UpdateTimeout = 1200
     Threads    = 10                                         #Number of background jobs to use
     Push       = $Env:au_Push -eq 'true'                    #Push to chocolatey
     PluginPath = ''                                         #Path to user plugins
@@ -16,14 +17,23 @@ $Options = [ordered]@{
         Params= @{                                          #Report parameters:
             Github_UserRepo = $Env:github_user_repo          #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage = ''                                #  Markdown, Text: Custom user message to show
+            UserMessage = "[History](#update-history) | **USING AU 2016.11.5**" #  Markdown, Text: Custom user message to show
+            NoIcons     = $false
+            IconSize    = 32
+            Title       = ''
         }
+    }
+
+    History = @{
+      Lines = 30
+      Github_UserRepo = $Env:github_user_repo
+      Path = "$PSScriptRoot\Update-History.md"
     }
 
     Gist = @{
         Id          = $Env:gist_id                          #Your gist id or leave empty for anonymous
         ApiKey      = $Env:github_access_token                   #Your github api key
-        Path        = "$PSScriptRoot\Update-AUPackages.md"  #List of files to add to gist
+        Path        = "$PSScriptRoot\Update-AUPackages.md","$PSScriptRoot\Update-History.md"  #List of files to add to gist
     }
 
     Git = @{
@@ -34,6 +44,16 @@ $Options = [ordered]@{
     RunInfo = @{
         Exclude = 'password', 'apikey'                      #Option keys which contain those words will be removed
         Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+    }
+
+    ForcedPackages = $ForcedPackages -split ' '
+    BeforeEach = {
+      param($PackageName, $Options)
+      $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
+      if (!$p) { return }
+
+      $global:au_Force = $true
+      $global:au_Version = ($p -split ':')[1]
     }
 
     Mail = if ($Env:mail_user) {
@@ -51,8 +71,8 @@ $Options = [ordered]@{
            } else {}
 }
 
-$global:au_Root = "$PSScriptRoot/../automatic"                           #Path to the AU packages
-$info = updateall -Name $Name -Options $Options
+$global:au_Root = $Root
+$global:info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
-if ($info.error_count.total) { throw 'Errors during update' }
+if ($global:info.error_count.total) { throw 'Errors during update' }
