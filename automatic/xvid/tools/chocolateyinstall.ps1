7092aa5ead3bb52bf97fb40bd474645bcb2690a2
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-13 09:59:40 +0100

    (xvid) Updated chocolateyInstall.ps1 to use embedded file

--- automatic/xvid/tools/chocolateyinstall.ps1
+++ automatic/xvid/tools/chocolateyinstall.ps1
@@ -1,23 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-
-$packageName= 'xvid'
-$url        = 'https://assets.xvid.com/assets/Xvid-1.3.4-20150621.exe'
-$checksum = '88263578d2c3e2dc1e02116a1fb5d9238843b9d84aa371a0f5b32f1b2507524e'
-$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = 'xvid'
   fileType      = 'exe'
-  url           = $url
-
   softwareName  = 'xvid*'
-
-  checksum      = $checksum
-  checksumType  = $checksumType
-
+  file          = "$toolsDir\"
   silentArgs    = "--unattendedmodeui none --AutoUpdater no --mode unattended --decode_divx yes --decode_3ivx yes --decode_other yes"
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
