26a84dcd7cb417b78e9b690bc00e7e6c6ccef5b9
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-13 10:34:50 +0100

    Updated [AU xvid]

--- automatic/xvid/update.ps1
+++ automatic/xvid/update.ps1
@@ -12,6 +12,8 @@ function global:au_BeforeUpdate {
 
   $Latest.ChecksumType32 = 'sha256'
   $Latest.Checksum32 = Get-FileHash -Algorithm $Latest.ChecksumType32 -Path $filePath | % Hash
+
+  $Latest
 }
 
 function global:au_SearchReplace {
@@ -29,19 +31,17 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases;
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $re = "Xvid.*\.exe";
     $url = $download_page.links | ? href -Match $re | select -First 1 -ExpandProperty href;
 
     $version = $url -split '-' | select -Last 1 -Skip 1;
 
-    $Latest = @{
+    @{
       Version = $version
       URL32 = $url
     }
-
-    return $Latest;
 }
 
 update -ChecksumFor none
