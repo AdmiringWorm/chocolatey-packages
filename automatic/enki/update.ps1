7f368c79701ed75b1f9f15372dc7a87b45931019
AdmiringWorm <kim.nordmo@gmail.com>
2017-03-31 16:27:16 +0200

    (enki) Removed `-UseBasicParsing`

--- automatic/enki/update.ps1
+++ automatic/enki/update.ps1
@@ -23,7 +23,7 @@ function global:au_SearchReplace {
   }
 }
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases
 
   $re = '\.exe$'
   $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
