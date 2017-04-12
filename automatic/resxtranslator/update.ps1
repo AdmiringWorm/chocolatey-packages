b3bdbf400bff438ce7d93b7eccd40ac65d6dadac
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-13 00:39:30 +0200

    (resxtranslator) Changed to provide v2.1

--- automatic/resxtranslator/update.ps1
+++ automatic/resxtranslator/update.ps1
@@ -2,7 +2,7 @@
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
 $domain   = 'https://github.com'
-$releases = "$domain/HakanL/resxtranslator/releases/tag/v2.0"
+$releases = "$domain/HakanL/resxtranslator/releases/tag/v2.1"
 $licenseUrl = "https://github.com/HakanL/resxtranslator/blob/master/src/Licence.txt"
 
 function global:au_BeforeUpdate {
