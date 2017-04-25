a19ace38871dfefca2aadcef44086e87998f15ba
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-25 19:31:21 +0200

    Added missing scripts

--- scripts/Get-PaddedVersion.ps1
+++ scripts/Get-PaddedVersion.ps1
@@ -1,3 +1,4 @@
+<# AUTHOR: AdmiringWorm; SOURCE: https://github.com/chocolatey/chocolatey-coreteampackages/blob/master/scripts/Get-PaddedVersion.ps1 #>
 <#
 .SYNOPSIS
   Pad the revision number if the passed version is using a 4-part version scheme.
@@ -21,20 +22,20 @@
   The full padded version, or the specified Version if padding was not needed.
 
 .EXAMPLE
-  Get-Padded-Version -Version '24.0.0.195'
+  Get-PaddedVersion -Version '24.0.0.195'
   will output '24.0.0.195000' if the nuspec version is less than the version
 
 .EXAMPLE
-  Get-Padded-Version -Version '24.0.0.195'
+  Get-PaddedVersion -Version '24.0.0.195'
   will output '24.0.0.195001' if the nuspec version is equal to '24.0.0.195'
   or equal to '24.0.0.195000'
 
 .EXAMPLE
-  Get-Padded-Version -Version '24.0.1.2' -OnlyBelowVersion '24.0.1'
+  Get-PaddedVersion -Version '24.0.1.2' -OnlyBelowVersion '24.0.1'
   Will output the same version that was given without padded zeroes.
 #>
 
-function Get-Padded-Version() {
+function Get-PaddedVersion() {
   param(
     [Parameter(Mandatory = $true, Position = 0)]
     [string]$Version,
@@ -62,17 +63,20 @@ function Get-Padded-Version() {
   $revision = $versionInfo.Revision
   $paddedRev = [int]($revision.ToString().PadRight($RevisionLength, '0'));
 
+  Write-Debug "Loading nuspec file to get the previous version"
+  $content = gc -Encoding UTF8 (Resolve-Path "*.nuspec")
+  $content | ? { $_ -match "\<version\>([\d\.]+)\<\/version\>" } | Out-Null
+  if ($Matches) { $newVersion = [version]$Matches[1] }
+
   if ($global:au_force -eq $true) {
-    Write-Debug "Loading nuspec file to see if we need to pad the version"
-    $content = gc -Encoding UTF8 (Resolve-Path "*.nuspec");
-    $content | ? { $_ -match "\<version\>([\d\.]+)\<\/version\>" } | Out-Null
-    if ($Matches) {
-      $newVersion = [version]$Matches[1]
+    if ($newVersion) {
       $paddedNewRev = [int]$newVersion.Revision.ToString().PadRight($RevisionLength, '0')
       while ($paddedRev -le $paddedNewRev) {
         $paddedRev++ | Out-Null
       }
     }
+  } elseif($newVersion -and $newVersion -eq $versionInfo) {
+    return $Version
   }
 
   return $versionInfo.ToString() -replace '^([\d]+\.[\d]+\.[\d]+\.)\d+$',"`${1}$paddedRev"
