7bb7aec506debf8fe9c887c31d8c847e3919eeb8
AdmiringWorm <kim.nordmo@gmail.com>
2017-05-13 17:58:52 +0200

    Updated update_all.ps1 script

--- update_all.ps1
+++ update_all.ps1
@@ -5,35 +5,35 @@ param([string] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
-    Timeout    = 100                                        #Connection timeout in seconds
-    UpdateTimeout = 1200
-    Threads    = 10                                         #Number of background jobs to use
-    Push       = $Env:au_Push -eq 'true'                    #Push to chocolatey
-    PluginPath = 'scripts\au_plugins'                       #Path to user plugins
+    PluginPath    = 'scripts\au_plugins'                    #Path to user plugins
+    Timeout       = 100                                     #Connection timeout in seconds
+    UpdateTimeout = 1200                                    #Update timeout in seconds
+    Threads       = 10                                      #Number of background jobs to use
+    Push          = $Env:au_Push -eq 'true'                 #Push to chocolatey
 
     Report = @{
         Type = 'markdown'                                   #Report type: markdown or text
         Path = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
         Params= @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo          #  Markdown: shows user info in upper right corner
+            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
             UserMessage = "[Weekly Test Results](https://gist.github.com/$Env:gist_id_test) | [History](#update-history) | **USING AU Custom**" #  Markdown, Text: Custom user message to show
-            NoIcons     = $false
-            IconSize    = 32
-            Title       = ''
+            NoIcons     = $false                            #  Markdown: don't show icon
+            IconSize    = 32                                #  Markdown: icon size
+            Title       = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
         }
     }
 
     History = @{
-      Lines = 30
-      Github_UserRepo = $Env:github_user_repo
-      Path = "$PSScriptRoot\Update-History.md"
+        Lines = 90                                          #Number of lines to show
+        Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
+        Path = "$PSScriptRoot\Update-History.md"            #Path where to save history
     }
 
     Gist = @{
-        Id          = $Env:gist_id                          #Your gist id or leave empty for anonymous
-        ApiKey      = $Env:github_api_key                   #Your github api key
-        Path        = "$PSScriptRoot\Update-AUPackages.md","$PSScriptRoot\Update-History.md"  #List of files to add to gist
+        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
     }
 
     Git = @{
@@ -50,35 +50,37 @@ $Options = [ordered]@{
         Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
     }
 
-    ForcedPackages = $ForcedPackages -split ' '
-	UpdateIconScript = "$PSScriptRoot\setup\Update-IconUrl.ps1"
-    BeforeEach = {
-      param($PackageName, $Options)
-	  . $Options.UpdateIconScript $PackageName.ToLowerInvariant() -Quiet
-      $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
-      if (!$p) { return }
-
-      $global:au_Force = $true
-      $global:au_Version = ($p -split ':')[1]
-    }
-
     Mail = if ($Env:mail_user) {
             @{
-                To          = $Env:mail_user
-                Server      = $Env:mail_server
-                UserName    = $Env:mail_user
-                Password    = $Env:mail_pass
-                Port        = $Env:mail_port
-                EnableSsl   = $Env:mail_enablessl -eq 'true'
+                To         = $Env:mail_user
+                Server     = $Env:mail_server
+                UserName   = $Env:mail_user
+                Password   = $Env:mail_pass
+                Port       = $Env:mail_port
+                EnableSsl  = $Env:mail_enablessl -eq 'true'
                 Attachment = "$PSScriptRoot\update_info.xml"
-                UserMessage = ''
+                UserMessage = 'Update status: http://gep13.me/choco-au'
                 SendAlways  = $false                        #Send notifications every time
              }
            } else {}
+
+    ForcedPackages = $ForcedPackages -split ' '
+    UpdateIconScript = "$PSScriptRoot\scripts\Update-IconUrl.ps1"
+    BeforeEach = {
+        param($PackageName, $Options )
+        . $Options.UpdateIconScript $PackageName.ToLowerInvariant() -Quiet -ThrowErrorOnIconNotFound
+
+        $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
+        if (!$p) { return }
+
+        $global:au_Force   = $true
+        $global:au_Version = ($p -split ':')[1]
+    }
 }
 
-$global:au_Root = $Root
+if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
+$global:au_Root = $Root                                    #Path to the AU packages
 $global:info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
-if ($global:info.error_count.total) { throw 'Errors during update' }
+#if ($global:info.error_count.total) { throw 'Errors during update' }
