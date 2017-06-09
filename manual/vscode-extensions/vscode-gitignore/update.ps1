bbab078ab504d2168a3760e4655f6b1fed112aef
AdmiringWorm <kim.nordmo@gmail.com>
2017-06-09 19:43:51 +0200

    (vscode-gitignore) Fixed image link being included in description [PUSH vscode-gitignore]

--- manual/vscode-extensions/vscode-gitignore/update.ps1
+++ manual/vscode-extensions/vscode-gitignore/update.ps1
@@ -1,5 +1,5 @@
 import-module "$PSScriptRoot\..\..\..\scripts\au_extensions.psm1"
 
-Set-DescriptionFromReadme
+Set-DescriptionFromReadme -SkipFirst 1
 
 . "$PSScriptRoot\..\..\..\scripts\Update-IconUrl.ps1" -Name "vscode-gitignore" -PackagesDirectory "..\manual\vscode-extensions"
