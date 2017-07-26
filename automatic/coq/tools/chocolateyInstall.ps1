36fb25e223df9a1ab06239393e517d918395e436
Chocolatey <chocolatey@realdimensions.net>
2017-07-26 00:09:37 +0000

    AU: 1 updated - Coq [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/664602c122eee275bbd76f24c704a18b8f965084

--- automatic/coq/tools/chocolateyInstall.ps1
+++ automatic/coq/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://coq.inria.fr/distrib/V8.6/files/coq-installer-8.6-i686.exe'
-  url64bit       = 'https://coq.inria.fr/distrib/V8.6/files/coq-installer-8.6-x86_64.exe'
+  url            = 'https://coq.inria.fr/distrib/V8.6.1/files/coq-installer-8.6.1-i686.exe'
+  url64bit       = 'https://coq.inria.fr/distrib/V8.6.1/files/coq-installer-8.6.1-x86_64.exe'
   softwareName   = 'coq*'
-  checksum       = 'e4a4ece9c3d9e138818645e148ea8baf67524f7a59effad35e9d9c9f561db653'
+  checksum       = '0183a773ca32f8709125559d7878557d63b6df1a2616ebaf65a26fefd8ed7c3d'
   checksumType   = 'sha256'
-  checksum64     = 'c03eb2ec0dee3878446b76152e3ed1af475dbf80e72707b50a5c987dd3a7cadf'
+  checksum64     = 'c6da8b192c17b15e858bb9647456a822f5755eca8bca96fafe9d17dd4441d6a0'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
