3c22e7b014710f6de60ca834dfbc427cc70bb095
AdmiringWorm <kim.nordmo@gmail.com>
2017-09-27 21:47:45 +0200

    (privazer.portable) Added portable package
    ref chocolatey/chocolatey-package-requests#174

---
+++ automatic/privazer.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = ''
+  checksum       = ''
+  checksumType   = ''
+  fileFullPath   = "$toolsDir\privazer.exe"
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+Set-Content -Path "$($packageArgs.fileFullPath).gui" -Value ""
---
+++ automatic/privazer.portable/update.ps1
@@ -0,0 +1,18 @@
+﻿. "$PSScriptRoot\..\privazer\update.ps1"
+
+function global:au_AfterUpdate {
+  Set-DescriptionFromReadme -SkipFirst 1
+  Remove-Item "$PSScriptRoot\tools\*.exe"
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)^(\s*url\s*=\s*)'.*'" = "`${1}'$($Latest.URL_P)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum32)'"
+      "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+update -ChecksumFor 32
