1133632fe5b0bca9b3a6ca4d6a63f6a43bfc3a61
Chocolatey <chocolatey@realdimensions.net>
2017-10-04 00:10:11 +0000

    AU: 1 updated - deepgit [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/172e3b80df6cdf7ee055606e78bb9fe57dac8eff

--- automatic/deepgit/tools/chocolateyInstall.ps1
+++ automatic/deepgit/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.syntevo.com/static/smart/download/deepgit/deepgit-win32-setup-jre-2_0_3.zip'
+  url            = 'https://www.syntevo.com/static/smart/download/deepgit/deepgit-win32-setup-jre-3.zip'
   softwareName   = 'DeepGit'
-  checksum       = 'a06bbc9f329ee6dfaf39b652623471e07a6b02a9cdea7fab06e1eeb7813017caa1dac13ddd2f6f1e56c3dbf4a4e5f56989bc29f64ec2563bb6c1422936028ab0'
+  checksum       = 'e1d1f7dd2d052b33d0e1548b3e4d12e54168faff0a9fcd862f05c4a8abcf0c624a85b707f016e29effd2ce8abdb1430ddc2077ebb153be8ba9b705ce1a7b5250'
   checksumType   = 'sha512'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
