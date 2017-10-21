b8c910163f61de186b22662d6ae8c069e7402587
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-21 18:03:47 +0200

    (deepgit) Fixed the quoting of silent args [AU deepgit]

--- automatic/deepgit/tools/chocolateyInstall.ps1
+++ automatic/deepgit/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   softwareName   = 'DeepGit'
   checksum       = 'e1d1f7dd2d052b33d0e1548b3e4d12e54168faff0a9fcd862f05c4a8abcf0c624a85b707f016e29effd2ce8abdb1430ddc2077ebb153be8ba9b705ce1a7b5250'
   checksumType   = 'sha512'
-  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`"'
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
   destination    = Get-PackageCacheLocation
 }
