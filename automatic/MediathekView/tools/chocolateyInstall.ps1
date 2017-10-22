4d14db348f01ed2cdadea4350ce6ffd84236bdcb
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-22 15:26:13 +0200

    (mediathekview) Initial Migration

---
+++ automatic/MediathekView/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'MediathekView'
+$url = 'https://sourceforge.net/projects/zdfmediathk/files/Mediathek/Mediathek%2012/MediathekView_12.zip/download'
+$validExitCodes = @(0)
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+try {
+
+  $target = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$($packageName)__Start.exe"
+  Install-ChocolateyDesktopLink $target
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
