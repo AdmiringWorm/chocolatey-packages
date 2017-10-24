94c6284d936accc10eb435377bc2eab9214bc11f
Chocolatey <chocolatey@realdimensions.net>
2017-10-24 16:37:49 +0000

    AU: 1 updated - authy-desktop [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/5e7a6ea511e960644d28c5e5924bf370a8e6b215

--- automatic/authy-desktop/tools/chocolateyinstall.ps1
+++ automatic/authy-desktop/tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/authy-electron-repository-production/stable/1.0.13/win32/x32/authy-installer.exe'
-  url64bit       = 'https://s3.amazonaws.com/authy-electron-repository-production/stable/1.0.13/win32/x64/authy-installer.exe'
+  url            = 'https://s3.amazonaws.com/authy-electron-repository-production/authy/stable/1.1.0/win32/x32/authy-installer.exe'
+  url64bit       = 'https://s3.amazonaws.com/authy-electron-repository-production/authy/stable/1.1.0/win32/x64/authy-installer.exe'
   softwareName   = 'Authy Desktop*'
-  checksum       = 'b4e98f21f998758a57a08027e2f171c2285053b882f1e9d90da03a768eeb6cfa'
-  checksumType   = ''
-  checksum64     = 'db1f0d2f3630201638cb7c1f6616229d9f033c4dd690fa17d06019608d1f065e'
-  checksumType64 = ''
+  checksum       = 'ad4f38a40a122cf3e42adf6c4ec3623f05eddeecfa637bd5111164ba9e2cfd578a12fda772909e49761b0145df9d9fadd6b9db9d8718f4d085daa4120959ee08'
+  checksumType   = 'sha512'
+  checksum64     = '818769f046fdec1bc5df92cc734ae73187a6a52626fb3d50136dc79dee75da15923e07e5e6752b4504fa18a5916e6c1cb3ef8cf70bd3600d81ea13e2d9b84ef3'
+  checksumType64 = 'sha512'
   silentArgs     = '-s'
   validExitCodes = @(0)
 }
