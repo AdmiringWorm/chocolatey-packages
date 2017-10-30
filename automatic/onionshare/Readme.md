1097f11fbbb0a847eeee7f1fb4bc75b0cb22af7d
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-10-30 18:43:40 +0100

    (onionshare) New Package

---
+++ automatic/onionshare/Readme.md
@@ -0,0 +1,6 @@
+# [<img src="https://cdn.rawgit.com/AdmiringWorm/chocolatey-packages/b9cad337b9aac277e0e32c7c7de3919409c1a38e/icons/onionshare.png" height="48" width="48" /> OnionShare](https://chocolatey.org/packages/onionshare)
+
+OnionShare lets you securely and anonymously share files of any size. It works by starting a web server, making it accessible as a Tor onion service, and generating an unguessable URL to access and download the files. It doesn't require setting up a server on the internet somewhere or using a third party file-sharing service. You host the file on your own computer and use a Tor onion service to make it temporarily accessible over the internet. The other user just needs to use Tor Browser to download the file from you.
+
+## Notes
+- Uninstall is not completely silen, a dialog pops up asking you to confirm the uninstall.
---
+++ automatic/onionshare/update.ps1
@@ -0,0 +1,40 @@
+﻿Import-Module AU
+
+$releases = 'https://onionshare.org/'
+$softwareName = 'OnionShare'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+    }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = '\.exe$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
+
+  $verRe = '\/'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1 | % { $_.TrimStart('v') }
+  @{
+    URL32 = [uri]$url32
+    Version = [version]$version32
+  }
+}
+
+update -ChecksumFor none
