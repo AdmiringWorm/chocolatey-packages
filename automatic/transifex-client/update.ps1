c2b2b1686c39ce66f4c930de0a002032546444e1
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-02 15:09:02 +0100

    (transifex-client) Updated update.ps1 script

--- automatic/transifex-client/update.ps1
+++ automatic/transifex-client/update.ps1
@@ -1,4 +1,5 @@
 ﻿Import-Module AU
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'https://github.com/transifex/transifex-client/releases/latest'
 
@@ -6,40 +7,39 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"      = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
       "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^[$]32bitExec\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+      "(?i)(^[$]32bitExec\s*=\s*`"[$]toolsPath\\).*"    = "`${1}$($Latest.FileName32)`""
       "(?i)(^\s*[$]64bitExec\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
   }
 }
 
+function global:au_AfterUpdate {
+  Update-Metadata -key 'releaseNotes' -value "https://github.com/transifex-client/releases/tag/$($Latest.RemoteVersion)"
+}
+
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re = 'tx\.py35\.exe$$'
-  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
-
-  $re = 'tx\.py35\.64\.exe$'
-  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
+  $re = 'tx\.py35.*\.exe$'
+  $urls = $download_page.Links | ? href -match $re | select -first 2 -expand href | % { 'https://github.com' + $_ }
 
   $verRe = '\/'
-  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
-  $version64 = $url64 -split "$verRe" | select -last 1 -skip 1
-  if ($version32 -ne $version64) {
-    throw "32bit version do not match the 64bit version"
-  }
+  $version = $urls[0] -split "$verRe" | select -last 1 -skip 1
+
   @{
-    URL32 = [uri]$url32
-    URL64 = [uri]$url64
-    Version = [version]$version32
+    URL32         = [uri]($urls | ? { $_ -notmatch 'x64' })
+    URL64         = [uri]($urls | ? { $_ -match 'x64' })
+    Version       = [version]$version
+    RemoteVersion = [version]$version
   }
 }
 
