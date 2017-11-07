878fb82c1b63295424d300430479afe9f70a4b54
Chocolatey <chocolatey@realdimensions.net>
2017-11-07 19:02:23 +0000

    AU: 1 updated - plane9 [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/c76c7152880e3e3663d98b540c9a21d040563008

--- automatic/plane9/tools/chocolateyInstall.ps1
+++ automatic/plane9/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = ''
-  softwareName   = 'PLANE9*'
-  checksum       = ''
-  checksumType   = ''
+  url            = 'http://s3.plane9.com/releases/Plane9-2.5.1.3.exe'
+  softwareName   = 'Plane9*'
+  checksum       = '6e98b95d4ac555072213e2d94eaf70f9e7d4503bce3e250738b69eba21bb32da9d793cb31b04bdae1bb475c9d711194b739a944a983b9ad60af6f42237228c73'
+  checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
