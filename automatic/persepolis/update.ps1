1b3a14afec593832d03b189d6fce563140238d95
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-07 17:46:04 +0100

    (persepolis) Changed to use github helper

--- automatic/persepolis/update.ps1
+++ automatic/persepolis/update.ps1
@@ -1,45 +1,44 @@
 ﻿Import-Module AU
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$releases = 'https://github.com/persepolisdm/persepolis/releases/latest'
+$repoUser = "persepolisdm"
+$repoName = "persepolis"
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
-      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUri)>"
       "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
       "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
   }
 }
 
+function global:au_AfterUpdate {
+  Update-metadata -key "releaseNotes" -value $Latest.ReleaseNotes
+}
+
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $release = getLatestReleases $repoUser $repoName $false
 
-  $re = '32bit\.exe$'
-  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
+  $url32 = $release.latestStable.Assets | ? { $_ -match '32bit\.exe$' } | select -first 1
+  $url64 = $release.latestStable.Assets | ? { $_ -match '64bit\.exe$' } | select -first 1
 
-  $re = '64bit\.exe$'
-  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
-
-  $verRe = '[-]|\.exe|\.msi|\.zip'
-  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
-  $version64 = $url64 -split "$verRe" | select -last 1 -skip 1
-  if ($version32 -ne $version64) {
-    throw "32bit version do not match the 64bit version"
-  }
   @{
-    URL32 = [uri]$url32
-    [uri]URL64 = $url64
-    Version = [version]$version32
+    Version = $release.latestStable.Version
+    URL32   = $url32
+    URL64   = $url64
+    ReleaseNotes = $release.latestStable.ReleaseUrl
+    ReleaseUri   = $release.latestStable.ReleaseUrl
   }
 }
 
