1223c42b0c799b1e8e538eafc61d5cc38c0f7861
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-07 16:53:27 +0100

    (persepolis) New package

---
+++ automatic/persepolis/Readme.md
@@ -0,0 +1,14 @@
+# [<img src="https://cdn.rawgit.com/AdmiringWorm/chocolatey-packages/af7bdbb132aaba7e1470a6421de7e1cd48fa7b53/icons/persepolis.png" height="48" width="48" /> Persepolis Download Manager](https://chocolatey.org/packages/persepolis)
+
+Persepolis is a Download Manager & a GUI For `aria2`. It's written in Python. Persepolis is a Sample of Free and open source software. It's developed For GNU/Linux Distributions, BSDs, macOS and Microsoft Windows.
+
+- Graphical UI for aria2
+- Multi segment downloading
+- Scheduling downloads
+- Download queuing
+
+## Package Parameters
+- `/UseInf:` - Change the inno setup configuration file to use/save when installing
+
+**EXAMPLE**
+`choco install persepolis --params "'/UseInf:C:\persepolis.inf'"`
---
+++ automatic/persepolis/tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
+  softwareName   = 'Persepolis Download Manager*'
+  silentArgs     = "/SILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  validExitCodes = @(0)
+}
+
+$pp = Get-PackageParameters
+if (!$pp.UseInf) {
+  $pp.UseInf = "$env:TEMP\$env:chocolateyPackageName.Install.inf"
+}
+
+if (Test-Path "$($pp.UseInf)") {
+  Write-Host "Using existing configuration file at '$($pp.UseInf)'"
+  $packageArgs['silentArgs'] = "$($packageArgs['silentArgs']) /LOADINF=`"$($pp.UseInf)`""
+} else {
+  Write-Host "Creating new configuration file at '$($pp.UseInf)'"
+  $packageArgs['silentArgs'] = "$($packageArgs['silentArgs']) /SAVEINF=`"$($pp.UseInf)`""
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
