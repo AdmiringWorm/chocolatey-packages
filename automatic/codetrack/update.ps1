8ebe5697d480e3abdc9c47cbb87354365796edb7
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-19 14:59:34 +0100

    (codetrack) Changed to use streams

--- automatic/codetrack/update.ps1
+++ automatic/codetrack/update.ps1
@@ -1,20 +1,19 @@
-﻿Import-Module AU
-Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+﻿[CmdletBinding()]
+param($IncludeStream, [switch]$Force)
+Import-Module AU
 
 $releases = 'https://www.getcodetrack.com/releases.html'
+$softwareName = 'codetrack*'
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
-function global:au_AfterUpdate {
-  Update-ChangelogVersion -version $Latest.Version
-}
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"      = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(^\s*checksum type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
       "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
@@ -26,14 +25,23 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   $re = 'codetrack.*\.zip$'
-  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { 'https://www.getcodetrack.com/' + $_ }
+  $urls32 = $download_page.Links | ? href -match $re | select -expand href | % { 'https://www.getcodetrack.com/' + $_ }
 
-  $verRe = 'k_|\.zip$'
-  $version32 = ($url32 -split "$verRe" | select -last 1 -skip 1) -replace '_','.'
-  @{
-    URL32 = $url32
-    Version = $version32
+  $streams = @{}
+  $urls32 | % {
+    $verRe = 'k_|\.zip$'
+    $version = ($_ -split "$verRe" | select -last 1 -skip 1) -replace '_', '.'
+    $versionTwoPart = $version -replace '^(\d+\.\d+).*', '$1'
+
+    if (!($streams.ContainsKey($versionTwoPart))) {
+      $streams.Add($versionTwoPart, @{
+          Version = $version.ToString()
+          URL32   = $_
+        })
+    }
   }
+
+  return @{ Streams = $streams }
 }
 
-update -ChecksumFor none
+update -ChecksumFor none -IncludeStream $IncludeStream -Force:$Force
