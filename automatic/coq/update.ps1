7ed73321ca84f28a60554584d815f5aaa7e0cb10
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-22 04:36:09 +0100

    (coq) Updated script formatting

--- automatic/coq/update.ps1
+++ automatic/coq/update.ps1
@@ -7,7 +7,7 @@ $softwareName = 'coq*'
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_AfterUpdate {
-  Update-ChangelogVersion -version $Latest.Version
+  Update-Changelog -useIssueTitle
   $releaseNotes = "
 [Software Changelog]($($Latest.ReleaseNotes))
 [Package Changelog](https://github.com/AdmiringWorm/chocolatey-packages/blob/master/automatic/$($Latest.PackageName.ToLowerInvariant())/Changelog.md)"
@@ -17,17 +17,17 @@ function global:au_AfterUpdate {
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"        = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
       "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"         = "`${1}'$softwareName'"
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
       "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
     ".\tools\chocolateyUninstall.ps1" = @{
@@ -52,11 +52,11 @@ function global:au_GetLatest {
     throw "32bit version do not match the 64bit version"
   }
   @{
-    URL32 = $url32
-    URL64 = $url64
-    Version = $version32
+    URL32        = $url32
+    URL64        = $url64
+    Version      = $version32
     ReleaseNotes = $download_page.links | ? href -match "CHANGES$" | select -first 1 -expand href | % { 'https://github.com' + $_ }
-    PackageName = 'Coq'
+    PackageName  = 'Coq'
   }
 }
 
