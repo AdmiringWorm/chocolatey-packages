e1cb903b1651d87441e5800a83a2a23a6f417ea5
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-22 04:35:21 +0100

    (build) Added automatic import of modules

--- test_all.ps1
+++ test_all.ps1
@@ -6,41 +6,48 @@ if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 $global:au_root = Resolve-Path $Root
 
 if (($Name.Length -gt 0) -and ($Name[0] -match '^random (.+)')) {
-    [array] $lsau = lsau
+  [array] $lsau = lsau
 
-    $group = [int]$Matches[1]
-    $n = (Get-Random -Maximum $group)
-    Write-Host "TESTING GROUP $($n+1) of $group"
+  $group = [int]$Matches[1]
+  $n = (Get-Random -Maximum $group)
+  Write-Host "TESTING GROUP $($n+1) of $group"
 
-    $group_size = [int]($lsau.Count / $group) + 1
-    $Name = $lsau | select -First $group_size -Skip ($group_size * $n) | % { $_.Name }
+  $group_size = [int]($lsau.Count / $group) + 1
+  $Name = $lsau | select -First $group_size -Skip ($group_size * $n) | % { $_.Name }
 
-    Write-Host ($Name -join ' ')
-    Write-Host ('-' * 80)
+  Write-Host ($Name -join ' ')
+  Write-Host ('-' * 80)
 }
 
 $options = [ordered]@{
-    Force       = $true
-    Push        = $false
-    PluginPath  = 'scripts\au_plugins'                    #Path to user plugins
+  Force       = $true
+  Push        = $false
+  PluginPath  = 'scripts\au_plugins'                    #Path to user plugins
 
-    Report      = @{
-        Type   = 'markdown'                                   #Report type: markdown or text
-        Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"      #Path where to save the report
-        Params = @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-            NoAppVeyor      = $false                             #  Markdown: do not show AppVeyor build shield
-            Title           = "Update Force Test - Group ${n}"
-            UserMessage     = "[Update report](https://gist.github.com/$Env:gist_id) | **USING AU CUSTOM VERSION**"       #  Markdown, Text: Custom user message to show
-        }
+  Report      = @{
+    Type   = 'markdown'                                   #Report type: markdown or text
+    Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"      #Path where to save the report
+    Params = @{                                          #Report parameters:
+      Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
+      NoAppVeyor      = $false                             #  Markdown: do not show AppVeyor build shield
+      Title           = "Update Force Test - Group ${n}"
+      UserMessage     = "[Update report](https://gist.github.com/$Env:gist_id) | **USING AU CUSTOM VERSION**"       #  Markdown, Text: Custom user message to show
     }
+  }
 
-    Gist        = @{
-        Id          = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
-        ApiKey      = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path        = "$PSScriptRoot\Update-Force-Test-${n}.md"       #List of files to add to the gist
-        Description = "Update Force Test Report #powershell #chocolatey"
-    }
+  Gist        = @{
+    Id          = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
+    ApiKey      = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+    Path        = "$PSScriptRoot\Update-Force-Test-${n}.md"       #List of files to add to the gist
+    Description = "Update Force Test Report #powershell #chocolatey"
+  }
+
+  ModulePaths = @("$PSScriptRoot\scripts\au_extensions.psm1")
+
+  BeforeEach  = {
+    param($PackageName, $Options )
+    $Options.ModulePaths | % { Import-Module $_ }
+  }
 }
 
 
@@ -49,5 +56,5 @@ $global:info = updateall -Name $Name -Options $Options
 $au_errors = $global:info | ? { $_.Error } | select -ExpandProperty Error
 
 if ($ThrowOnErrors -and $au_errors.Count -gt 0) {
-    throw 'Errors during update'
+  throw 'Errors during update'
 }
--- update_all.ps1
+++ update_all.ps1
@@ -5,105 +5,107 @@ param([string] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
-    WhatIf           = $au_WhatIf                              #Whatif all packages
-    Force            = $false                                  #Force all packages
-    Timeout          = 100                                     #Connection timeout in seconds
-    UpdateTimeout    = 1200                                    #Update timeout in seconds
-    Threads          = 10                                      #Number of background jobs to use
-    Push             = $Env:au_Push -eq 'true'                 #Push to chocolatey
-    PushAll          = $true
-    PluginPath       = 'scripts\au_plugins'                    #Path to user plugins
-    IgnoreOn         = @(                                      #Error message parts to set the package ignore status
-        'Could not create SSL/TLS secure channel'
-        'Could not establish trust relationship'
-        'The operation has timed out'
-        'Internal Server Error'
-        'Service Temporarily Unavailable'
-    )
-    RepeatOn         = @(                                      #Error message parts on which to repeat package updater
-        'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
-        'Could not establish trust relationship'              # -||-
-        'Unable to connect'
-        'The remote name could not be resolved'
-        'Choco pack failed with exit code 1'                  # https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
-        'The operation has timed out'
-        'Internal Server Error'
-        'An exception occurred during a WebClient request'
-    )
-    #RepeatSleep   = 250                                    #How much to sleep between repeats in seconds, by default 0
-    #RepeatCount   = 2                                      #How many times to repeat on errors, by default 1
+  WhatIf           = $au_WhatIf                              #Whatif all packages
+  Force            = $false                                  #Force all packages
+  Timeout          = 100                                     #Connection timeout in seconds
+  UpdateTimeout    = 1200                                    #Update timeout in seconds
+  Threads          = 10                                      #Number of background jobs to use
+  Push             = $Env:au_Push -eq 'true'                 #Push to chocolatey
+  PushAll          = $true
+  PluginPath       = 'scripts\au_plugins'                    #Path to user plugins
+  IgnoreOn         = @(                                      #Error message parts to set the package ignore status
+    'Could not create SSL/TLS secure channel'
+    'Could not establish trust relationship'
+    'The operation has timed out'
+    'Internal Server Error'
+    'Service Temporarily Unavailable'
+  )
+  RepeatOn         = @(                                      #Error message parts on which to repeat package updater
+    'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
+    'Could not establish trust relationship'              # -||-
+    'Unable to connect'
+    'The remote name could not be resolved'
+    'Choco pack failed with exit code 1'                  # https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
+    'The operation has timed out'
+    'Internal Server Error'
+    'An exception occurred during a WebClient request'
+  )
+  #RepeatSleep   = 250                                    #How much to sleep between repeats in seconds, by default 0
+  #RepeatCount   = 2                                      #How many times to repeat on errors, by default 1
 
-    Report           = @{
-        Type   = 'markdown'                                   #Report type: markdown or text
-        Path   = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
-        Params = @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-            NoAppVeyor      = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage     = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/AdmiringWorm/chocolatey-packages/releases) | **TESTING AU STREAMS VERSION**"       #  Markdown, Text: Custom user message to show
-            NoIcons         = $false                            #  Markdown: don't show icon
-            IconSize        = 32                                #  Markdown: icon size
-            Title           = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
-        }
+  Report           = @{
+    Type   = 'markdown'                                   #Report type: markdown or text
+    Path   = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
+    Params = @{                                          #Report parameters:
+      Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
+      NoAppVeyor      = $false                            #  Markdown: do not show AppVeyor build shield
+      UserMessage     = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/AdmiringWorm/chocolatey-packages/releases) | **TESTING AU STREAMS VERSION**"       #  Markdown, Text: Custom user message to show
+      NoIcons         = $false                            #  Markdown: don't show icon
+      IconSize        = 32                                #  Markdown: icon size
+      Title           = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
     }
+  }
 
-    History          = @{
-        Lines           = 90                                          #Number of lines to show
-        Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
-        Path            = "$PSScriptRoot\Update-History.md"            #Path where to save history
+  History          = @{
+    Lines           = 90                                          #Number of lines to show
+    Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
+    Path            = "$PSScriptRoot\Update-History.md"            #Path where to save history
+  }
+
+  Gist             = @{
+    Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+    ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+    Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
+  }
+
+  Git              = @{
+    User     = ''                                       #Git username, leave empty if github api key is used
+    Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+  }
+
+  GitReleases      = @{
+    ApiToken    = $Env:github_api_key
+    ReleaseType = 'date'
+  }
+
+  Gitter           = @{
+    WebHook = $Env:gitter_webhook
+  }
+
+  RunInfo          = @{
+    Exclude = 'password', 'apikey', 'UserName', 'To', 'ApiToken'    #Option keys which contain those words will be removed
+    Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+  }
+
+  Mail             = if ($Env:mail_user) {
+    @{
+      To          = $Env:mail_user
+      Server      = $Env:mail_server
+      UserName    = $Env:mail_user
+      Password    = $Env:mail_pass
+      Port        = $Env:mail_port
+      EnableSsl   = $Env:mail_enablessl -eq 'true'
+      Attachment  = "$PSScriptRoot\update_info.xml"
+      UserMessage = "Update status: Update status: https://gist.github.com/choco-bot/$Env:gist_id"
+      SendAlways  = $false                        #Send notifications every time
     }
+  }
+  else {}
 
-    Gist             = @{
-        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
-    }
+  ForcedPackages   = $ForcedPackages -split ' '
+  UpdateIconScript = "$PSScriptRoot\scripts\Update-IconUrl.ps1"
+  ModulePaths      = @("$PSScriptRoot\scripts\au_extensions.psm1")
+  BeforeEach       = {
+    param($PackageName, $Options )
+    $Options.ModulePaths | % { Import-Module $_ }
+    . $Options.UpdateIconScript $PackageName.ToLowerInvariant() -Quiet -ThrowErrorOnIconNotFound
 
-    Git              = @{
-        User     = ''                                       #Git username, leave empty if github api key is used
-        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-    }
+    $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
+    if (!$p) { return }
 
-    GitReleases      = @{
-        ApiToken    = $Env:github_api_key
-        ReleaseType = 'date'
-    }
-
-    Gitter           = @{
-        WebHook = $Env:gitter_webhook
-    }
-
-    RunInfo          = @{
-        Exclude = 'password', 'apikey', 'UserName', 'To', 'ApiToken'    #Option keys which contain those words will be removed
-        Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
-    }
-
-    Mail             = if ($Env:mail_user) {
-        @{
-            To          = $Env:mail_user
-            Server      = $Env:mail_server
-            UserName    = $Env:mail_user
-            Password    = $Env:mail_pass
-            Port        = $Env:mail_port
-            EnableSsl   = $Env:mail_enablessl -eq 'true'
-            Attachment  = "$PSScriptRoot\update_info.xml"
-            UserMessage = "Update status: Update status: https://gist.github.com/choco-bot/$Env:gist_id"
-            SendAlways  = $false                        #Send notifications every time
-        }
-    }
-    else {}
-
-    ForcedPackages   = $ForcedPackages -split ' '
-    UpdateIconScript = "$PSScriptRoot\scripts\Update-IconUrl.ps1"
-    BeforeEach       = {
-        param($PackageName, $Options )
-        . $Options.UpdateIconScript $PackageName.ToLowerInvariant() -Quiet -ThrowErrorOnIconNotFound
-
-        $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
-        if (!$p) { return }
-
-        $global:au_Force = $true
-        $global:au_Version = ($p -split ':')[1]
-    }
+    $global:au_Force = $true
+    $global:au_Version = ($p -split ':')[1]
+  }
 }
 
 if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
