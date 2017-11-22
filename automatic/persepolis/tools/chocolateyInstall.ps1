70deb79d2f39abf30860f695fbb7e3c7cf449516
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-22 19:35:41 +0100

    (persepolis) Expanded aliases in chocolatey script
    changelog:compatibility

--- automatic/persepolis/tools/chocolateyInstall.ps1
+++ automatic/persepolis/tools/chocolateyInstall.ps1
@@ -28,4 +28,4 @@ else {
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
