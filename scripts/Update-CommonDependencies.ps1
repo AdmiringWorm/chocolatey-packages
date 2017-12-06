5f56577bb2816a17f78a550eeb5db17a2629d1a3
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-12-06 19:48:44 +0100

    (scripts) Updated common dependencies check

--- scripts/Update-CommonDependencies.ps1
+++ scripts/Update-CommonDependencies.ps1
@@ -6,6 +6,7 @@
   'kb2999226'
   'dotnet4.0'
   'vcredist2012'
+  'vcredist2015'
   'vcredist140'
   'dotnet4.6'
 ) | % {
@@ -16,8 +17,7 @@
 # Convert and update other references
 @{
   'chocolatey-uninstall.extension' = 'chocolatey-core.extension'
-  'vcredist2015' = 'vcredist140'
-  'vcredist2017' = 'vcredist140'
+  'vcredist2017'                   = 'vcredist140'
 }.GetEnumerator() | % {
   "Changing dependency from $($_.Key) to $($_.Value) and using latest version..."
   . $PSScriptRoot\Update-Dependency.ps1 -OldDependencyName $_.Key -NewDependencyName $_.Value
