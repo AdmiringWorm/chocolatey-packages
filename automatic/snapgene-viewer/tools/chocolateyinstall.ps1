532d1dd9d1f15e0ade8f8df3063c0ad5a485aa27
Chocolatey <chocolatey@realdimensions.net>
2017-12-06 00:11:05 +0000

    AU: 1 updated - snapgene-viewer [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/191d54be02a8e19f318e57c227b28bda2ffa5ce2

--- automatic/snapgene-viewer/tools/chocolateyinstall.ps1
+++ automatic/snapgene-viewer/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.snapgene.com/products/snapgene_viewer/download.php?&majorRelease=4.1&minorRelease=4.1.1&os=windows'
+  url            = 'https://www.snapgene.com/products/snapgene_viewer/download.php?&majorRelease=4.1&minorRelease=4.1.2&os=windows'
   softwareName   = 'SnapGene Viewer'
-  checksum       = '2ce285fa09f57fefda187cce205d1553a847dc0d2ac557270bad4da94a7683b46758860997c90d8e27816c65a45413d2c37e00fdfe46d9da8c2be9b586607a95'
+  checksum       = '665db57596f69549dcc65bf3ea932400a40ac08c93fe70e7844e8390499f4306b184d7ed2c87a1d3c33e2a3a39d75f80dc2b0302d54dc6fea69c2e80e6b10eff'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
