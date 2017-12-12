ac3fbbd05fd113e9ca1581d7ea029402156416ed
Chocolatey <chocolatey@realdimensions.net>
2017-12-12 00:22:14 +0000

    AU: 2 updated - deepgit uplay [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/b997bb37ede0d4954958fe51441c3c3319eda223

--- automatic/deepgit/tools/chocolateyInstall.ps1
+++ automatic/deepgit/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.syntevo.com/static/smart/download/deepgit/deepgit-win32-setup-jre-3_0_1.zip'
+  url            = 'https://www.syntevo.com/static/smart/download/deepgit/deepgit-win32-setup-jre-3_0_2.zip'
   softwareName   = 'DeepGit'
-  checksum       = 'd0b2e9fb58ee41ce3520290b235c43a4d3c17cb7d74f2d93dea1f4dcc1edca5c65997c46cc6a76c97f71711e3c0b37ee4ec974dfe9b78ec57b46606f1b66be61'
+  checksum       = 'bb0c65072d15388a5f6c55de76b4f3a19d1d3613a03d753af1b521f659d960a54701da1055ad59c0eae1e3991c44fdc861fcc4fd5ef9fc9fb2ac7c490e79ae24'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
