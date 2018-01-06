f459ce7f573045ad2f9f1a563ae1039f3e4265a9
Kim J. Nordmo <kim.nordmo@gmail.com>
2018-01-06 17:17:41 +0100

    (qtcreator) Removed useless powershell script

--- automatic/qtcreator/tools/common.ps1
+++
@@ -1,9 +0,0 @@
-﻿$productName = 'Qt Creator'
-$packageName = "$env:chocolateyPackageName"
-$packageVersionMinor = "4.1"
-$packageVersionPatch = "$env:chocolateyPackageVersion"
-
-$shortcutName = "$productName $packageVersionPatch (Chocolatey).lnk"
-$shortcutDir = [environment]::getfolderpath('Programs')
-$shortcutDir = Join-Path $shortcutDir $productName
-$shortcutFile = Join-Path $shortcutDir $shortcutName
