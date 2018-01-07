e62b5d038dc0831d2d44d4a689717a9d6acd9590
AdmiringWorm <kim.nordmo@gmail.com>
2018-01-07 15:22:24 +0100

    (reshade) Added changelog for package
    ref #6

---
+++ automatic/reshade/Changelog.md
@@ -0,0 +1,7 @@
+# ![ReShade Changelog](https://img.shields.io/badge/ReShade-Package%20Changelog-blue.svg?style=for-the-badge)
+
+## Version 3.0.8 (2017-10-30)
+- Corrected tags
+
+## Version 3.0.8 (2017-10-30)
+- Initial implementation of the reshade chocolatey package
--- automatic/reshade/update.ps1
+++ automatic/reshade/update.ps1
@@ -11,13 +11,15 @@ function global:au_BeforeUpdate {
   $Latest.Checksum32 = Get-FileHash -Algorithm $Latest.ChecksumType32 -Path $output | % Hash
 }
 
+function global:au_AfterUpdate { Update-Changelog -useIssueTitle }
+
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"      = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
       "(?i)(^[$]exe\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
@@ -34,7 +36,7 @@ function global:au_GetLatest {
   $verRe = '[_]|\.exe$'
   $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
   @{
-    URL32 = [uri]$url32
+    URL32   = [uri]$url32
     Version = [version]$version32
   }
 }
