93ed393ce972fa1970901d3476c062cbbbf42d76
AdmiringWorm <kim.nordmo@gmail.com>
2018-01-08 23:14:51 +0100

    (origin) Changed to make use of new etag helper

--- automatic/origin/update.ps1
+++ automatic/origin/update.ps1
@@ -33,30 +33,9 @@ function GetResultInformation([string]$url32) {
 function global:au_GetLatest() {
   $url32 = 'https://download.dm.origin.com/origin/live/OriginSetup.exe'
 
-  if (($global:au_Force -ne $true) -and (Test-Path "$PSScriptRoot\info")) {
-    $items = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -first 1 | % { $_ -split '\|' }
-    $headers = Get-WebHeaders $url32
-    if ($items) {
-      $etag = $items[0]
-      $version = $items[1]
-      if ($headers.ETag -ne $etag) {
-        $result = GetResultInformation $url32
-        "$($headers.ETag)|$($result.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
-      }
-      else {
-        $result = @{ URL32 = $url32; Version = $version }
-      }
-    }
-    else {
-      $result = GetResultInformation $url32
-      "$($headers.ETag)|$($result.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
-    }
-  }
-  else {
-    $headers = Get-WebHeaders $url32
-    $result = GetResultInformation $url32
-    "$($headers.ETag)|$($result.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
-  }
+  $result = Update-OnETagChanged -execUrl $url32 -OnETagChanged {
+    GetResultInformation $url32
+  } -OnUpdated { @{ URL32 = $url32 } }
 
   return $result
 }
