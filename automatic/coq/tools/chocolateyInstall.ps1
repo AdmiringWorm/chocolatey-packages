4ee35e031e5eb9cc296854031731fa720f71eeaf
Chocolatey <chocolatey@realdimensions.net>
2018-02-17 08:11:27 +0000

    AU: 1 updated - Coq [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/6c9b82444eae15ea20b4fd97ccbdaa3bd2dbc5cf

--- automatic/coq/tools/chocolateyInstall.ps1
+++ automatic/coq/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\coq-8.7.1-installer-windows-i686.exe"
-  file64         = "$toolsPath\coq-8.7.1-installer-windows-x86_64.exe"
+  file           = "$toolsPath\coq-8.7.2-installer-windows-i686.exe"
+  file64         = "$toolsPath\coq-8.7.2-installer-windows-x86_64.exe"
   softwareName   = 'coq*'
   silentArgs     = '/S'
   validExitCodes = @(0)
@@ -14,4 +14,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
