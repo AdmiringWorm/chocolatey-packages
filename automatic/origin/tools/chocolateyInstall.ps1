e0b6dc8e627bdc2f64ab2af24644e70c65e8a484
Chocolatey <chocolatey@realdimensions.net>
2018-03-28 00:11:17 +0000

    AU: 1 updated - origin [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/87b38a28c75c798f7e4d697889dbb5af6508bbc3

--- automatic/origin/tools/chocolateyInstall.ps1
+++ automatic/origin/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   url          = 'https://download.dm.origin.com/origin/live/OriginSetup.exe'
   softwareName = 'Origin*' # Probably not needed, but we add registry keys so we keep it
-  checksum     = 'BA86F37F173C28A22B8CC30B8FA4943F322A09F211FEAC8B4FF7F5B4D96CE0FDBEEC112EADBE5A937FA7B5554DEA37DC5CE20BC21B7990CA44D6D176898F4939'
+  checksum     = 'D99A833F0E222D9C00FDA495AECE29CDB94120B4EF1021AB2D7A118008D58118CE702ABB38A5A8FBEEEB3843307F10375F8781B12DDE8503A78BC71BA7CA259E'
   checksumType = 'sha512'
   destination  = Get-PackageCacheLocation
 }
@@ -20,10 +20,10 @@ $packageArgs['file'] = $zipFile
 $packageArgs['destination'] = GetInstallLocation $pp
 
 if ($pp.NoAutoUpdate) {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.14.38647'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.15.44004'
 }
 else {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.14.38647'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.15.44004'
 }
 
 Get-ChocolateyUnzip @packageArgs
