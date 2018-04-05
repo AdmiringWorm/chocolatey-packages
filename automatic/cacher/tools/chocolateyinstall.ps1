01b89efac5135d2023d744ac8907fc1dec2bb351
Chocolatey <chocolatey@realdimensions.net>
2018-04-05 16:11:43 +0000

    AU: 3 updated - cacher tablacus uplay [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/22e5a3c69d0521224a88ec01da1a3ddccf63839e

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.3.11.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.3.13.exe'
   softwareName   = 'Cacher*'
-  checksum       = 'afa4e41c6cef3e28baa3e35ba326187f6e472aa2f6ea47ececff8f58dbcfd57a32275b2827855b3abaff74c1c930e756490953e68561153fd17b4c03ee792cbd'
+  checksum       = 'bb899ad6887aec78ddfd6930a101e2fa2bdd99336dea77b022883118db07445901585ca5d6d8e1996700ab2dd73fcd73f820bebc7d6bbc462aa7f34dfbea45a8'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
