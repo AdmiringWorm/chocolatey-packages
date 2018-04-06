89d264697153fb8b89d2f00639427f4a39ab03d3
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-06 20:07:24 +0200

    (Update-CommonDependencies) Updated script to find all dependencies versions first

--- scripts/Update-CommonDependencies.ps1
+++ scripts/Update-CommonDependencies.ps1
@@ -1,17 +1,31 @@
-
-# Update existing common dependencies
-@(
+$allDeps = @(
   'chocolatey-core.extension'
   'autohotkey.portable'
   'kb2999226'
-  'dotnet4.0'
+  'kb3033929'
+  'kb3035131'
   'vcredist2012'
   'vcredist2015'
   'vcredist140'
+  'dotnet4.0'
+  'dotnet4.5'
+  'dotnet4.5.1'
+  'dotnet4.5.2'
   'dotnet4.6'
-) | % {
+  'dotnet4.6.1'
+  'dotnet4.6.2'
+  'dotnet4.7'
+)# | % {
+ # "Updating version for dependency $_..."
+ # . $PSScriptRoot\Update-Dependency.ps1 $_
+#}
+
+$foundDeps = . choco search $allDeps -r | ConvertFrom-Csv -Delimiter '|' -Header 'id','version'
+
+$allDeps | % {
   "Updating version for dependency $_..."
-  . $PSScriptRoot\Update-Dependency.ps1 $_
+  $dependency = $foundDeps | ? id -eq $_ | select -first 1
+  . $PSScriptRoot\Update-Dependency.ps1 $_ -Version $dependency.version
 }
 
 # Convert and update other references
@@ -20,5 +34,10 @@
   'vcredist2017'                   = 'vcredist140'
 }.GetEnumerator() | % {
   "Changing dependency from $($_.Key) to $($_.Value) and using latest version..."
-  . $PSScriptRoot\Update-Dependency.ps1 -OldDependencyName $_.Key -NewDependencyName $_.Value
+  $dependency = $foundDeps | ? id -eq $_.Value | select -first 1
+  if ($dependency) {
+    . $PSScriptRoot\Update-Dependency.ps1 -OldDependencyName $_.Key -NewDependencyName $_.Value -Version $dependency.version
+  } else {
+    . $PSScriptRoot\Update-Dependency.ps1 -OldDependencyName $_.Key -NewDependencyName $_.Value
+  }
 }
