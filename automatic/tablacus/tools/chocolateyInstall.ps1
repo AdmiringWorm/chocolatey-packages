535b3ba5d51bcf8a1449cfe0e24738f76c485d38
Chocolatey <chocolatey@realdimensions.net>
2018-04-18 16:14:31 +0000

    AU: 2 updated - tablacus uplay [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/dd8b01b51cb117ca1c8564f7cbf24e3f6670edd2

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te180416.zip"
+  file        = "$toolsPath\te180418.zip"
   destination = "$toolsPath"
 }
 
