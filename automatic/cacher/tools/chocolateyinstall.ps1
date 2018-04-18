2a1bd4ac4f5250b808db8abb99c44c911c881dbf
Chocolatey <chocolatey@realdimensions.net>
2018-04-18 00:09:33 +0000

    AU: 1 updated - cacher [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/5c689c324da1018ea8feeae7cb823e6310bb44c4

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.4.2.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.4.3.exe'
   softwareName   = 'Cacher*'
-  checksum       = '90ac85346544ed84d31cf1606fa8ac5b4d296233c827803c7cb79fdbd470fe619fa0c479e7e80144efe53f1533527ac501c9e2f8c4e608430fa70742f121bc77'
+  checksum       = '26d28d53a0e2b62aa2661db0992f0c91cd0551530aac0d3f30fb9f7a6a1d40d2e19020fb4b1893a0ea99fbb1880456e1438af66fbc841e687b3eee2fcd7e1d8b'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
