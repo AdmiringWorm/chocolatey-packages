41939cea3db9a75bdf262b50e0f8063b844ba0a6
Chocolatey <chocolatey@realdimensions.net>
2018-04-20 00:10:13 +0000

    AU: 1 updated - qmmp [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/490574c505f66a0e7894a7d0aed8188104f9c50b

--- automatic/qmmp/Changelog.md
+++ automatic/qmmp/Changelog.md
@@ -1,4 +1,7 @@
 # ![Qmmp Changelog](https://img.shields.io/badge/Qmmp-Package%20Changelog-blue.svg?style=for-the-badge)
 
+## Version: 0.11.1 (2018-04-20)
+- **ENHANCEMENT:** Add package changelogs [#6](https://github.com/AdmiringWorm/chocolatey-packages/issues/6)
+
 ## Version 0.11.0 (2017-11-25)
 - Initial implementation of the qmmp chocolatey package
--- automatic/qmmp/tools/chocolateyInstall.ps1
+++ automatic/qmmp/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\qmmp-0.11.0-win32.exe"
+  file           = "$toolsPath\qmmp-0.11.1-win32.exe"
   softwareName   = 'Qt-based Multimedia Player'
   silentArgs     = '/S'
   validExitCodes = @(0)
@@ -13,4 +13,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
