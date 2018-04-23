69f9019dd2f2e107973b482b357006baf55d6bc9
Chocolatey <chocolatey@realdimensions.net>
2018-04-23 16:14:56 +0000

    AU: 4 updated - r r.project snapgene-viewer tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/9c6a05a945a6c6dbad6536e5f1f3b25d75617f40

--- automatic/snapgene-viewer/tools/chocolateyinstall.ps1
+++ automatic/snapgene-viewer/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.snapgene.com/products/snapgene_viewer/download.php?&majorRelease=4.1&minorRelease=4.1.8&os=windows'
+  url            = 'https://www.snapgene.com/products/snapgene_viewer/download.php?&majorRelease=4.1&minorRelease=4.1.9&os=windows'
   softwareName   = 'SnapGene Viewer'
-  checksum       = '5bef9e7e34244c617d7efd51f01344096338b76608dae787bfec013469a2a90481d31f62b3d577008451f95f8983e453bb0d2136e1d5c3c86b0453a519d47fd2'
+  checksum       = '85a7c054740d3c3ce16e47c9971bc1848d77ff3d3c0dec75a66a7183052c8e83dc49f5ad19e9314c5a339dee1a5ca7dd1290fd5fe659e6a327396b59ac011a49'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te180418.zip"
+  file        = "$toolsPath\te180423.zip"
   destination = "$toolsPath"
 }
 
