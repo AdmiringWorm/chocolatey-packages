4ca342b5b7ac95aacefa2e0d67cd238d70f6bd33
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-26 11:41:27 +0200

    (folder_size) Imported package

---
+++ automatic/folder_size/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = ''
+  fileType      = ''
+  softwareName  = ''
+
+  checksum      = ''
+  checksum64    = ''
+  checksumType  = ''
+  checksumType64= ''
+  url           = ''
+  url64bit      = ''
+
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @()
+}
+
+Install-ChocolateyPackage @packageArgs
