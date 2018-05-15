c3c8e9036c2d599d655817876ef3131c352484b4
Chocolatey <chocolatey@realdimensions.net>
2018-05-15 00:13:55 +0000

    AU: 2 updated - cacher gtkwave [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/aaf88746de9752c84c9c2df7bcdfd1cb96d0b032

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.5.1.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.5.3.exe'
   softwareName   = 'Cacher*'
-  checksum       = '83e691cde99c82afab47e142205bc1604cbdb78fe2c91a7419f1666c649816758d96a1626eb9cda18e766202103c41acbfc1cdac57081a3e8efe6c1b3235ec65'
+  checksum       = '6d8c43edde5bba38fb162ac3f81f35bbeb21c5f45e92d07755c1536b5c5afb2d906e28f20d2b342b4ff63df0843e008f3406a673def084223e20ee09210b5ba7'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
