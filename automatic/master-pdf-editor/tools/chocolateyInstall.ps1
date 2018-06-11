f61a2cd927d30aea41956d1c7939af71a6db63e4
Chocolatey <chocolatey@realdimensions.net>
2018-06-11 16:09:43 +0000

    AU: 3 updated - master-pdf-editor qtcreator uplay [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/357f166644b81cb8e0a820cee8869e75f8cb7608

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = '520FB48F53C4861FFC362F2075989A6346319CDECC8CFBD19AB469B056D595BAA41092D29B05F774631066AB8B6F3A203FE5C400E1532851EB74C25879F35C14'
+  checksum       = '56309E4EF32FD8B624EFE2878B2A020CC9D4858DA703422A1168774984ACF629BE70E2BF639AB22514C06BDE8676A889D248AE5DB4F3893A33345F32E0E53BD2'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
