f81addd0b30a960dbe4fa7e7bd789a4ab441630a
AdmiringWorm <kim.nordmo@gmail.com>
2018-06-14 11:10:30 +0200

    (codemaid) Initial package

---
+++ automatic/codemaid/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$parameters = @{
+  PackageName = $env:ChocolateyPackageName
+  File        = "$toolsPath\"
+}
+
+Install-VisualStudioVsixExtension @parameters
---
+++ automatic/codemaid/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿# We'll blindly uninstall the extension
+
+$parameters = @{
+  PackageName = $env:ChocolateyPackageName
+  VsixId      = '4c82e17d-927e-42d2-8460-b473ac7df316'
+}
+
+Uninstall-VisualStudioVsixExtension @parameters
