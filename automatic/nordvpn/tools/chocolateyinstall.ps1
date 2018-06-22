39e1266362fbe893ab1cbea083dec01fd3611705
Chocolatey <chocolatey@realdimensions.net>
2018-06-22 16:15:44 +0000

    AU: 2 updated - nordvpn tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/f54ab1cd6d448f3f5808a3df0c41b80ab5c36d26

--- automatic/nordvpn/tools/chocolateyinstall.ps1
+++ automatic/nordvpn/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.14.28/NordVPNSetup.exe'
+  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.14.31/NordVPNSetup.exe'
   softwareName   = 'NordVPN*'
-  checksum       = 'BD429E5278D83D75F702F602A31F6FA3713534859ACE0FBE0243C2A435923850C92F1E80023B9FE9385D193E2D2D92B7BC8434B4664EA17C8D6F21BE6277968B'
+  checksum       = '2C8E1EFB02588BD37414A0257255C1814366F6B060DAEDC5D0B741E22558B1AACC8842A8DBF2861DC0EFD4D4C9AB83246F3BEA8C646E275416E72B1D8287009D'
   checksumType   = 'sha512'
   silentArgs     = '/exebasicui /exenoupdates /qb /norestart'
   validExitCodes = @(0)
--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te180613.zip"
+  file        = "$toolsPath\te180621.zip"
   destination = "$toolsPath"
 }
 
