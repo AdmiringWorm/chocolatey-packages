3adc83a0cc1ef85952faaa6cfb37ce5857bb30a8
AdmiringWorm <kim.nordmo@gmail.com>
2018-06-26 18:49:42 +0200

    (game-backup-monitor) New package

---
+++ automatic/gbm/Readme.md
@@ -0,0 +1,21 @@
+# [![Game Backup Monitor](https://img.shields.io/chocolatey/v/game-backup-monitor.svg?label=Game%20Backup%20Monitor&style=for-the-badge)](https://chocolatey.org/packages/game-backup-monitor)
+
+Game Backup Monitor is a simple, but flexible application that detects games as you play them. Once you finish playing a game, a backup of the saved games will be made automatically using 7-Zip compression. GBM can be used on a single computer, or on multiple computers to effectively add cloud support to any game or application.
+
+Game Backup Monitor was designed with gaming in mind, but it can be used to monitor any application on your PC!
+
+Game Backup Monitor is completely free and open source software.
+
+## Features
+- Backup your saves automatically and immediately after playing a game. No scheduling or input required!
+- Backup to a cloud folder and add cloud save support to any game.
+- GBM can detect and backup saves from games running on emulators or interpreters such as DOSBox, ScummVM or Wine.
+- A growing list of pre-configured games for Windows and Linux.
+- Easily setup an unlimited number of games to monitor, each with their own specific settings.
+- Game configurations are designed to be easily portable and shared.
+- GBM can automatically launch other software when a monitored game is detected, such as overlays or custom control schemes.
+- Track the hours that you've played any monitored game, similar to Steam. (Optional)
+- Track individual gaming sessions, know exactly when and how much you've played any monitored game. (Optional)
+
+## Notes
+- Game Backup Monitor is currently only available in English.
---
+++ automatic/gbm/update.ps1
@@ -0,0 +1,53 @@
+﻿Import-Module AU
+
+$releases = 'https://github.com/MikeMaximus/gbm/releases/latest'
+$softwareName = 'Game Backup Monitor*'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_AfterUpdate { Update-Metadata -key "releaseNotes" -value $Latest.ReleaseNotes }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt"        = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
+    }
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"         = "`${1}'$softwareName'"
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = '32\-bit\.Installer\.exe$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
+
+  $re = '64\-bit\.Installer\.exe$'
+  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
+
+  $verRe = '\.v|\.(32|64)'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 2
+  $version64 = $url64 -split "$verRe" | select -last 1 -skip 2
+  if ($version32 -ne $version64) {
+    throw "32bit version do not match the 64bit version"
+  }
+  @{
+    URL32   = $url32
+    URL64   = $url64
+    Version = $version32
+    ReleaseNotes = Get-RedirectedUrl $releases
+  }
+}
+
+update -ChecksumFor none
