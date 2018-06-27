dd0474772b967cc35f650f628a5efa40b089305b
Chocolatey <chocolatey@realdimensions.net>
2018-06-27 18:38:41 +0000

    AU: 1 updated - gbm [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/d7a886f90320d4d4941c815a9d44eff7a5aea0de

--- automatic/gbm/tools/chocolateyinstall.ps1
+++ automatic/gbm/tools/chocolateyinstall.ps1
@@ -5,13 +5,13 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\"
-  file64         = "$toolsPath\"
-  softwareName   = 'game-backup-monitor*'
+  file           = "$toolsPath\GBM.v1.1.2.32-bit.Installer.exe"
+  file64         = "$toolsPath\GBM.v1.1.2.64-bit.Installer.exe"
+  softwareName   = 'Game Backup Monitor*'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
