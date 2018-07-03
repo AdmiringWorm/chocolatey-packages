5aa09217709f3f1f2f02568991b64573261f6bff
AdmiringWorm <kim.nordmo@gmail.com>
2018-07-04 00:01:00 +0200

    (codemaid) Auto generate vsix id

--- automatic/codemaid/update.ps1
+++ automatic/codemaid/update.ps1
@@ -8,19 +8,31 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"        = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
     }
-    ".\tools\chocolateyInstall.ps1" = @{
+    ".\tools\chocolateyInstall.ps1"   = @{
       "(?i)(^\s*File\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+    }
+    '.\tools\chocolateyuninstall.ps1' = @{
+      "(?i)(^\s*VsixId\s*=\s*)'.*'" = "`${1}'$($Latest.VsixId)'"
     }
   }
 }
 
 function global:au_AfterUpdate { Update-Changelog -useIssueTitle }
+
+function GetVsixIdFromManifest([string]$tag) {
+  $url = "https://github.com/codecadwallader/codemaid/raw/$tag/CodeMaid/source.extension.vsixmanifest"
+  $manifest = New-Object xml
+  $manifest.Load($url)
+
+  $id = $manifest.PackageManifest.Metadata.Identity.Id | select -First 1
+  return $id
+}
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
@@ -36,9 +48,10 @@ function global:au_GetLatest {
 
     if (!($streams.ContainsKey($version.ToString(2)))) {
       $streams.Add($version.ToString(2), @{
-        Version = $version.ToString()
-        URL32   = $_
-      })
+          Version = $version.ToString()
+          URL32   = $_
+          VsixId  = GetVsixIdFromManifest -tag ($_ -split '\/' | select -last 1 -skip 1)
+        })
     }
   }
 
