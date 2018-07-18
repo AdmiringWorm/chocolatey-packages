3f92ee3c1ed7beb36ce660caf9e908f93c936f99
Chocolatey <chocolatey@realdimensions.net>
2018-07-18 16:09:15 +0000

    AU: 2 updated - lunacy qtcreator [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/0a15401a17c797efe37b70f2bbf54f4efbcd9302

--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://desk.icons8.com/lunacy/LunacySetup.exe'
   softwareName   = 'Lunacy'
-  checksum       = '24B885D8F88C512022C5D9B4ADA6A6A55F0E46B180A23575318D64F0686E2305529CD69357B878D5518D52B60658937501421E6F8DAD92E3EBBB8CB3DED315B0'
+  checksum       = '5C74E0F300A63FEB032ED468443198E03B3C7F699B40A71118792F6C9CC77047A3F09B03AC118530E21C8482D6203B2207B5F8D0B4DA2CE3AA9485E4A49CB8C1'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
