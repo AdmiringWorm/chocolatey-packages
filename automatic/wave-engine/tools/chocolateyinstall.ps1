05796660d4b2ebbb850509d5933ea9ad66958cdd
Chocolatey <chocolatey@realdimensions.net>
2018-07-31 08:08:51 +0000

    AU: 1 updated - wave-engine [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/9dff5b7aa4ec9c4c2712c522ac741da926ebbf76

--- automatic/wave-engine/tools/chocolateyinstall.ps1
+++ automatic/wave-engine/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageName  = $env:chocolateyPackageName
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
-  url           = 'https://waveenginefiles.blob.core.windows.net/installers/WaveEngineSetup_2_4_1.exe'
-  checksum      = '8aae5abbc4c3489bad00798cadf027b90c072eb13280106a2c4279971a73adf4'
+  url           = 'https://waveenginefiles.blob.core.windows.net/installers/WaveEngineSetup_2_5_0.exe'
+  checksum      = '00fcad5db799b6731db76ea7401f34ccd999b558d6a67dfe48baf06ad437b091'
   checksumType  = 'sha256'
 
   validExitCodes= @(0, 3010, 1641)
