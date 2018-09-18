588119adb0aaba7bef40796e749b93f95dfb801e
Chocolatey <chocolatey@realdimensions.net>
2018-09-18 08:06:59 +0000

    AU: 1 updated - origin [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/bffe4b0a78e187aff7ba0e5819e3f2f242ced341

--- automatic/origin/tools/chocolateyInstall.ps1
+++ automatic/origin/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   url          = 'https://download.dm.origin.com/origin/live/OriginSetup.exe'
   softwareName = 'Origin*' # Probably not needed, but we add registry keys so we keep it
-  checksum     = 'A29B628AEED0BF02AEA6B5A4E049B4CE767A3977E180D3652781D7938F4C7612F10840564EAC442059ABE0ECAA75664F2EE1FFFA8ED57F0D0A364118DE4FFE9F'
+  checksum     = '6DBA9A87A7F02C8430B0D79438F4473FF04D0A724EE9105A1B518343D2330BEF5528FE0A22B89DC622E2B6232F52B03F6D3CF60D8377D78AE6A7A80F7174AEFC'
   checksumType = 'sha512'
   destination  = Get-PackageCacheLocation
 }
@@ -16,7 +16,7 @@ if ($env:ChocolateyForce -ne $true) {
   try {
     $InstalledVersion = Get-UninstallRegistryKey $packageArgs['softwareName'] | Select-Object -First 1 -Expand 'DisplayVersion'
 
-    if ([Version]::Parse($InstalledVersion) -ge [Version]::Parse('10.5.26.8488'))
+    if ([Version]::Parse($InstalledVersion) -ge [Version]::Parse('10.5.27.11381'))
     {
       Write-Host "Skipping installation because version $InstalledVersion is already installed."
       return
@@ -34,10 +34,10 @@ $packageArgs['file'] = $zipFile
 $packageArgs['destination'] = GetInstallLocation $pp
 
 if ($pp.NoAutoUpdate) {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.26.8488'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.27.11381'
 }
 else {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.26.8488'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.27.11381'
 }
 
 Get-ChocolateyUnzip @packageArgs
