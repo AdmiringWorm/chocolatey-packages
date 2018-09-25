f4b8aac0eb40119aca2852f706b2116f620cb784
Chocolatey <chocolatey@realdimensions.net>
2018-09-25 16:12:03 +0000

    AU: 2 updated - cacher tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/46b11b165dd0eb9bf8e434a83557eff2472b49bc

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.5.15.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.6.1.exe'
   softwareName   = 'Cacher*'
-  checksum       = '08359011e5b173702b2e6b9858f12ea272ef8d6ab083f3c1da849ddd00dd786f32dacf73872daecd7da65af76d713405c0a739a7b36232cf3ca6f365d8d6e8a1'
+  checksum       = 'fb893d44d0e936dacb4b3b299556a6add5e417843af30fcc278b92b02d1ef86e514abdbcdb07561d509ff54cbb4d1f66ee6f7be2fb0f33b646749b4ec8ceca9f'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
