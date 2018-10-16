7784d57986191014e707e25e5c4a5ccc74aad169
AdmiringWorm <kim.nordmo@gmail.com>
2018-10-16 18:33:55 +0200

    (icofx) Added new package

---
+++ automatic/icofx/tools/ChocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = ''
+  softwareName   = 'IcoFX*'
+  checksum       = ''
+  checksumType   = ''
+  silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
