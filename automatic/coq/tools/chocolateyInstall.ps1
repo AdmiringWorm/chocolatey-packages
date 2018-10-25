2634f635ee41dc8a94aa9efb93875ed4440b1ddf
Chocolatey <chocolatey@realdimensions.net>
2018-10-25 11:50:59 +0000

    AU: 1 updated - coq [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/7e5aa43062de6a32946febf138ba9e82c9784aff

--- automatic/coq/tools/chocolateyInstall.ps1
+++ automatic/coq/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = ''
-  url64bit       = ''
+  url            = 'https://github.com/coq/coq/releases/download/V8.8.2/coq-8.8.2-installer-windows-i686.exe'
+  url64bit       = 'https://github.com/coq/coq/releases/download/V8.8.2/coq-8.8.2-installer-windows-x86_64.exe'
   softwareName   = 'coq*'
-  checksum       = ''
-  checksumType   = ''
-  checksum64     = ''
-  checksumType64 = ''
+  checksum       = 'ccc220d0f7425dbfde2bc825e9a3d0c51d9fa1aa907a573de0b9b6bfa00236ef'
+  checksumType   = 'sha256'
+  checksum64     = 'f60b842e508546e955bafe9d61d2c997d3848814e3ae79f7e5deed996838165f'
+  checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
