8dfe76b1c508195641a74c2d9a5cc8ca691c6d24
AdmiringWorm <kim.nordmo@gmail.com>
2018-10-25 13:28:50 +0200

    (coq) Changed to remote package due to size limitations

--- automatic/coq/tools/chocolateyInstall.ps1
+++ automatic/coq/tools/chocolateyInstall.ps1
@@ -1,17 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
-
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\coq-8.8.1-installer-windows-i686.exe"
-  file64         = "$toolsPath\coq-8.8.1-installer-windows-x86_64.exe"
+  url            = ''
+  url64bit       = ''
   softwareName   = 'coq*'
+  checksum       = ''
+  checksumType   = ''
+  checksum64     = ''
+  checksumType64 = ''
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
-Install-ChocolateyInstallPackage @packageArgs
-
-Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
+Install-ChocolateyPackage @packageArgs
--- automatic/coq/update.ps1
+++ automatic/coq/update.ps1
@@ -1,10 +1,7 @@
 ﻿Import-Module AU
-Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'https://github.com/coq/coq/releases/latest'
 $softwareName = 'coq*'
-
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_AfterUpdate {
   Update-Changelog -useIssueTitle
@@ -17,18 +14,14 @@ function global:au_AfterUpdate {
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt"        = @{
-      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
-    }
     ".\tools\chocolateyInstall.ps1"   = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'"         = "`${1}'$softwareName'"
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
-      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"   = "`${1}'$softwareName'"
+      "(?i)^(\s*url\s*=\s*)'.*'"            = "`${1}'$($Latest.URL32)'"
+      "(?i)^(\s*url64(bit)?\s*=\s*)'.*'"    = "`${1}'$($Latest.URL64)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'"       = "`${1}'$($Latest.Checksum32)'"
+      "(?i)^(\s*checksumType\s*=\s*)'.*'"   = "`${1}'$($Latest.ChecksumType32)'"
+      "(?i)^(\s*checksum64\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum64)'"
+      "(?i)^(\s*checksumType64\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType64)'"
     }
     ".\tools\chocolateyUninstall.ps1" = @{
       "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
@@ -60,4 +53,4 @@ function global:au_GetLatest {
   }
 }
 
-update -ChecksumFor none
+update
