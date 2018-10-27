8d660ff85161cecd1f680de60b9faec371e6dd75
Chocolatey <chocolatey@realdimensions.net>
2018-10-27 00:07:14 +0000

    AU: 2 updated - cacher lunacy [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/13a7429104c23e743bdbebb3184499312673eeaa

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%201.6.13.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.0.0.exe'
   softwareName   = 'Cacher*'
-  checksum       = '26dab7db5002533d0824778f0f840797bf80698aa0614443d40a8610c27a492d27eb1a75179505709236f833439193073c0f76f18c30da930988b69bc03407e1'
+  checksum       = 'e182fa92ae75118bfc2bbc7ab2d770805d4de6e7859da7ae25f90d9135ee9d39cbf77d59f45d4a7d860d6cec4333ed530bda519a5baa7579797badcd2cd91cd9'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://desk.icons8.com/lunacy/LunacySetup.exe'
   softwareName   = 'Lunacy'
-  checksum       = '1FA2EC4C862238B667D03063D38DA27600F811C8B47BE790920DC583A4AC7A702819A7238DBAE591E458396AFFED3831011F09D650B471044886843F7D3C5734'
+  checksum       = 'ED77614CAC19A75F67791DAE35A936055ED0D5C37B16C1034699CD721DB3FFA87D0DA259BA7C0E0AE7CA77C6F810282F345E68AC08D9231BE7C8044A608EACBD'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
