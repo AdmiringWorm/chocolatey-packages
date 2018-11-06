7799af54f1e74984964974949d8d8387c075230b
Chocolatey <chocolatey@realdimensions.net>
2018-11-06 16:20:27 +0000

    AU: 1 updated - nordvpn [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/97a14cf0080b5b7c8d2507a6d184ffd3ceceff1d

--- automatic/nordvpn/tools/chocolateyinstall.ps1
+++ automatic/nordvpn/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.18.5/NordVPNSetup.exe'
+  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.18.9/NordVPNSetup.exe'
   softwareName   = 'NordVPN*'
-  checksum       = '1894785444E0D656906D9EC7609D3123437EE5284E8B1793061215110703E9F06325D1B65AA87BE61B348D9552CB96C8E235D65DAAE676034471B5469E417C4E'
+  checksum       = '7733A1F5B93DF0590C82671AF2E74FCAE89D3B833033E7DA18D4BA6B6A8C567D1220962F8DBAF3DF15D184206B9B2D08CB52748BA60DEDC65BE61EFEF6525502'
   checksumType   = 'sha512'
   silentArgs     = '/exebasicui /exenoupdates /qb /norestart'
   validExitCodes = @(0)
