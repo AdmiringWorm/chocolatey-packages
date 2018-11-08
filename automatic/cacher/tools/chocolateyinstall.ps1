5679ea14d497f7b3f49dd74049e46a65083829d6
Chocolatey <chocolatey@realdimensions.net>
2018-11-08 16:09:03 +0000

    AU: 3 updated - cacher Qemu tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/3fbb8fde3b21d804137b9f177511814d6cff9164

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.0.7.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.0.9.exe'
   softwareName   = 'Cacher*'
-  checksum       = '8ed7a6a83ea02b1b78f68f750836e6ec96c1ae23f7963f284e6f7bcbb866dd39f89c89062a0f1b23a2d953fbbce756cdef33b9fd2b088e07bca72a5cbc9b0cfd'
+  checksum       = 'f5d6a9afefe161fd20cb81fcc2b3e352894a3c0c3ebd218a3f7717ac5004abd1081ba39c7073b990f04d52d401487cf497f4ef5d94eed21d91ff7d875330b196'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/qemu/tools/chocolateyInstall.ps1
+++ automatic/qemu/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20180815.exe'
-  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20180815.exe'
+  url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20181108.exe'
+  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20181108.exe'
   softwareName   = 'QEMU'
-  checksum       = 'd90770104d552092c25b4e575c6e87a03436faf6feddea23c227027935076aff'
+  checksum       = '0d1ee0ca453058bf4c00be7422d6254479e9b8bef968b7668e8a6de723c9b2c6'
   checksumType   = 'sha256'
-  checksum64     = '4e2ab3b53c89289b1b775f9db05ebbac541a8f7fdc7ec424874f5de83e091097'
+  checksum64     = 'ee1e7853b160bfe7207300e65c9394f53f4537bc05967bb00c997d9da0ff1ac7'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
