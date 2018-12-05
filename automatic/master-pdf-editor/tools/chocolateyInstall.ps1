82a785eaeaaab31ac4f9396dd5494beec85cacb4
Chocolatey <chocolatey@realdimensions.net>
2018-12-05 16:08:08 +0000

    AU: 3 updated - master-pdf-editor tablacus uplay [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/c7121ca0bb7d73fcc29beace1a548a9d2429d88a

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = 'C76F29AC9C0F8062761ACA0DB84BB71481B026789267BF5D0B872A1E79510A7939AD2F549CF95416125307B6FB3DF3F29C802E9D894FF9D43D568A1C82FEADC3'
+  checksum       = '5E197D7311CDA8B02D5D10A72900CFAE2A94FFD2405DCDA2894B3B6B03253EEE06716246569B8016469FD587FD886E16FC066880E03AD747E25B045D19C4C5FA'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
