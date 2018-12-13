2c7c291a64912f3cff3c8963e0c4299ea59d0343
Chocolatey <chocolatey@realdimensions.net>
2018-12-13 09:02:48 +0000

    AU: 1 updated - stretchly [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/d92d263a15d87a4b3608b02d4a31db0ad8d477df

--- automatic/stretchly/tools/chocolateyInstall.ps1
+++ automatic/stretchly/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\"
+  file           = "$toolsPath\stretchly.Setup.0.18.0.exe"
   softwareName   = 'stretchly*'
   silentArgs     = '/S /allusers'
   validExitCodes = @(0)
@@ -13,4 +13,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
