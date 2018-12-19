065679789cf6523b53ffed027b00e11b57d2f52c
Chocolatey <chocolatey@realdimensions.net>
2018-12-19 00:11:36 +0000

    AU: 2 updated - gbm master-pdf-editor [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/6be76fff7772aeabe7dac2682bbfe2f0dfa720ac

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = '5E197D7311CDA8B02D5D10A72900CFAE2A94FFD2405DCDA2894B3B6B03253EEE06716246569B8016469FD587FD886E16FC066880E03AD747E25B045D19C4C5FA'
+  checksum       = '364E5555CCB5B5B752A6CCB8F9B5DA3D35B864CDF9EE8ABFEEFF38D2965B39BB67231DCE448DC83708D6AE2BA986039433DAFE0E22C1DC68D0CB42DEA06034DD'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
