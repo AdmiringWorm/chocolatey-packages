62f610fcd4409739110c85b81ecddd1d673988b2
Chocolatey <chocolatey@realdimensions.net>
2018-12-20 08:08:34 +0000

    AU: 2 updated - nagstamon.install unit-test-boilerplate-generator [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/40ea572b4822245382f52ab3eec859b1db091e39

--- automatic/nagstamon.install/tools/chocolateyInstall.ps1
+++ automatic/nagstamon.install/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\Nagstamon-3.0.2-win32_setup.exe"
-  file64         = "$toolsPath\Nagstamon-3.0.2-win64_setup.exe"
+  file           = "$toolsPath\Nagstamon-3.2-win32_setup.exe"
+  file64         = "$toolsPath\Nagstamon-3.2-win64_setup.exe"
   softwareName   = 'Nagstamon*'
   silentArgs     = "/SILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
@@ -27,4 +27,4 @@ if (Test-Path "$($pp.UseInf)") {
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
