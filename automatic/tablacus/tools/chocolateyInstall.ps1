2e46b2631b23c690249b115843d882856e0a973a
Chocolatey <chocolatey@realdimensions.net>
2018-12-22 16:06:59 +0000

    AU: 2 updated - disposablefixer tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/4fc37438806ab1fa1380193f17d1b22ac3902768

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te181221.zip"
+  file        = "$toolsPath\te181222.zip"
   destination = "$toolsPath"
 }
 
