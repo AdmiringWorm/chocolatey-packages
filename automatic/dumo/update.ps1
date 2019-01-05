121d5ff245195da12e0398dab512182232afa302
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-05 07:35:14 +0100

    (dumo) Fixed update script

--- automatic/dumo/update.ps1
+++ automatic/dumo/update.ps1
@@ -12,9 +12,6 @@ function global:au_SearchReplace {
       "(?i)^(\s*checksum\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum32)'"
       "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
     }
-    ".\tools\chocolateyUninstall.ps1" = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
-    }
   }
 }
 
@@ -26,7 +23,7 @@ function GetResultInformation([string]$url32) {
 
   return @{
     URL32 = $url32
-    Version = Get-Item $dest | % { $_.VersionInfo.ProductVersion }
+    Version = [version](Get-Item $dest | % { $_.VersionInfo.ProductVersion })
     Checksum32 = Get-FileHash $dest -Algorithm SHA512 | % Hash
     ChecksumType32 = 'sha512'
   }
