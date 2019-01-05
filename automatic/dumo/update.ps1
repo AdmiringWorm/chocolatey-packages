010e227fa09dde7316f5bf163554fde4eceaeb7f
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-05 06:41:44 +0100

    (dumo) Migrated package

---
+++ automatic/dumo/update.ps1
@@ -0,0 +1,35 @@
+﻿Import-Module AU
+
+$releases = 'https://www.kcsoftwares.com/?download'
+$softwareName = 'dumo*'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)^(\s*url\s*=\s*)'.*'" = "`${1}'$($Latest.URL32)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum32)'"
+      "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = 'dumo.*\.exe$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
+
+  $verRe = '[-]|\.exe|\.msi|\.zip'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
+
+  @{
+    URL32 = $url32
+    Version = $version32
+  }
+}
+
+update -ChecksumFor 32
