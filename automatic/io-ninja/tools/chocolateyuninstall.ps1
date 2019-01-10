b072fcdcce903a07f43c9e18a26b274e77680bbd
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-10 18:15:17 +0100

    (io-ninja) Updated package code to latest edition

--- automatic/io-ninja/tools/chocolateyuninstall.ps1
+++ automatic/io-ninja/tools/chocolateyuninstall.ps1
@@ -1,39 +1,29 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'IO Ninja'
-$softwareName = 'IO Ninja*'
-$installerType = 'MSI' 
-
-$silentArgs = '/qn /norestart'
-$validExitCodes = @(0, 3010, 1605, 1614, 1641)
-if ($installerType -ne 'MSI') {
-  $validExitCodes = @(0)
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'io-ninja*'
+  fileType      = 'msi'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
 }
 
 $uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+[array]$key = Get-UninstallRegistryKey @packageArgs
 
 if ($key.Count -eq 1) {
-  $key | % { 
-    $file = "$($_.UninstallString)"
+  $key | % {
+    $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+    $packageArgs['file'] = ''
 
-    if ($installerType -eq 'MSI') {
-      $silentArgs = "$($_.PSChildName) $silentArgs"
-
-      $file = ''
-    }
-
-    Uninstall-ChocolateyPackage -PackageName $packageName `
-                                -FileType $installerType `
-                                -SilentArgs "$silentArgs" `
-                                -ValidExitCodes $validExitCodes `
-                                -File "$file"
+    Uninstall-ChocolateyPackage @packageArgs
   }
 } elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
+  Write-Warning "$($packageArgs.packageName) has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
-  Write-Warning "$key.Count matches found!"
+  Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $_.DisplayName"}
+  Write-Warning "Please alert the package maintainer that the following keys were matched:"
+  $key | % { Write-Warning "- $($_.DisplayName)" }
 }
