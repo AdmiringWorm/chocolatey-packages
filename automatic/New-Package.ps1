917e848aa17eb32250ac9b457a4f1e6af97f54b1
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-17 21:47:41 +0100

    (scripts) Updated New-Package script to optionally allow setting id

--- automatic/New-Package.ps1
+++ automatic/New-Package.ps1
@@ -3,7 +3,8 @@ param(
     $Name,
     [ValidateSet('Portable', 'Installer')]
     [string] $Type='Installer',
-    [string]$iconUrl = $null
+    [string]$iconUrl = $null,
+    [string]$id = $null
 )
 # Original Source: <https://github.com/majkinetor/chocolatey/blob/master/New-Package.ps1>
 
@@ -26,14 +27,23 @@ function New-Package{
         [string] $GithubRepository,
 
         # The url to the icon
-        [string]$iconUrl
+        [string]$iconUrl,
+
+        # Optional ID of the package, it will be determined by the Name parameter if none is specified
+        [string]$id
     )
 
     if ($Name -eq $null) { throw "Name can't be empty" }
     if (Test-Path $Name) { throw "Package with that name already exists" }
     if (!(Test-Path _template)) { throw "Template for the packages not found" }
-    $LowerName = $Name.ToLower() -replace ' ','-'
-    cp _template $LowerName -Recurse
+    if (!$id) {
+      $id = $Name.ToLower() -replace ' ','-'
+    } else {
+      $id = $id.ToLower() -replace ' ','-'
+    }
+
+
+    cp _template $id -Recurse
 
     if ($iconUrl) {
       [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor
@@ -41,37 +51,37 @@ function New-Package{
         [System.Net.SecurityProtocolType]::Tls -bor
         [System.Net.SecurityProtocolType]::Ssl3
       $extension = [System.IO.Path]::GetExtension($iconUrl)
-      iwr $iconUrl -OutFile "../icons/${LowerName}${extension}" -UseBasicParsing
+      iwr $iconUrl -OutFile "../icons/${id}${extension}" -UseBasicParsing
     }
 
-    Move-Item "$LowerName/template.nuspec" "$LowerName/$LowerName.nuspec" -Force;
-    ../scripts/Update-IconUrl.ps1 -Name "$LowerName" -GithubRepository $GithubRepository -Optimize;
-    $nuspec = gc -Encoding utf8 "$LowerName/$LowerName.nuspec";
+    Move-Item "$id/template.nuspec" "$id/$id.nuspec" -Force;
+    ../scripts/Update-IconUrl.ps1 -Name "$id" -GithubRepository $GithubRepository -Optimize;
+    $nuspec = gc -Encoding utf8 "$id/$id.nuspec";
 
     Write-Verbose 'Fixing nuspec'
-    $nuspec = $nuspec -replace '<id>.+',               "<id>$LowerName</id>"
+    $nuspec = $nuspec -replace '<id>.+',               "<id>$id</id>"
     $nuspec = $nuspec -replace '<title>.+',            "<title>$Name</title>"
-    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubRepository/tree/master/automatic/$LowerName</packageSourceUrl>"
-    $nuspec | Out-File -Encoding UTF8 "$LowerName\$LowerName.nuspec"
+    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubRepository/tree/master/automatic/$id</packageSourceUrl>"
+    $nuspec | Out-File -Encoding UTF8 "$id\$id.nuspec"
 
     switch ($Type)
     {
         'Installer' {
             Write-Verbose 'Using installer template'
-            rm "$LowerName\tools\chocolateyInstallZip.ps1"
-            mv "$LowerName\tools\chocolateyInstallExe.ps1" "$LowerName\tools\chocolateyInstall.ps1"
+            rm "$id\tools\chocolateyInstallZip.ps1"
+            mv "$id\tools\chocolateyInstallExe.ps1" "$id\tools\chocolateyInstall.ps1"
         }
         'Portable' {
             Write-Verbose 'Using portable template'
-            rm "$LowerName\tools\chocolateyInstallExe.ps1"
-            mv "$LowerName\tools\chocolateyInstallZip.ps1" "$LowerName\tools\chocolateyInstall.ps1"
+            rm "$id\tools\chocolateyInstallExe.ps1"
+            mv "$id\tools\chocolateyInstallZip.ps1" "$id\tools\chocolateyInstall.ps1"
         }
     }
 
     Write-Verbose 'Fixing chocolateyInstall.ps1'
-    $installer = gc "$LowerName\tools\chocolateyInstall.ps1"
+    $installer = gc "$id\tools\chocolateyInstall.ps1"
     $installer = $installer -replace "(?i)(^\s*softwareName\s*=\s*)('.*')", "`$1'$($Name)'"
-    $installer -replace "(?i)(^\s*packageName\s*=\s*)('.*')", "`$1'$($LowerName)'" | sc "$LowerName\tools\chocolateyInstall.ps1"
+    $installer -replace "(?i)(^\s*packageName\s*=\s*)('.*')", "`$1'$($id)'" | sc "$id\tools\chocolateyInstall.ps1"
 }
 
-New-Package $Name $Type -GithubRepository AdmiringWorm/chocolatey-packages -Verbose -iconUrl $iconUrl
+New-Package $Name $Type -GithubRepository AdmiringWorm/chocolatey-packages -Verbose -iconUrl $iconUrl -id $id
