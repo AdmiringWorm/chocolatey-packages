0b04fe6abd850ff14a3256516cd5167f65971c3c
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-22 19:21:27 +0100

    (codemaid) Added unit tests

---
+++ automatic/codemaid/package.Tests.ps1
@@ -0,0 +1,12 @@
+. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$toolsPath = if (Test-Path Env:\ChocolateyToolsLocation) { $env:ChocolateyToolsLocation } else { "$env:SystemDrive\tools" }
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+    -packageName "$packageName" `
+    -packagePath "$PSScriptRoot" `
+    -expectedEmbeddedMatch "^CodeMaid\.v[\d\.]+\.vsix$" `
+    -licenseShouldMatch "GNU LESSER GENERAL PUBLIC LICENSE" `
+    -metaPackage:$(!(Test-Path Env:\APPVEYOR))
