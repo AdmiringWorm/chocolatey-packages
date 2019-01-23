a9a609ff9f169bfbba862731bb7bb068280840fa
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-23 19:54:07 +0100

    (gbm) Changed to validate license file during update

--- automatic/gbm/update.ps1
+++ automatic/gbm/update.ps1
@@ -6,7 +6,19 @@ $repoInfo = @{
   repoName = 'gbm'
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_BeforeUpdate($Package) {
+  # Download the latest License, and verify it is still an GNU GENERAL PUBLIC LICENSE
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { rm $licenseFile }
+  iwr -UseBasicParsing -Uri ($Package.NuspecXml.package.metadata.licenseUrl -replace 'blob','raw') -OutFile $licenseFile
+
+  $isCorrectLicense = Get-Content $licenseFile -Encoding UTF8 | ? { $_ -match 'GNU GENERAL PUBLIC LICENSE' } | select -first 1
+
+  if (!$isCorrectLicense) { throw "License has changed, please update..." }
+
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
 function global:au_AfterUpdate {
   Update-Metadata -key "releaseNotes" -value @"
 ### [Package Changelog](https://github.com/AdmiringWorm/chocolatey-packages/blob/master/automatic/gbm/Changelog.md)
