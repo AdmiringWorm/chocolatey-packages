cc9430983ed3cf3aa1cc33bf09669eaa5a437e04
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-23 09:05:38 +0100

    (dilay) Added ability to download and verify license

--- automatic/dilay/update.ps1
+++ automatic/dilay/update.ps1
@@ -3,7 +3,18 @@
 $releases = 'http://abau.org/dilay/download.html'
 
 function global:au_AfterUpdate { Update-Changelog -useIssueTitle }
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_BeforeUpdate($Package) {
+  # Download the latest License, and verify it is still an GNU license
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { rm $licenseFile }
+  iwr -UseBasicParsing -Uri ($Package.NuspecXml.package.metadata.licenseUrl -replace 'blob','raw') -OutFile $licenseFile
+
+  $isApacheLicense = Get-Content $licenseFile -Encoding UTF8 | ? { $_ -match 'GNU GENERAL PUBLIC LICENSE' }
+
+  if (!$isApacheLicense) { throw "License has changed, please update..." }
+
+  Get-RemoteFiles -Purge -NoSuffix
+}
 
 function global:au_SearchReplace {
   @{
