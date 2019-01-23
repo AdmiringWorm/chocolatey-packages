976abb38e532d48c017f3fbb3cf10d5aef1b43d9
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-23 22:20:19 +0100

    (ghostdoc-enterprise) Added unit tests

---
+++ automatic/ghostdoc-enterprise/package.Tests.ps1
@@ -0,0 +1,7 @@
+. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot"
