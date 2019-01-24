c2e526cf73210bb0be5ee845f166d2d4dc70749d
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-24 20:56:37 +0100

    (boxcli) Corrected stream used in package tests

--- automatic/boxcli/package.Tests.ps1
+++ automatic/boxcli/package.Tests.ps1
@@ -3,7 +3,7 @@
 Run-PesterTests `
   -packageName "boxcli" `
   -packagePath "$PSScriptRoot" `
-  -streams "1.0","1.1","1.2" `
+  -streams "1.0","1.1","latest" `
   -expectedEmbeddedMatch "^BoxCLI-win10-x64\.msi$" `
   -customDirectoryArgument "INSTALLFOLDER=" `
   -licenseShouldMatch "Apache License" `
