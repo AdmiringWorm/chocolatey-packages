ed4b021a12db53ec764c0828e8b4b3e640509d57
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-25 19:59:24 +0100

    (unit-tests) Fixed typo in test scripts

--- automatic/disposablefixer/package.Tests.ps1
+++ automatic/disposablefixer/package.Tests.ps1
@@ -3,7 +3,7 @@
 $toolsPath = if (Test-Path Env:\ChocolateyToolsLocation) { $env:ChocolateyToolsLocation } else { "$env:SystemDrive\tools" }
 
 $packageName = Split-Path -Leaf $PSScriptRoot
-$runChocoUnitTests = Test-Path Env:\APPVEYOR_BUILD_WORKER_IMAGE -and ($env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2017') # DisposableFixer only supports Visual Studio 2017 right now.
+$runChocoUnitTests = (Test-Path Env:\APPVEYOR_BUILD_WORKER_IMAGE) -and ($env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2017') # DisposableFixer only supports Visual Studio 2017 right now.
 
 Run-PesterTests `
     -packageName "$packageName" `
--- automatic/wave-engine/package.Tests.ps1
+++ automatic/wave-engine/package.Tests.ps1
@@ -2,7 +2,7 @@
 
 $packageName = Split-Path -Leaf $PSScriptRoot
 
-$runChocoUnitTests = Test-Path Env:\APPVEYOR_BUILD_WORKER_IMAGE -and !($env:APPVEYOR_BUILD_WORKER_IMAGE -ne 'Visual Studio 2017') # Somehow, the VS 2017 image will not work with this package, due to prompting installation of GTKSharp (even the dependency does not work as it should)
+$runChocoUnitTests = (Test-Path Env:\APPVEYOR_BUILD_WORKER_IMAGE) -and !($env:APPVEYOR_BUILD_WORKER_IMAGE -ne 'Visual Studio 2017') # Somehow, the VS 2017 image will not work with this package, due to prompting installation of GTKSharp (even the dependency does not work as it should)
 
 Run-PesterTests `
   -packageName "$packageName" `
