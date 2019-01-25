903a48b40f5b12cada2f7925e4b022fc038318f6
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-25 19:41:38 +0100

    (wave-engine) Changed to not run unit tests on appveyor 2017 image

--- automatic/wave-engine/package.Tests.ps1
+++ automatic/wave-engine/package.Tests.ps1
@@ -2,8 +2,10 @@
 
 $packageName = Split-Path -Leaf $PSScriptRoot
 
+$runChocoUnitTests = Test-Path Env:\APPVEYOR_BUILD_WORKER_IMAGE -and !($env:APPVEYOR_BUILD_WORKER_IMAGE -ne 'Visual Studio 2017') # Somehow, the VS 2017 image will not work with this package, due to prompting installation of GTKSharp (even the dependency does not work as it should)
+
 Run-PesterTests `
   -packageName "$packageName" `
   -packagePath "$PSScriptRoot" `
-  -expectedDefaultDirectory "${env:ChocolateyInstall}\lib\teeworlds\tools\teeworlds-*-win32" `
-  -testChoco:$(Test-Path Env:\APPVEYOR)
+  -expectedDefaultDirectory "${env:ProgramFiles(x86)}\Wave Engine" `
+  -testChoco:$runChocoUnitTests
