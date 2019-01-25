c1c0e9a737042ed8f67a059ffa306a96c3671ea6
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-25 19:40:17 +0100

    (disposablefixer) Changed to only run chocolatey unit tests on VS 2017 appveyor image

--- automatic/disposablefixer/package.Tests.ps1
+++ automatic/disposablefixer/package.Tests.ps1
@@ -3,10 +3,11 @@
 $toolsPath = if (Test-Path Env:\ChocolateyToolsLocation) { $env:ChocolateyToolsLocation } else { "$env:SystemDrive\tools" }
 
 $packageName = Split-Path -Leaf $PSScriptRoot
+$runChocoUnitTests = Test-Path Env:\APPVEYOR_BUILD_WORKER_IMAGE -and ($env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2017') # DisposableFixer only supports Visual Studio 2017 right now.
 
 Run-PesterTests `
     -packageName "$packageName" `
     -packagePath "$PSScriptRoot" `
     -expectedEmbeddedMatch "^disposablefixer.vsix$" `
     -licenseShouldMatch "Creative Commons Public Licenses" `
-    -testChoco:$((Test-Path Env:\APPVEYOR))
+    -testChoco:$runChocoUnitTests
