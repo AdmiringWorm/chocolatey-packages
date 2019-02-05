29234074808413a8dec973e560ebe960b491256a
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-05 17:41:57 +0100

    (uplay) Added unit tests

---
+++ automatic/uplay/package.Tests.ps1
@@ -0,0 +1,9 @@
+. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot" `
+  -expectedDefaultDirectory "${env:ProgramFiles(x86)}\Ubisoft\Ubisoft Game Launcher" `
+  -customDirectoryArgument "/D="
