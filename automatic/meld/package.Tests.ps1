34931a4c1e65908376ffcc693f429edda54c40c3
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-09 15:53:11 +0100

    (meld) Added unit tests

---
+++ automatic/meld/package.Tests.ps1
@@ -0,0 +1,11 @@
+. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot" `
+  -expectedEmbeddedMatch "^Meld\-[\d\.]+\-mingw\.msi$" `
+  -licenseShouldMatch "GNU GENERAL PUBLIC LICENSE" `
+  -expectedDefaultDirectory "${env:ProgramFiles(x86)}\Meld" `
+  -customDirectoryArgument "TARGETDIR="
