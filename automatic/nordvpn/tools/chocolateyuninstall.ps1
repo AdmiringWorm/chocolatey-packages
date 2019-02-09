666078c5ea90b7b156ba386aabbaa1aa910826c0
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-09 17:48:43 +0100

    (nordvpn) Fixed uninstall script to use all installed nordvpn msis
    changelog:bug

--- automatic/nordvpn/tools/chocolateyuninstall.ps1
+++ automatic/nordvpn/tools/chocolateyuninstall.ps1
@@ -1,20 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$uninstallArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'NordVPN*'
-  fileType       = 'exe'
-  silentArgs     = '/exenoui /exenoupdates'
-  validExitCodes = @(0)
+  fileType       = 'msi'
+  silentArgs     = ""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 $uninstalled = $false
 
-[array]$key = Get-UninstallRegistryKey @packageArgs
+[array]$key = Get-UninstallRegistryKey @packageArgs | Where-Object { $_.UninstallString -match 'MsiExec' }
 
-if ($key.Count -eq 1) {
+if ($key.Count -eq 2 -or $key.Count -eq 1) {
   $key | ForEach-Object {
-    $packageArgs['file'] = "$($_.UninstallString)"
+    $packageArgs['packageName'] = $_.DisplayName
+    $packageArgs['silentArgs'] = "$($_.PSChildName) $uninstallArgs"
+    $packageArgs['file'] = ''
 
     Uninstall-ChocolateyPackage @packageArgs
   }
@@ -22,7 +25,7 @@ if ($key.Count -eq 1) {
 elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
 }
-elseif ($key.Count -gt 1) {
+elseif ($key.Count -gt 2) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert the package maintainer that the following keys were matched:"
