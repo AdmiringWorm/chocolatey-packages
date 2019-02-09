8994a21e690a2d49ff99f032819950ec8b044d4c
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-09 16:00:33 +0100

    (meld) Changed to always have a 'latest' stream

--- automatic/meld/package.Tests.ps1
+++ automatic/meld/package.Tests.ps1
@@ -5,6 +5,7 @@ $packageName = Split-Path -Leaf $PSScriptRoot
 Run-PesterTests `
   -packageName "$packageName" `
   -packagePath "$PSScriptRoot" `
+  -streams 'latest' `
   -expectedEmbeddedMatch "^Meld\-[\d\.]+\-mingw\.msi$" `
   -licenseShouldMatch "GNU GENERAL PUBLIC LICENSE" `
   -expectedDefaultDirectory "${env:ProgramFiles(x86)}\Meld" `
