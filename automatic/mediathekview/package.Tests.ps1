fa02e1d7d8fccec3351fc74e74812d7436665cc2
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-17 18:26:04 +0100

    (mediathekview) Added unit tests

---
+++ automatic/mediathekview/package.Tests.ps1
@@ -0,0 +1,11 @@
+. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot" `
+  -licenseShouldMatch "GNU GENERAL PUBLIC LICENSE" `
+  -expectedEmbeddedMatch "^MediathekView\-[\d\.]+\.zip$" `
+  -expectedDefaultDirectory "${env:ChocolateyInstall}\lib\MediathekView\tools\MediathekView-*" `
+  -expectedShimFiles "MediaThekView.exe"
