0ad942c45bd944c07428a7b017e91d4cc36081cd
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-17 08:50:39 +0100

    (herdprotect) Disabled automatic update

--- update_all.ps1
+++ update_all.ps1
@@ -61,6 +61,7 @@ $Options = [ordered]@{
   Git              = @{
     User     = ''                                       #Git username, leave empty if github api key is used
     Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+    Branch   = 'develop'
   }
 
   GitReleases      = @{
