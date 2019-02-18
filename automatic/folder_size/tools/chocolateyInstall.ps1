d1c8c8cf76dc6d762c211c7cd441d4a5d07091e8
Chocolatey <chocolatey@realdimensions.net>
2019-02-18 16:10:14 +0000

    AU: 2 updated - folder_size master-pdf-editor [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/97b199a45ebda3fada626f547346ea44c76aee38

--- automatic/folder_size/tools/chocolateyInstall.ps1
+++ automatic/folder_size/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'http://www.mindgems.com/software/FolderSize.exe'
+  url            = 'https://www.mindgems.com/software/FolderSize.exe'
   softwareName   = 'Folder Size*'
-  checksum       = '4D7A758FDA01E9C00FD14B3ED09BCC9253A8462488BE2F438A2B5AAA44F9EBA2EF6B32D52FF9D57A86B7C53250C87FCC0661A2C6A4992B8E0286F2EC783AA81F'
+  checksum       = 'C612A81419201237BE0C956727B8F5AB43F03CD5930FB7D878DB3AEBEA879DC4F9B106238523C2B75077093990EDA8FA64FB8B878FF424BD8F4A98BEA25C09BD'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
