e514730e38eab3ebf2ae11bd2a914f3e5bba6a3a
Chocolatey <chocolatey@realdimensions.net>
2019-03-07 00:11:42 +0000

    AU: 2 updated - disposablefixer sheepit-client [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/b51cc072305941bfdfbd5133fe12b51e7cabe9a4

--- automatic/sheepit-client/tools/chocolateyInstall.ps1
+++ automatic/sheepit-client/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ if ($runningProcess) {
 }
 
 $packageToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$packageClientPath = "$packageToolsPath\sheepit-5.1289.2967.exe"
+$packageClientPath = "$packageToolsPath\sheepit-5.1292.2977.exe"
 $toolsPath = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
 $clientOutputPath = "$toolsPath\sheepit.exe"
 
