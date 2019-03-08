c89d31870135ac186d85da362ace99efb63dd6be
Chocolatey <chocolatey@realdimensions.net>
2019-03-08 00:09:04 +0000

    AU: 2 updated - cacher reshade [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/f0cdd634081bc5e1f4d08090745b86d3f2660f06

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.6.1.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.6.2.exe'
   softwareName   = 'Cacher*'
-  checksum       = '916b7eaed8437b8716f19a93f5f06591a52105a75092b088e2fd4bf6067440a66e34f078ea159758370faac7622ecdaa6fd925e202e246415141b36d9ed116b0'
+  checksum       = '1ea4b5d8aa73641429107b51047d215f77e1523c91684b0555c79c909bc1fa2e91d70e7b9fc13d7bc59653ab32910902fe9779e762e7ffa2949bf85576b3d8aa'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
