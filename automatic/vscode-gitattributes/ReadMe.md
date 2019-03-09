b3d7b590ffbb524a00672e55cf055acffb9a43cb
AdmiringWorm <kim.nordmo@gmail.com>
2019-03-09 16:13:41 +0100

    (vscode-gitattributes) Changed to automatic package

--- automatic/vscode-gitattributes/ReadMe.md
+++ automatic/vscode-gitattributes/ReadMe.md
@@ -6,7 +6,3 @@ An extension for Visual Studio Code that assists you in working with `.gitattrib
 
 - Language support for `.gitattributes` files.
 - Allows adding a `.gitattributes` file by puiing files from [alexkaratarakis/gitattributes](https://github.com/alexkaratarakis/gitattributes) repository.
-
-## Notes
-
-- The version used for this package is the latest version available when it was initially created/update, but will install the latest version available at the time of install.
---
+++ automatic/vscode-gitattributes/update.ps1
@@ -0,0 +1,47 @@
+Import-Module "AU"
+
+$releases = "https://marketplace.visualstudio.com/items?itemName=hashhar.gitattributes"
+
+function global:au_BeforeUpdate($Package) {
+  $vscode = $Package.nuspecXml.package.metadata.dependencies.dependency | ? id -Match '^vscode$'
+
+  if (([version]$Latest.VsCodeVersion) -lt '1.30.0') {
+    $vscode.version = '1.30.0'
+  }
+  else {
+    $vscode.version = $Latest.VsCodeVersion
+  }
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(attributes@)[^']*" = "`${1}$($Latest.RemoteVersion)"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing "https://marketplace.visualstudio.com/items?itemName=hashhar.gitattributes"
+
+  if ($download_page.Content -match 'source":"([^"]+)') {
+    $manifestFile = $Matches[1]
+  }
+  else {
+    throw "Unable to grab manifest file"
+  }
+
+  $json = Invoke-RestMethod -UseBasicParsing $manifestFile
+
+  $vsCodeVersion = $json.engines.vscode.TrimStart('^')
+  if ($vsCodeVersion -eq '1.5.0') { $vsCodeVersion = '1.5' }
+
+  @{
+    Version       = $json.version
+    RemoteVersion = $json.version
+    VsCodeVersion = $vsCodeVersion
+  }
+}
+
+$global:au_Force = $true
+update -ChecksumFor none
