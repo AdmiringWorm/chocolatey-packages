728fb72dce1cfe4770c2ef977ce8537942a389ff
AdmiringWorm <kim.nordmo@gmail.com>
2019-03-09 16:40:23 +0100

    (vscode-gitignore) Implemented au update script

--- automatic/vscode-gitignore/update.ps1
+++ automatic/vscode-gitignore/update.ps1
@@ -1,8 +1,44 @@
-import-module "$PSScriptRoot\..\..\..\scripts\au_extensions.psm1"
+Import-Module "AU"
 
-Set-DescriptionFromReadme -SkipFirst 1
+function global:au_BeforeUpdate($Package) {
+  $vscode = $Package.nuspecXml.package.metadata.dependencies.dependency | ? id -Match '^vscode$'
 
-. "$PSScriptRoot\..\..\..\scripts\Update-IconUrl.ps1" -Name "vscode-gitignore" -PackagesDirectory "..\manual\vscode-extensions"
+  if (([version]$Latest.VsCodeVersion) -lt '1.30.0') {
+    $vscode.version = '1.30.0'
+  }
+  else {
+    $vscode.version = $Latest.VsCodeVersion
+  }
+}
 
-# Uncomment the following line before doing a manual update
-#Update-Changelog -useIssueTitle
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(ignore@)[^']*" = "`${1}$($Latest.RemoteVersion)"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing "https://marketplace.visualstudio.com/items?itemName=codezombiech.gitignore"
+
+  if ($download_page.Content -match 'source":"([^"]+)') {
+    $manifestFile = $Matches[1]
+  }
+  else {
+    throw "Unable to grab manifest file"
+  }
+
+  $json = Invoke-RestMethod -UseBasicParsing $manifestFile
+
+  $vsCodeVersion = $json.engines.vscode.TrimStart('^')
+
+  @{
+    Version       = $json.version
+    RemoteVersion = $json.version
+    VsCodeVersion = $vsCodeVersion
+  }
+}
+
+$global:au_Force = $true
+update -ChecksumFor none
