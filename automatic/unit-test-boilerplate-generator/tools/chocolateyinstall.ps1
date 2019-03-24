c2d2476d137a26f0c92c104bbf8a6d730e41e4c5
Chocolatey <chocolatey@realdimensions.net>
2019-03-24 08:07:41 +0000

    AU: 1 updated - unit-test-boilerplate-generator [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/9f4e73bfbe400e20029c1247f5c7776f48640a48

--- automatic/unit-test-boilerplate-generator/tools/chocolateyinstall.ps1
+++ automatic/unit-test-boilerplate-generator/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $parameters = @{
   PackageName = $env:ChocolateyPackageName
-  File        = "$toolsPath\Unit_Test_Boilerplate_Generator_v1.9.17.vsix"
+  File        = "$toolsPath\Unit_Test_Boilerplate_Generator_v1.9.20.vsix"
 }
 
 Install-VisualStudioVsixExtension @parameters
