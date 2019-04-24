99ee4b123e7d158bd830e173e105aa8879374bdf
AdmiringWorm <kim.nordmo@gmail.com>
2019-04-25 00:04:05 +0200

    (disposablefixer) Fixed updater
    fixes #64

--- automatic/disposablefixer/update.ps1
+++ automatic/disposablefixer/update.ps1
@@ -35,16 +35,28 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases
 
-  $json = $download_page.Scripts | ? class -eq 'vss-extension' | Select-Object -expand innerHtml | ConvertFrom-Json | Select-Object -expand versions
-  $url = $json.files | ? source -match "\.vsix$" | Select-Object -expand source -first 1
+  if ($download_page.Content -match 'AssetUri":"([^"]+)') {
+    $assetUri = $Matches[1]
+  }
+  else {
+    throw "Unable to grab asset uri file"
+  }
 
-  $filename = [IO.Path]::GetFilename($url)
+  if ($download_page.Content -match 'Payload.FileName":"([^"]+vsix)') {
+    $filename = $Matches[1]
+  } else {
+    throw "Unable to grab the filename"
+  }
 
-  $version = $json.version | Select-Object -first 1
+  if ($download_page.Content -match 'Version":"([\d]\.[\d\.]+)') {
+    $version = $Matches[1]
+  } else {
+    throw "Unable to grab the version"
+  }
 
   @{
     Version    = $version
-    URL32      = $url
+    URL32      = "$assetUri/$filename"
     Filename32 = $filename
   }
 }
