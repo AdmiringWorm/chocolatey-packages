30328091bd2f4d9df2687bc305d70a79940e74a6
Chocolatey <chocolatey@realdimensions.net>
2019-05-22 08:07:20 +0000

    AU: 4 updated - cacher electrum-ltc electrum-ltc.install electrum-ltc.portable [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/832497e760ef61c89f5c2acf0c20f2e77ac6fed1

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.9.0.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.10.2.exe'
   softwareName   = 'Cacher*'
-  checksum       = '64a2872ad2f07a79b75876dbb61ece0c56ee5fbd296a807b513d4fd123397f753cacec1ba069e56776e5f8a0951aceed48187916ae7bf9228fd1d72a67b18046'
+  checksum       = '6ecf4272407f30d0cc6b3b02368fb34d0dae80cf562aa97c6829f1daf223e0cd92e4e5f56bc41c7e983ff1a9d83f485462a1fb3ae93027845337b154adefc196'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
