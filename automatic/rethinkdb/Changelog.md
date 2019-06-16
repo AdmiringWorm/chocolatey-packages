2f291320361413367f71ccfa20ea80141ad9059d
Chocolatey <chocolatey@realdimensions.net>
2019-06-16 19:30:11 +0000

    AU: 1 updated - rethinkdb [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/d1e1f3d8b71e4a07047122e28f41f94da97214df

--- automatic/rethinkdb/Changelog.md
+++ automatic/rethinkdb/Changelog.md
@@ -1,5 +1,5 @@
 # ![RethinkDB Changelog](https://img.shields.io/badge/RethinkDB-Package%20Changelog-blue.svg?style=for-the-badge)
 
-## Upcoming
+## Version: 2.3.6 (2019-06-16)
 
 - Taken over as a new maintainer of the rethinkdb package (original maintainer: rainbowhyphen)
--- automatic/rethinkdb/tools/chocolateyinstall.ps1
+++ automatic/rethinkdb/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = $toolsDir
-  file        = "$toolsDir\"
+  file        = "$toolsDir\rethinkdb-2.3.6.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
