24c902ec837d891e0e76ee0ad3ed96335a313921
Chocolatey <chocolatey@realdimensions.net>
2019-06-16 15:11:37 +0000

    AU: 2 updated - innosetup tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/4bff28b43ecd79de2917fbee3eda556747844b17

--- automatic/innosetup/tools/chocolateyInstall.ps1
+++ automatic/innosetup/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\isetup-4.2.7.exe"
+$filePath = "$toolsDir\innosetup-6.0.2.exe"
 
 $packageArgs = @{
 	packageName   = 'innosetup'
