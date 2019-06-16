354f28367022e760398984a0b9a0479dbf9c8367
sharkykh <sharkykh@gmail.com>
2019-06-16 09:54:44 +0300

    Fix missing version 5

--- automatic/innosetup/tools/chocolateyInstall.ps1
+++ automatic/innosetup/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\isetup-4.2.7.exe"
+$filePath = "$toolsDir\innosetup-5.6.1-unicode.exe"
 
 $packageArgs = @{
 	packageName   = 'innosetup'
 	fileType      = 'exe'
 	softwareName  = 'Inno Setup*'
-  checksum      = ''
-  checksumType  = ''
+  checksum      = '27D49E9BC769E9D1B214C153011978DB90DC01C2ACD1DDCD9ED7B3FE3B96B538'
+  checksumType  = 'sha256'
 	file          = $filePath
 	silentArgs    = "/SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
 	validExitCodes= @(0,3010,1641)
