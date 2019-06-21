de144c38efa2f8569fece614e08a5d36c994b210
Chocolatey <chocolatey@realdimensions.net>
2019-06-21 18:13:08 +0000

    AU: 1 updated - master-pdf-editor [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/7017465cf70d024dbe7c0e25421cf6049ddfaf90

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = 'F3EB86EE2837512D321FE3012FEB2DE1553F73DBB90CBE5DE93D154753ADDF2E46CFC0C536963B5BBC57288CB0F48C4C0CA2F61AAC935BE5F9BF97EAF9124287'
+  checksum       = '76A567F0E2337602F7F0CAE83FB5521E3C525DE23F6623DFA23BE99D65602A3EE422D400A079E5BF59215566BF0F43E3BDF7C5B7C857FA0C495F1468C5C1A32E'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
