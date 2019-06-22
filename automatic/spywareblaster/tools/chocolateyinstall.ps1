9483756235c5a9e761633c9ce042e42619eb84f2
AdmiringWorm <kim.nordmo@gmail.com>
2019-06-22 14:29:49 +0200

    (spywareblaster) Removed unused package arguments

--- automatic/spywareblaster/tools/chocolateyinstall.ps1
+++ automatic/spywareblaster/tools/chocolateyinstall.ps1
@@ -4,13 +4,10 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   url            = 'https://www.brightfort.net/downloads/spywareblastersetup55.exe'
-  url64bit       = ''
 
   softwareName   = 'SpywareBlaster *'
   checksum       = '616d909a54e2f7ece430056ee3c59834e05728b16f583733c775cea6d2e1d36d93f8dd772e30d68373c0929ba2f117d6d1036fc6da83b90f13da1f131c6a4f9a'
   checksumType   = 'sha512'
-  checksum64     = ''
-  checksumType64 = 'sha256'
 
   silentArgs     = "/SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0, 3010, 1641)
