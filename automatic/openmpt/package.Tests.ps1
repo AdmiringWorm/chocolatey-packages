7d1b096e618d1d5b851934e0f49dc8c8d0e202d0
AdmiringWorm <kim.nordmo@gmail.com>
2019-06-23 14:54:08 +0200

    (openmpt) Added unit tests for package
    ref #48

---
+++ automatic/openmpt/package.Tests.ps1
@@ -0,0 +1,13 @@
+﻿. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot" `
+  -expectedEmbeddedMatch "^OpenMPT-[\d\.]+-Setup(-x64)?\.exe$" `
+  -expectedDefaultDirectory "${env:ProgramFiles}\OpenMPT" `
+  -licenseShouldMatch "are permitted provided that the following conditions are met" `
+  -customDirectoryArgument "/DIR=" `
+  -customUninstallChecks { sleep 100 } `
+  -test32bit
