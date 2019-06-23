b591ec4763892fa6391654d785fd2dd2668bfa28
AdmiringWorm <kim.nordmo@gmail.com>
2019-06-23 17:48:10 +0200

    (qemu) Added unit tests for package

---
+++ automatic/qemu/package.Tests.ps1
@@ -0,0 +1,10 @@
+﻿. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot" `
+  -expectedDefaultDirectory "${env:ProgramFiles}\qemu" `
+  -customDirectoryArgument "/D=" `
+  -test32bit
