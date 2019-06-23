a4e72a8f40c2c71d25f03ac956f43383be1d034f
AdmiringWorm <kim.nordmo@gmail.com>
2019-06-23 20:09:56 +0200

    (qilin-editor) Added unit tests for package

---
+++ automatic/qilin-editor/package.Tests.ps1
@@ -0,0 +1,12 @@
+﻿. "$PSScriptRoot\..\..\scripts\Run-PesterTests.ps1"
+
+$packageName = Split-Path -Leaf $PSScriptRoot
+
+Run-PesterTests `
+  -packageName "$packageName" `
+  -packagePath "$PSScriptRoot" `
+  -expectedEmbeddedMatch "^qilin-windows.zip$" `
+  -licenseShouldMatch 'MIT License' `
+  -expectedShimFiles 'qilin.exe' `
+  -notExpectedShimFiles 'chromedriver.exe','me.laniewski.qilin.exe','nwjc' `
+  -installWithPreRelease
