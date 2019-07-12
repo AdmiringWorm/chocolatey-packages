3b68d2f5d2943663c4810f8391e9ba825641fc7b
Chocolatey <chocolatey@realdimensions.net>
2019-07-12 16:08:44 +0000

    AU: 2 updated - cacher sheepit-client [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/809ffac6416412e566b34ada9aac1e87c26e5246

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.12.0.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.12.2.exe'
   softwareName   = 'Cacher*'
-  checksum       = 'c7e25ff6ec7c22dd6f656ecf8f4c80acccb630f346c35c7596c8be921ea708d9c73f02cb0d1e291f8c52c4163967973ea41af94efc219e725fdc14218810cd96'
+  checksum       = '6b5a1a9afc1dd6f12ea02475bb6f0181f6676567cea870d9aee06cbced54caa019ac7559bdf0eaaa59588bf7fd9206f63f27c50a566f7a47d0df3f59ec8ba935'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
