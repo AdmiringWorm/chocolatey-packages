18ca4edfc688325f35f56a92a7abc5af099a7fba
Chocolatey <chocolatey@realdimensions.net>
2019-07-18 00:08:14 +0000

    AU: 1 updated - Qemu [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/d88111cc28d82e189a6424a8b338ed9831d08d68

--- automatic/qemu/tools/chocolateyInstall.ps1
+++ automatic/qemu/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20190713.exe'
-  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20190713.exe'
+  url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20190717.exe'
+  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20190717.exe'
   softwareName   = 'QEMU'
-  checksum       = '55a3f993678cde4055ebc5d77bc6f537ebb7cc515c9b05a57dc3524b559af603'
+  checksum       = '8b1d26d710971d3b813f344789a9b46a535190da0b39015fb8c7995182be89f7'
   checksumType   = 'sha256'
-  checksum64     = 'b99a4d43bc449650e78b8e2943300dbf7b6b37b7f354a1beb2b2401ea3f3aac2'
+  checksum64     = 'f76dcf9eed6118600e2d02750885114348b1cd079e96c5492befee66ca277ed6'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
