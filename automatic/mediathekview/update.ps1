7a54c1806d33908c70d47d3e9b132651900b343c
AdmiringWorm <kim.nordmo@gmail.com>
2019-08-19 07:48:45 +0200

    (mediathekview) Update++

--- automatic/mediathekview/update.ps1
+++ automatic/mediathekview/update.ps1
@@ -35,7 +35,7 @@ function global:au_GetLatest {
   $re = 'MediathekView\-[\d\.]+.*\.zip$'
   $url32 = $download_page.Links | ? href -match $re | select -last 1 -expand href | % { $releases + $_ }
 
-  $verRe = '[-]|\.zip'
+  $verRe = 'View-|-win\.zip'
   $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
   @{
     URL32       = [uri]$url32
