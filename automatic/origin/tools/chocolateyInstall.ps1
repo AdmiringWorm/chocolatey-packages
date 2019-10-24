a112da48511ef2203798dc9c2bdd2b9039cb1768
Chocolatey <chocolatey@realdimensions.net>
2019-10-24 23:14:58 +0000

    AU: 1 updated - origin [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/f642759639a90a8b0e1fb971a6cb7fc7ecb22d7d

--- automatic/origin/tools/chocolateyInstall.ps1
+++ automatic/origin/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   url          = 'https://download.dm.origin.com/origin/live/OriginSetup.exe'
   softwareName = 'Origin*' # Probably not needed, but we add registry keys so we keep it
-  checksum     = 'F03F9DB201FD47677B51AFC412A7F66D414C32AAF57C80F56BBBD9FB8EDA5D0EB808D7467DD9156D5C0622725E6E1654379CCE0016BFFD8708B13BBD16ED0BC0'
+  checksum     = '592169AF87E69EA23B318EA29492A6111DC6A42BCF7E589097D666C026C30CC98930F330AC7CC8A6374234278ED38041C2AA5658B24A0CBDF7F02D83569DB968'
   checksumType = 'sha512'
   destination  = Get-PackageCacheLocation
 }
@@ -16,7 +16,7 @@ if ($env:ChocolateyForce -ne $true) {
   try {
     $InstalledVersion = Get-UninstallRegistryKey $packageArgs['softwareName'] | Select-Object -First 1 -Expand 'DisplayVersion'
 
-    if ([Version]::Parse($InstalledVersion) -ge [Version]::Parse('10.5.50.31938'))
+    if ([Version]::Parse($InstalledVersion) -ge [Version]::Parse('10.5.52.32372'))
     {
       Write-Host "Skipping installation because version $InstalledVersion is already installed."
       return
@@ -34,10 +34,10 @@ $packageArgs['file'] = $zipFile
 $packageArgs['destination'] = GetInstallLocation $pp
 
 if ($pp.NoAutoUpdate) {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.50.31938'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.52.32372'
 }
 else {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.50.31938'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.52.32372'
 }
 
 Get-ChocolateyUnzip @packageArgs
