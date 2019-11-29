9f4a3aba80f4969d17918dcebdf5e24e8fb51f6f
Chocolatey <chocolatey@realdimensions.net>
2019-11-29 16:07:58 +0000

    AU: 1 updated - Coq [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/25e341b45abe34b054856c2ea9438c6cd0d387cb

--- automatic/coq/tools/chocolateyInstall.ps1
+++ automatic/coq/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://github.com/coq/coq/releases/download/V8.10.1/coq-8.10.1-installer-windows-i686.exe'
-  url64bit       = 'https://github.com/coq/coq/releases/download/V8.10.1/coq-8.10.1-installer-windows-x86_64.exe'
+  url            = 'https://github.com/coq/coq/releases/download/V8.10.2/coq-8.10.2-installer-windows-i686.exe'
+  url64bit       = 'https://github.com/coq/coq/releases/download/V8.10.2/coq-8.10.2-installer-windows-x86_64.exe'
   softwareName   = 'coq*'
-  checksum       = 'f7138651b1614ea9d4cd105bd60f192ffac77b6e51dc766684019e7ab4348ed5'
+  checksum       = '04372c3407f4abb7f48d098d57f299be7343f2d345a7bc82fc2a3d03622b9b45'
   checksumType   = 'sha256'
-  checksum64     = '6bacd5e727bb3d9a405dea880e21453fbd2d6a24524454aa32e2b7ab81c1e638'
+  checksum64     = '4febccc360a273ea4e2de93412d9c9c1880550d03663c73e0b980b61f7d489d9'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
