9930dcf1ba55abdec7d72fbf15df26d6533ff66a
AdmiringWorm <kim.nordmo@gmail.com>
2019-12-05 05:25:07 +0100

    (mediathekview) Corrected install directory in integration test

--- automatic/mediathekview/package.Tests.ps1
+++ automatic/mediathekview/package.Tests.ps1
@@ -7,5 +7,5 @@ Run-PesterTests `
   -packagePath "$PSScriptRoot" `
   -licenseShouldMatch "GNU GENERAL PUBLIC LICENSE" `
   -expectedEmbeddedMatch "^MediathekView\-[\d\.]+\-win\.zip$" `
-  -expectedDefaultDirectory "${env:ChocolateyInstall}\lib\MediathekView\tools\MediathekView-*" `
+  -expectedDefaultDirectory "${env:ChocolateyInstall}\lib\MediathekView\tools\MediathekView" `
   -expectedShimFiles "MediaThekView.exe"
