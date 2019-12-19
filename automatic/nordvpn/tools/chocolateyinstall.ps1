fb84ff478fac60968e4a46c3dbddfdd2239b2fe7
Chocolatey <chocolatey@realdimensions.net>
2019-12-19 23:12:33 +0000

    AU: 2 updated - authy-desktop nordvpn [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/1ff95155c6c5f38fed475e0916965f6a4b529afb

--- automatic/nordvpn/tools/chocolateyinstall.ps1
+++ automatic/nordvpn/tools/chocolateyinstall.ps1
@@ -8,9 +8,9 @@ Import-Certificate -FilePath "$toolsPath\nordvpn.cer" -CertStoreLocation "Cert:\
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.26.6/NordVPNSetup.exe'
+  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.26.7/NordVPNSetup.exe'
   softwareName   = 'NordVPN*'
-  checksum       = '415590492DD213ED535EC0C1950F833229CE123B5E90741F3E34E3F9D8911BD8CD087346ED783331EE6991B93682D10A162EFF14D69CB65CE982F20681C6A858'
+  checksum       = '44D3FD36EB6B5A4FAF2084F6599435DC31AA02AB06F4DFDBE3B1599FCCB6DAD9A3EFB1387641977563141719A04328D5AEBF95EA1BADA6A51237270058282196'
   checksumType   = 'sha512'
   silentArgs     = '/exebasicui /exenoupdates /qb /norestart'
   validExitCodes = @(0)
