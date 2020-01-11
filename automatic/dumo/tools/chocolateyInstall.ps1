417750c84b53e0bb62b34f584e12e078f8cfdb70
Chocolatey <chocolatey@realdimensions.net>
2020-01-11 00:13:11 +0000

    AU: 2 updated - dumo openmpt [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/3eec0f53456ea5fce36baa906c882a7ca8f6c645

--- automatic/dumo/tools/chocolateyInstall.ps1
+++ automatic/dumo/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://kcsoftwares.com/files/dumo_lite.exe'
   softwareName   = 'KC Softwares DUMo'
-  checksum       = 'D7B03014B937106C45C1E24F014A9505610A294BF295D0B33F20FA7336D9EC0AF5A4A80D59D0EEB1673A681372DFC5363A7EC66DA836F52068FAB30F9242A75F'
+  checksum       = '57D8DC460846C416761FE746F850156B11DE319066F4207AA0C4C8DCADBAD871ADB4D5FB6FE30BD542B648A6935F0A0E348A91E6A2F8A0D74C1B9A4310026480'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
