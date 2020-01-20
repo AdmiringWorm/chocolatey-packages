8177bfea0304dcbc183c8b800aea56060eefcb42
Chocolatey <chocolatey@realdimensions.net>
2020-01-20 12:16:54 +0000

    AU: 1 updated - nordvpn [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/cb59b4155053e6489e625da3ef71f1696594fe2b

--- automatic/nordvpn/tools/chocolateyinstall.ps1
+++ automatic/nordvpn/tools/chocolateyinstall.ps1
@@ -8,9 +8,9 @@ Import-Certificate -FilePath "$toolsPath\nordvpn.cer" -CertStoreLocation "Cert:\
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.26.8/NordVPNSetup.exe'
+  url            = 'https://downloads.nordcdn.com/apps/windows/10/NordVPN/6.26.11/NordVPNSetup.exe'
   softwareName   = 'NordVPN*'
-  checksum       = '47D5C37646E313B8C10DAB7EEB3CF75E05B2BF3A16CA1F58ED017E51FFAF82D061629345C9DB675E8E2E58843D6BE5FD5B49CD8040BFC9B819DE26D4C8565629'
+  checksum       = '4D66A24449979BBE0A3BFAAF9BEB8E9E9F675ECCB12E25D7E9C93496E354ECDCEFD8B611865B3C1A0F64298FCE95A74F25D5B798731E1688287426F8042CC489'
   checksumType   = 'sha512'
   silentArgs     = '/exebasicui /exenoupdates /qb /norestart'
   validExitCodes = @(0)
