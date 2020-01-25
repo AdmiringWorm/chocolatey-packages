6bd9a01971e9c2f595b06382202e992ff4a0da31
Chocolatey <chocolatey@realdimensions.net>
2020-01-25 13:38:33 +0000

    AU: 2 updated - lunacy unit-test-boilerplate-generator [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/191510989c7395cc7e079fe4c4799b895f04d065

--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://desk.icons8.com/lunacy/LunacySetup.exe'
   softwareName   = 'Lunacy'
-  checksum       = '46A42160C91DE965EB7ACA5F642BC321E8EC64D8DD4EF7D60FD628DE709E128448F4D4AF2A1F09058B5631A7E3496AC04470B25C04B05532272A411C962F3C9C'
+  checksum       = 'FB0B8098D4008621CFBFCE12C84F90D001EABA10F01A48C25C11E232B8B1BEBC05DC1C902DD7B1ABA939B12607A7586B2AD04EE103D447FBF40D808AA2F25D96'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
