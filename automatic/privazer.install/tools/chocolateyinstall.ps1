0aeb91bf4b5e2acb7a57261aacf1cb1a338cde8c
Chocolatey <chocolatey@realdimensions.net>
2020-02-20 13:33:34 +0000

    AU: 1 updated - privazer.install [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/266db93c6021973bf44c5bc0d1c6aa0e64afa96a

--- automatic/privazer.install/tools/chocolateyinstall.ps1
+++ automatic/privazer.install/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   silentArgs     = '-silent'
   validExitCodes = @(0)
   url            = 'https://privazer.com/PrivaZer_free.exe'
-  checksum       = 'ee4fba0d7654cb8c2984e9f8a53528b2dd3fe5ec297d77552d13b10e02f59f59cfd23aaa146ab33fbe1aa14a320e24ae7284eb3f660106878243a650160339f6'
+  checksum       = 'e2da2dbfb071bdc845a1e42615114ab9a68a9b4d954129b395b87d688ba5431b957846c17b7cee01af2605e7920e14f973f3a9ee250128341e750797fcf715a5'
   checksumType   = 'sha512'
 }
 
