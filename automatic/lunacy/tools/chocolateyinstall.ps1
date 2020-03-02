63deadbc90271699497d5aed24399557e03215fc
Chocolatey <chocolatey@realdimensions.net>
2020-03-02 13:34:10 +0000

    AU: 2 updated - lunacy nordvpn [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/fc90739b9f51ac4814362ef4a22fdb06dc0d1e31

--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://desk.icons8.com/lunacy/LunacySetup.exe'
   softwareName   = 'Lunacy'
-  checksum       = 'B5ABFB3177F69D737DF2D20DE65C22B0659AC555927308720D4E2E5B49B3A77B1AC41B4064CEBAE6A6BDBBF754C922790D9E42F88752EEC1DB58DE28737FD903'
+  checksum       = 'F1DDF7EECF30DD2B5DEE5132644F00EBF1D1D3181A9C8E763C8AA5E1923F7C1D71A319A7DFAE6EFDA7013B98B29F54ED2A4D91256B4766BBA13A471EE83003AB'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
