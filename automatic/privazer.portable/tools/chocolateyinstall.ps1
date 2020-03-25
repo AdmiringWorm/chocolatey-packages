0066195e54304e15a25824990c29c7b7551d6bdf
Chocolatey <chocolatey@realdimensions.net>
2020-03-25 13:39:42 +0000

    AU: 1 updated - privazer.portable [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/f97fad23d41eb2f97ef17edf5e95f55dbbcaa82c

--- automatic/privazer.portable/tools/chocolateyinstall.ps1
+++ automatic/privazer.portable/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url            = 'https://privazer.com/PrivaZer.exe'
-  checksum       = '465df68bf8a11b37c8216efbf169e5d71748811deecf46259769126c4a0e62271041248d1cb2979905f56e219dabe7b9d6503e8b7f95654d20f2322c7b102f84'
+  checksum       = '3645bc5f2a46e60a6131e5748e0d967926a0e73bb457a3cd2ec834e2fcf58d33aa931c3c00c0b30332046611e92afaa756e21e01f83317fc25972d3c860cf247'
   checksumType   = 'sha512'
   fileFullPath   = "$toolsDir\privazer.exe"
 }
