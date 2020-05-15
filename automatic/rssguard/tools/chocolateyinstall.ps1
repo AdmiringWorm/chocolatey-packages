0f5186b12c75533100f0e0138f90d59737337908
Chocolatey <chocolatey@realdimensions.net>
2020-05-15 13:33:54 +0000

    AU: 1 updated - RssGuard [skip ci]

--- automatic/rssguard/tools/chocolateyinstall.ps1
+++ automatic/rssguard/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = ''
-  file64      = "$toolsPath\rssguard-3.6.1-af57e4e-win64.7z"
+  file64      = "$toolsPath\rssguard-3.6.2-f40297d-win64.7z"
   destination = $toolsPath
 }
 
