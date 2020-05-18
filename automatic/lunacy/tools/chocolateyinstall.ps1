90843f698a54b9af5b897a854f1bc803a9a2b946
Chocolatey <chocolatey@realdimensions.net>
2020-05-18 13:38:09 +0000

    AU: 2 updated - lunacy uplay [skip ci]

--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://desk.icons8.com/lunacy/LunacySetup.exe'
   softwareName   = 'Lunacy'
-  checksum       = '7EDD30EA1DC9CBA9277FD57E585398FE37DDB043CBB350A33B6A58CAC1EF570F4A2DA6B33F2CE598BC7B7C251418300F5F78290FC5905C651E1033856F165304'
+  checksum       = 'FB903ABD421D4FE228ABC37904E4A0C34CA45C1EBBB91C90172580B6E0DB73002A2499FC3E7F06216CAEF8A6649A21F9FD6BDAA809E0B67EA218E3E8079B2E00'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
