76493c2b9d48f91acf0055d259496a3177d44cea
Chocolatey <chocolatey@realdimensions.net>
2020-05-21 13:38:09 +0000

    AU: 2 updated - innosetup lunacy [skip ci]

--- automatic/innosetup/tools/chocolateyInstall.ps1
+++ automatic/innosetup/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\innosetup-6.0.4.exe"
+$filePath = "$toolsDir\innosetup-6.0.5.exe"
 
 $packageArgs = @{
   packageName   = 'innosetup'
--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://desk.icons8.com/lunacy/LunacySetup.exe'
   softwareName   = 'Lunacy'
-  checksum       = 'FB903ABD421D4FE228ABC37904E4A0C34CA45C1EBBB91C90172580B6E0DB73002A2499FC3E7F06216CAEF8A6649A21F9FD6BDAA809E0B67EA218E3E8079B2E00'
+  checksum       = '84023E8210960392BB6EDE8EA7E4F5CB0CA033A12766F213213E18A3B33FAD14D98C8734D33A5611C80A6B8F7DE7B9B0FE8E24220598AE67AEF3D18978274C11'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
