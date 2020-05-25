0c39167f4d6de81ddf968d943b92e74a7ac621b7
Chocolatey <chocolatey@realdimensions.net>
2020-05-25 01:38:45 +0000

    AU: 1 updated - sheepit-client [skip ci]

--- automatic/sheepit-client/tools/chocolateyInstall.ps1
+++ automatic/sheepit-client/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ if ($runningProcess) {
 }
 
 $packageToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$packageClientPath = "$packageToolsPath\sheepit-6.1721.0.exe"
+$packageClientPath = "$packageToolsPath\sheepit-6.1725.0.exe"
 $toolsPath = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
 $clientOutputPath = "$toolsPath\sheepit.exe"
 
