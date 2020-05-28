f285211e57a0fba2013a62f610ebe9f0b0689ebc
Chocolatey <chocolatey@realdimensions.net>
2020-05-28 19:38:57 +0000

    AU: 2 updated - lunacy tablacus [skip ci]

--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://desk.icons8.com/lunacy/LunacySetup.exe'
   softwareName   = 'Lunacy'
-  checksum       = '84023E8210960392BB6EDE8EA7E4F5CB0CA033A12766F213213E18A3B33FAD14D98C8734D33A5611C80A6B8F7DE7B9B0FE8E24220598AE67AEF3D18978274C11'
+  checksum       = 'BE64923CE530ED9F604F8E28E1B358BEE595A952DBDD3B8034B2D3B160CC5FB2E1D010ACAC9333174955B9A48B4ECDE4C21609E43AAC46E3D68F71D24B8D3004'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te200528.zip"
+  file        = "$toolsPath\te200529.zip"
   destination = "$toolsPath"
 }
 
