b6e6a47738aae4d49054f6f8ede7528682680afd
Chocolatey <chocolatey@realdimensions.net>
2020-05-31 19:40:41 +0000

    AU: 2 updated - privazer.install tablacus [skip ci]

--- automatic/privazer.install/tools/chocolateyinstall.ps1
+++ automatic/privazer.install/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   silentArgs     = '-silent'
   validExitCodes = @(0)
   url            = 'https://privazer.com/PrivaZer_free.exe'
-  checksum       = 'eec092eb93f519b03f18a2dc43c48e1cb7b7daf791e4cfdee8785e0d90b3507a5ffbf22cd775d37ea9e71cc72cb4c7b7f62d3d4c4929f5a889e1c763d0b157df'
+  checksum       = '5f34e1b0ba3caab2ec27fb25e3f2087251180cb0a7c134e0fb721fcc77d5d21239146b07880a9e1bf55df34159a658b4d63f47b46da37555c9024d902c27c4f0'
   checksumType   = 'sha512'
 }
 
