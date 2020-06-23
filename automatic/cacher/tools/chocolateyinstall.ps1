6cc191ab3df1b4613106cdc7e410b846e216c736
Chocolatey <chocolatey@realdimensions.net>
2020-06-23 19:32:53 +0000

    AU: 2 updated - cacher linux-reader [skip ci]

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.30.2.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.30.3.exe'
   softwareName   = 'Cacher*'
-  checksum       = '1cd3df39f3b6fef040bbf96d817fdd06a5afd4c911254acf3b4334e882359a696b43fd6e67f4ae48a492eb8742cd8651815f4519ba72f4f96f24e30b4d3a47f0'
+  checksum       = '4dc4bec99baf5496bd9081ad58948f647a5cc9671c4bd5c6586101920a5c77f83437ac0b04bd9756c2692bea3e7c576d051428a6ae736859d4f4a4dae8b6b1c4'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
