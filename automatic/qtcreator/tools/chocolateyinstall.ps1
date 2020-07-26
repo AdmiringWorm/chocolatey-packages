26b70966ee9b3fc9222d2df0061c407b9464d2ab
Chocolatey <chocolatey@realdimensions.net>
2020-07-26 13:31:37 +0000

    AU: 2 updated - openmpt qtcreator [skip ci]

--- automatic/qtcreator/tools/chocolateyinstall.ps1
+++ automatic/qtcreator/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $installToolsPath = Get-ToolsLocation
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://download.qt.io/official_releases/qtcreator/4.12/4.12.3/installer_source/windows_msvc2017_x86/qtcreator.7z'
-  url64bit       = 'https://download.qt.io/official_releases/qtcreator/4.12/4.12.3/installer_source/windows_msvc2017_x64/qtcreator.7z'
+  url            = 'https://download.qt.io/official_releases/qtcreator/4.12/4.12.4/installer_source/windows_msvc2017_x86/qtcreator.7z'
+  url64bit       = 'https://download.qt.io/official_releases/qtcreator/4.12/4.12.4/installer_source/windows_msvc2017_x64/qtcreator.7z'
   destination    = "$installToolsPath\$env:ChocolateyPackageName"
-  checksum       = '8e9cc26a920d0f4994d11e6083f69d3ac89b275b1228d4904dd95931f5623cd1'
+  checksum       = '829a15724c5ac203e14fe3e7a74a08612afea031bf99eaa0e9d450c80384f3e2'
   checksumType   = 'sha256'
-  checksum64     = 'e3e28fc8f939507058bf3b5608debd37b81e7609ef231d35f1214b3a74f27785'
+  checksum64     = '7dc171daafbba724a02a26a07835014b9741376fa0eec832b0bcac27d38e41db'
   checksumType64 = 'sha256'
 }
 
