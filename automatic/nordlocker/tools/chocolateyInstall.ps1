a5d19410e7f685aca08eb11d8cb8e46992a7a80f
AdmiringWorm <kim.nordmo@gmail.com>
2020-08-21 13:11:57 +0200

    (nordlocker) Removed unneccessary arguments

--- automatic/nordlocker/tools/chocolateyInstall.ps1
+++ automatic/nordlocker/tools/chocolateyInstall.ps1
@@ -12,8 +12,6 @@ $packageArgs = @{
 
   checksum       = 'DB0B40A54CF83A1CD040685A82374D41486903E430F0E3200AF69A40B357E113057DE13EE41C0CFE864BE64757E5BAB303FB1A2165C90AC2EBC8E29414460F5B'
   checksumType   = 'sha512'
-  checksum64     = ''
-  checksumType64 = 'sha256'
 
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName)\$($env:chocolateyPackageVersion)\install.log`""
   validExitCodes = @(0, 3010, 1641)
