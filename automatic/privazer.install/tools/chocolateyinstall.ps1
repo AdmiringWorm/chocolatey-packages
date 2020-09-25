c186c546f72a4ea53c230bfc5939b319b4c9ec22
Chocolatey <chocolatey@realdimensions.net>
2020-09-25 19:41:04 +0000

    AU: 3 updated - privazer privazer.install privazer.portable [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/acd5d7ac2fae1a88a8a9cacb9161480f2499f4a9

--- automatic/privazer.install/tools/chocolateyinstall.ps1
+++ automatic/privazer.install/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   silentArgs     = '-silent'
   validExitCodes = @(0)
   url            = 'https://privazer.com/PrivaZer_free.exe'
-  checksum       = '4eaccb92ea73590559ccd6793903541d0bc1bf8db7669917288b18cfd8645db48a5bbf7067864dab218facc47e7913f657be8da56b5b6bc1099ef52aea6d889f'
+  checksum       = '6b14b19974f2da603c211b3d64276d9748e36af6b121afcd835f09d2eee817b6ff39562a4460f65fe2e8839552e4217da596a92d0e2bb0c63be0c2257403ae16'
   checksumType   = 'sha512'
 }
 
--- automatic/privazer.portable/tools/chocolateyinstall.ps1
+++ automatic/privazer.portable/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url            = 'https://privazer.com/PrivaZer.exe'
-  checksum       = '39694edf3aafa71be224c20322af717739317e9d7d129c90200b9241a46e3d5c05ec2b02fc2dc72fc2ad255db0bbd9325cade3b69d0ddaade8d0c5adcc3e94d0'
+  checksum       = '519f39ee23d67e4b046f09e8a59b3e44ccd6d788118827513ce39dc07ea778706f28fdb502332372e3d46eeec1e949e608a79244e058032e5251a32dc48894d4'
   checksumType   = 'sha512'
   fileFullPath   = "$toolsDir\privazer.exe"
 }
