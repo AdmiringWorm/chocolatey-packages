bd8e05d725b59ab7b0d71252656ba7c6f14e2a08
Michael Weghorn <m.weghorn@posteo.de>
2020-10-07 12:52:14 +0200

    (qtcreator-cdbext) Add Qt Creator CDB Extension
    This adds the CDB debugger extension for Qt Creator,
    which is provided in a separate .7z file on Qt's
    download page.
    
    Since it is an extension for Qt Creator, this
    package depends on the 'qtcreator' package.
    
    The packaging files were created based on the ones
    for the 'qtcreator' package, i.e. those for the
    existing 'qtcreator' package were initially
    copied, then adapted for this new package as
    needed.
    
    Fixes: #216

---
+++ automatic/qtcreator-cdbext/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$installToolsPath = Get-ToolsLocation
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://download.qt.io/official_releases/qtcreator/4.13/4.13.2/installer_source/windows_x86/qtcreatorcdbext.7z'
+  url64bit       = 'https://download.qt.io/official_releases/qtcreator/4.13/4.13.2/installer_source/windows_x64/qtcreatorcdbext.7z'
+  destination    = "$installToolsPath\qtcreator"
+  checksum       = '45f0103f3a5946ef61e828dac346699e54a30d9f6671a3eb7bb0b9a8777910c1'
+  checksumType   = 'sha256'
+  checksum64     = 'bd096cc7278b5229787966b45d55409301c8e2c4771080e26737c72ef5b10be7'
+  checksumType64 = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
