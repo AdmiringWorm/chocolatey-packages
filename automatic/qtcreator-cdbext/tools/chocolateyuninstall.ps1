a0dd9f132c7256abe6f248aa21e2d53b28f925fc
AdmiringWorm <kim.nordmo@gmail.com>
2020-10-09 15:51:21 +0200

    (qtcreator-cdbext) Add uninstallation script to remove plugin

---
+++ automatic/qtcreator-cdbext/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$toolsDir = Get-ToolsLocation
+
+$pluginDirs = @(
+  "$toolsDir\qtcreator\lib\qtcreatorcdbext64"
+  "$toolsDir\qtcreator\lib\qtcreatorcdbext32"
+)
+
+$pluginDirs | ? { Test-Path $_ } | % {
+  Remove-Item -Recurse -Force $_
+}
