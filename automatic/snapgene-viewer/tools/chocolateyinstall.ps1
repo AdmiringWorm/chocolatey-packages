a86b2cdbbdcb9d682aebc507ce58938775302669
Chocolatey <chocolatey@realdimensions.net>
2020-10-09 18:17:15 +0000

    AU: 2 updated - openmpt snapgene-viewer [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/b0192b9d46a8a30d68aa353de0102dcac8a54b3f

--- automatic/snapgene-viewer/tools/chocolateyinstall.ps1
+++ automatic/snapgene-viewer/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.snapgene.com/local/targets/viewer_download.php?majorRelease=3.2&minorRelease=3.2.1&os=windows'
+  url            = 'https://cdn.snapgene.com/downloads/SnapGeneViewer/5.x/5.1/5.1.7/snapgene_viewer_5.1.7_win.exe'
   softwareName   = 'SnapGene Viewer'
-  checksum       = '1b915a61b4c4c6a2da93d9f6dc34f0e62047e3cd52449d4639354d73b2098651847e1baf6cbff437cae406820104c6af115066691a5e2ed1e3134e3232c2acfd'
+  checksum       = 'ced76e6d74f6d6a6b56d01700d50d3af89c8d1bb16d37ba8d34e6bb7349e97fb2f0b3bdd657544bec220a9d092c3761916f16b035e7d34f29a36870e63b4b92c'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
