86233b4a50692571d44e37299d99cd6493243ff1
Chocolatey <chocolatey@realdimensions.net>
2020-10-14 01:32:55 +0000

    AU: 2 updated - cacher resxtranslator [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/a5b58035d2b6eadd5862fdcc127d8b601934c9e9

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.32.6.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.33.0.exe'
   softwareName   = 'Cacher*'
-  checksum       = '5ee609b4e1615b126f52b03a85dd8f82e28c9401addf0d2641bfd4a77d7e41c86346b6648a2f6e7056c892f99e7692fe77ddaf77cb15b71f1cd0826a687849dc'
+  checksum       = '07489bd0c855a77ff0ed49460246b52d63e67c14a535e59035efb701211c1578b85f439876d9aae9b50918934c9ceb91909fa079cb15afc64a1caf5f286bd833'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
