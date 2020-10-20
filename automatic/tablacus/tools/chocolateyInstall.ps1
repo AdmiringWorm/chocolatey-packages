4e7b274bd169b547769f8c9f0168117b8d1a656d
Chocolatey <chocolatey@realdimensions.net>
2020-10-20 13:33:16 +0000

    AU: 3 updated - nordlocker nordpass tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/9087434c7dfa2ad7d19d19fd2e27c9cc14642c7d

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te201009.zip"
+  file        = "$toolsPath\te201019.zip"
   destination = "$toolsPath"
 }
 
