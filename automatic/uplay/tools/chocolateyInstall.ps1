2d899068cf812b0b3803ffe7ba2922815a8e020d
Chocolatey <chocolatey@realdimensions.net>
2020-10-27 19:41:13 +0000

    AU: 2 updated - nordpass uplay [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/926ec2a71efa728d97afe058b598f73aa09a93b2

--- automatic/uplay/tools/chocolateyInstall.ps1
+++ automatic/uplay/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UplayInstaller.exe'
+  url            = 'https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UbisoftConnectInstaller.exe'
   softwareName   = 'Uplay'
-  checksum       = 'BF9047B3925BC270408990A5E899A1FED16EA0378B7319A1BDDFB526CC2212FBEC12F4549AAC3340AD4D35DCD9EAD6D1D23D07522940B2E109D7C220B110B97A'
+  checksum       = '8FB6754BC002A7EBF3CBD602AEEB80627130B6B0600549FBDB5961E3C4E23EFE68B09A53D925AAB55196528B295F8FDC220C3111911CFDC7FB90714356C7AF38'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
