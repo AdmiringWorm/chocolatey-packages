b8de93fcf6a9f35214a1519ee1b37aec9d77e26e
Chocolatey <chocolatey@realdimensions.net>
2020-10-30 01:40:59 +0000

    AU: 2 updated - reshade snapgene-viewer [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/4a7dde3c43fa92f58982177f8c28affc6c34add9

--- automatic/snapgene-viewer/tools/chocolateyinstall.ps1
+++ automatic/snapgene-viewer/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://cdn.snapgene.com/downloads/SnapGeneViewer/5.x/5.2/5.2.0/snapgene_viewer_5.2.0_win.exe'
+  url            = 'https://cdn.snapgene.com/downloads/SnapGeneViewer/5.x/5.2/5.2.1/snapgene_viewer_5.2.1_win.exe'
   softwareName   = 'SnapGene Viewer'
-  checksum       = '284975c9d7b6ebff4a67617eb4f8fb889dc8252f0831692d4567b3a377357be9ab4b6eb81d637da2a433ae7c95c84dedde02a1f5b68d1f822fa5120efd20166f'
+  checksum       = 'b3c85cf590a986119bd2a9c174e4e2471003733cd7ef034648db30538cacfa104763d100f16ae821096c5febebc5cc5304b6cb480779bd85712938e808e35ae7'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
