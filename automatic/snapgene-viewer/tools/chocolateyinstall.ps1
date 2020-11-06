4bff40d99fc625b431435beabc3fd850e0a9d399
Chocolatey <chocolatey@realdimensions.net>
2020-11-06 19:33:20 +0000

    AU: 2 updated - snapgene-viewer tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/608898fcac7afc82c179ad199219a4a6db342f17

--- automatic/snapgene-viewer/tools/chocolateyinstall.ps1
+++ automatic/snapgene-viewer/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://cdn.snapgene.com/downloads/SnapGeneViewer/5.x/5.2/5.2.1/snapgene_viewer_5.2.1_win.exe'
+  url            = 'https://cdn.snapgene.com/downloads/SnapGeneViewer/5.x/5.2/5.2.2/snapgene_viewer_5.2.2_win.exe'
   softwareName   = 'SnapGene Viewer'
-  checksum       = 'b3c85cf590a986119bd2a9c174e4e2471003733cd7ef034648db30538cacfa104763d100f16ae821096c5febebc5cc5304b6cb480779bd85712938e808e35ae7'
+  checksum       = '2dad2016a56c12ec77696055bb582f753ea20d688360e0e58ed7df4060878e67a0ff3c643d30379709b0ada1bdc0da2f7ceef3f76a35bf7a50a051a2889b449f'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
