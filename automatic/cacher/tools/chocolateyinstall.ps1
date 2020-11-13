f84a537a6f6c7a6398eda978f9959be493c1bb2e
Chocolatey <chocolatey@realdimensions.net>
2020-11-13 19:33:21 +0000

    AU: 2 updated - cacher tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/11eefb9bcbcd3c48cf0180900fda053e943fb3a1

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.33.2.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.33.4.exe'
   softwareName   = 'Cacher*'
-  checksum       = '4e1267ec63ee54678bfe80e97027ff47814f38ca281884cdaf4d3ee7c7cef17975934a0e2354907dde2951a28a75e5ad4cfe0e8d0a035bc37c3f243897059916'
+  checksum       = '01a9aa58e81fa908807619baa8ba32e48cb5b45bc424abfb7f279f10034cf3ac758e289ecf65e2966fc0b08883e5fd9ad87386a4ed4b75b27e7f6c621b0dd7b5'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te201111.zip"
+  file        = "$toolsPath\te201112.zip"
   destination = "$toolsPath"
 }
 
