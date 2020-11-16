7bc60a77ef9c5594c02daa358e0cf356141ad176
Chocolatey <chocolatey@realdimensions.net>
2020-11-16 13:33:19 +0000

    AU: 3 updated - nordpass tablacus uplay [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/49aebea922baa200922c4415142f8f29cbd704c6

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te201114.zip"
+  file        = "$toolsPath\te201115.zip"
   destination = "$toolsPath"
 }
 
