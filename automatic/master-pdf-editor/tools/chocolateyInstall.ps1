b940b092377cbb61ab4558173326459237728e52
Chocolatey <chocolatey@realdimensions.net>
2020-11-24 13:33:14 +0000

    AU: 2 updated - master-pdf-editor nordpass [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/143533649b43ea6bb5abf3a55a7d720db128d7a1

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = '01532A949DE903D943871FC7D4C78470D7622814B549BF39FCF96DA75259E351F0CBDC55E4331EFAA66E7ED9CF2A2258B650B093A19DBB45CBE595C7F25725A6'
+  checksum       = '9916CCFE64827AD30D8CB6EA1B5044A53B3C5544A246C0BAA6B153A8473A72141064927A0CC3DF7ED6701BAE3743708287543A375600FED120C8824FB809105F'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
