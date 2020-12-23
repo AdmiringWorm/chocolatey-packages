9231944822f17a46fd757714652cd5414dea9e54
Chocolatey <chocolatey@realdimensions.net>
2020-12-23 19:33:00 +0000

    AU: 3 updated - Cmder cmdermini tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/2ac0c591ceb2664e44441be6b3a2657d765d4504

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te201221.zip"
+  file        = "$toolsPath\te201222.zip"
   destination = "$toolsPath"
 }
 
