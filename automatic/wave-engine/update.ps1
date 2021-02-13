63256b4b11692a6549237894a613b6fa32c96c1e
AdmiringWorm <kim.nordmo@gmail.com>
2021-02-13 22:02:55 +0100

    (wave-engine) Updated regex for getting urls

--- automatic/wave-engine/update.ps1
+++ automatic/wave-engine/update.ps1
@@ -20,7 +20,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re = "\.exe$"
+  $re = "[\d_]+\.exe$"
   $url = $download_page.Links | ? href -match $re | select -first 1 -expand href
 
   $version = $url -split 'Setup_|\.exe$' | select -last 1 -skip 1 | % { $_ -replace '_','.' }
