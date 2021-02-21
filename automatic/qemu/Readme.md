ff84ab66c8cb3f6dbfb899e77e8cc331a9e67e72
AdmiringWorm <kim.nordmo@gmail.com>
2021-02-21 21:21:22 +0100

    (qemu) Change to allow installer versions to mismatch
    changelog:

--- automatic/qemu/Readme.md
+++ automatic/qemu/Readme.md
@@ -1,3 +1,9 @@
 # [<img src="https://cdn.jsdelivr.net/gh/AdmiringWorm/chocolatey-packages@3b29b52bbae47f2ef24f6b07947c3d80f6d6309c/automatic/qemu/icons/48x48.png" height="48" width="48" /> ![Qemu](https://img.shields.io/chocolatey/v/qemu.svg?label=Qemu&style=for-the-badge)](https://chocolatey.org/packages/qemu)
 
 QEMU is a generic and open source machine emulator and virtualizer.When used as a machine emulator, QEMU can run OSes and programs made for one machine (e.g. an ARM board) on a different machine (e.g. your own PC). By using dynamic translation, it achieves very good performance.When used as a virtualizer, QEMU achieves near native performances by executing the guest code directly on the host CPU. QEMU supports virtualization when executing under the Xen hypervisor or using the KVM kernel module in Linux. When using KVM, QEMU can virtualize x86, server and embedded PowerPC, and S390 guests.
+
+## NOTES
+
+- Sometimes an update is provided by the developers for one of the architecture, but the other is left as is.
+  This means some times either one of the installers to match the package version due to not needing to be updated.
+  This usually happens for the 32bit installer.
--- automatic/qemu/update.ps1
+++ automatic/qemu/update.ps1
@@ -11,16 +11,16 @@ function global:au_BeforeUpdate {
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
-      "(?i)^(\s*url\s*=\s*)'.*'" = "`${1}'$($Latest.URL32)'"
-      "(?i)^(\s*url64(bit)?\s*=\s*)'.*'" = "`${1}'$($Latest.URL64)'"
-      "(?i)^(\s*checksum\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum32)'"
-      "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
-      "(?i)^(\s*checksum64\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum64)'"
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"   = "`${1}'$softwareName'"
+      "(?i)^(\s*url\s*=\s*)'.*'"            = "`${1}'$($Latest.URL32)'"
+      "(?i)^(\s*url64(bit)?\s*=\s*)'.*'"    = "`${1}'$($Latest.URL64)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'"       = "`${1}'$($Latest.Checksum32)'"
+      "(?i)^(\s*checksumType\s*=\s*)'.*'"   = "`${1}'$($Latest.ChecksumType32)'"
+      "(?i)^(\s*checksum64\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum64)'"
       "(?i)^(\s*checksumType64\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType64)'"
-      "(?i)=\s*'.*'(\s*# fallback32)" = "= '$($Latest.FallBackUrl32)'`${1}"
-      "(?i)=\s*'.*'(\s*# fallback64)" = "= '$($Latest.FallBackUrl64)'`${1}"
+      "(?i)=\s*'.*'(\s*# fallback32)"       = "= '$($Latest.FallBackUrl32)'`${1}"
+      "(?i)=\s*'.*'(\s*# fallback64)"       = "= '$($Latest.FallBackUrl64)'`${1}"
     }
     ".\tools\chocolateyUninstall.ps1" = @{
       "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
@@ -34,13 +34,14 @@ function GetDataFrom($releasesUrl) {
   $download_page = Invoke-WebRequest -Uri $releasesUrl -UseBasicParsing
 
   $url = $download_page.Links | ? href -match "\.exe$" | select -last 1 -expand href | % { $releasesUrl + $_ }
-  $fallBackUrl = $url -replace "(w64|w32)\/","`$1/$(Get-Date -Format "yyyy")/"
+  $fallBackUrl = $url -replace "(w64|w32)\/", "`$1/$(Get-Date -Format "yyyy")/"
 
-  if($url -match "(\d{4})(\d{2})(\d{2})\.exe$") {
+  if ($url -match "(\d{4})(\d{2})(\d{2})\.exe$") {
     $version = "$($matches[1]).$($matches[2]).$($matches[3])"
 
     return @{URL = $url ; FallBackUrl = $fallBackUrl ; Version = $version }
-  } else {
+  }
+  else {
     throw "Unable to parse version information from '$releasesUrl'"
   }
 }
@@ -56,16 +57,13 @@ function global:au_GetLatest {
   $releasesUrl = $download_page.Links | ? href -match $re | select -first 1 -expand href
   $url64Data = GetDataFrom $releasesUrl
 
-  if ($url32Data.version -ne $url64Data.version) {
-    throw "32bit version do not match the 64bit version"
-  }
   @{
-    URL32 = $url32Data.URL
-    URL64 = $url64Data.URL
+    URL32         = $url32Data.URL
+    URL64         = $url64Data.URL
     FallBackUrl32 = $url32Data.FallBackUrl
     FallBackUrl64 = $url64Data.FallBackUrl
-    Version = $url32Data.version
-    PackageName = 'Qemu'
+    Version       = $url32Data.version
+    PackageName   = 'Qemu'
   }
 }
 
