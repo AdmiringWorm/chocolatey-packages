2904a1407175a57bdbbb51eaa9cbb969e33d73ac
AdmiringWorm <kim.nordmo@gmail.com>
2021-02-23 00:18:29 +0100

    (geoserver) Finished install and uninstall scripts

--- automatic/geoserver/tools/chocolateyinstall.ps1
+++ automatic/geoserver/tools/chocolateyinstall.ps1
@@ -1,13 +1,22 @@
-
+﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = Join-Path (Get-ToolsLocation) "GeoServer"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  file          = "$toolsDir\"
+  packageName  = $env:ChocolateyPackageName
+  destination  = $installDir
+  file         = "$toolsDir\"
 
-  softwareName  = 'geoserver*'
+  softwareName = 'geoserver*'
 }
 
 Get-ChocolateyUnzip @packageArgs
+
+
+@('startup'; 'shutdown') | % {
+  Install-BinFile "geoserver-$_" "$installDir\bin\$_.bat"
+}
+
+Write-Host "Adding environment variable for GeoServer install location"
+Install-ChocolateyEnvironmentVariable -VariableName "GEOSERVER_HOME" -VariableValue "$installDir" -VariableType 'Machine'
--- automatic/geoserver/tools/chocolateyuninstall.ps1
+++ automatic/geoserver/tools/chocolateyuninstall.ps1
@@ -1,37 +1,19 @@
-# TODO
-$ErrorActionPreference = 'Stop';
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  softwareName   = 'geoserver*'
-  fileType       = 'EXE_MSI_OR_MSU'
-  silentArgs     = "/qn /norestart"
-  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+﻿$ErrorActionPreference = 'Stop';
+
+Write-Host "Removing shim files"
+@('startup'; 'shutdown') | % {
+  Uninstall-BinFile "geoserver-$_"
 }
 
-$uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+$installPath = Join-Path (Get-ToolsLocation) "GeoServer"
 
-if ($key.Count -eq 1) {
-  $key | % {
-    $packageArgs['file'] = "$($_.UninstallString)"
-
-    if ($packageArgs['fileType'] -eq 'MSI') {
-      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
-
-      $packageArgs['file'] = ''
-    }
-    else {
-    }
-
-    Uninstall-ChocolateyPackage @packageArgs
-  }
+if (Test-Path $installPath) {
+  Write-Host "Removing installation directory..."
+  rm -Recurse -Force $installPath
 }
-elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
+else {
+  Write-Warning "Installation directory NOT found. Ignoring..."
 }
-elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % { Write-Warning "- $($_.DisplayName)" }
-}
+
+Write-Host "Removing environment variable for GeoServer install location"
+Uninstall-ChocolateyEnvironmentVariable -VariableName "GEOSERVER_HOME" -VariableType "Machine"
