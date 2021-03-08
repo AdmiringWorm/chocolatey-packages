d6ce3b98dfb23bfe91ba11d2593225eac5f46a3e
Chocolatey <chocolatey@realdimensions.net>
2021-03-08 19:41:03 +0000

    AU: 2 updated - master-pdf-editor tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/2033daeaa55444ee2e91d6a43be0914286f04fc2

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = 'B00B17B5AD7490F36DC3C510B2D3B0C629AAA14233EFAF134903E921BF2CC11908DBA8B7FE3441B36DC0BFF19BE1E1B69043A3D8711CBC9A0FA25330DD4CBA5A'
+  checksum       = '57D56E17AB7F64CFE5FE35BDEB7B352FBD5A4C15BB33A9610E68D6D92FC082E5F40E83271B6DDEE1E90F941DD65BD1F9B09C4399524192E1BC0AC544F45641B1'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te210306.zip"
+  file        = "$toolsPath\te210307.zip"
   destination = "$toolsPath"
 }
 
