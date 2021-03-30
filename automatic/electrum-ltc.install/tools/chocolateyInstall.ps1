fa56e90701db8ce3d7fcd780174c2cd4eeb328cf
Chocolatey <chocolatey@realdimensions.net>
2021-03-30 13:45:45 +0000

    AU: 3 updated - electrum-ltc electrum-ltc.install electrum-ltc.portable [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/6b6d36193bf4cc4faf7c9199a08a3834d6bb2322

--- automatic/electrum-ltc.install/tools/chocolateyInstall.ps1
+++ automatic/electrum-ltc.install/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName   = 'electrum-ltc.install'
   fileType      = 'exe'
   softwareName  = 'Electrum-LTC*'
-  file          = "$toolsDir\electrum-ltc-4.0.9.2-setup.exe"
+  file          = "$toolsDir\electrum-ltc-4.0.9.3-setup.exe"
   silentArgs    = "/S"
   validExitCodes= @(0)
 }
