e589c27d7bbb5f965d84bcccbd2a78bfb1606eb4
Chocolatey <chocolatey@realdimensions.net>
2021-05-26 20:12:38 +0000

    AU: 1 updated - origin [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/c6af803f8316ecd261114041159b1b5416d00bf2

--- automatic/origin/tools/chocolateyInstall.ps1
+++ automatic/origin/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   url          = 'https://download.dm.origin.com/origin/live/OriginSetup.exe'
   softwareName = 'Origin*' # Probably not needed, but we add registry keys so we keep it
-  checksum     = '50C826AAED05508E55102F02D505B00D5DB0FE21DA4BDFABE474E0B9FB884BFA144FE46086CB9235D831F82021388E20B8DE0F305FD9662208C6927007DFEAC1'
+  checksum     = '333863CB2B4425739CB7F40C1E985516CB2830C43D3B70E6A35170AE5295421844BE6CF5823B8C687030AC8F4631CFF5E065899CDC0A227C5BAD24BF1AF6B3AC'
   checksumType = 'sha512'
   destination  = Get-PackageCacheLocation
 }
@@ -16,7 +16,7 @@ if ($env:ChocolateyForce -ne $true) {
   try {
     $InstalledVersion = Get-UninstallRegistryKey $packageArgs['softwareName'] | Select-Object -First 1 -Expand 'DisplayVersion'
 
-    if ([Version]::Parse($InstalledVersion) -ge [Version]::Parse('10.5.96.47512'))
+    if ([Version]::Parse($InstalledVersion) -ge [Version]::Parse('10.5.100.48178'))
     {
       Write-Host "Skipping installation because version $InstalledVersion is already installed."
       return
@@ -35,10 +35,10 @@ $packageArgs['file'] = $zipFile
 $packageArgs['destination'] = GetInstallLocation $pp
 
 if ($pp.NoAutoUpdate) {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.96.47512'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $false -version '10.5.100.48178'
 }
 else {
-  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.96.47512'
+  CreateRegistrySettings -installPath $packageArgs['destination'] -autoUpdate $true -version '10.5.100.48178'
 }
 
 Get-ChocolateyUnzip @packageArgs
