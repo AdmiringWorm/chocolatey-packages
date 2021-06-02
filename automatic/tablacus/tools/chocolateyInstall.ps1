083962c842bf476cff98634ad005b1b9e5ca3fad
Chocolatey <chocolatey@realdimensions.net>
2021-06-02 14:56:08 +0000

    AU: 4 updated - ghostdoc-enterprise ghostdoc-pro nordpass tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/c48677f26d28e80290c212dd3f5130ffa4b42276

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te210529.zip"
+  file        = "$toolsPath\te210601.zip"
   destination = "$toolsPath"
 }
 
