b9dcb1f8feeff21d932e60a8307061647f91c800
Chocolatey <chocolatey@realdimensions.net>
2021-06-12 07:43:41 +0000

    AU: 1 updated - dumo [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/53bc28272ea059719ff8c9ddf7932dbc6e15abfa

--- automatic/dumo/tools/chocolateyInstall.ps1
+++ automatic/dumo/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://kcsoftwares.com/files/dumo_lite.exe'
   softwareName   = 'KC Softwares DUMo'
-  checksum       = 'A097EDD583F3D538000766A5BB86E5E93D8B51B35AADC0A9BF5C1C8F246EBE41AC9E3805E185F7C506F6D4581DED19FFD6B7D799A016B97B286DF6A364FA6757'
+  checksum       = '8BAC146CAA67BD0A70419B92EAEC895A6413E19E92AE24712B16024922957FF6AE7AA59485279EB9DEF958FBD3FD70A707B9B878936E5375CD1D2CDE4B7C0AE3'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
