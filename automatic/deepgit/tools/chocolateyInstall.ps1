7493940080342fd9a613a5b22a18bf7355872f35
Chocolatey <chocolatey@realdimensions.net>
2021-07-23 13:42:13 +0000

    AU: 1 updated - deepgit [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/26c241778843a4b2049d63f21424c3d525eaf322

--- automatic/deepgit/tools/chocolateyInstall.ps1
+++ automatic/deepgit/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.syntevo.com/downloads/deepgit/deepgit-win-setup-4_1_1.zip'
+  url            = 'https://www.syntevo.com/downloads/deepgit/deepgit-win-setup-4_2.zip'
   softwareName   = 'DeepGit'
-  checksum       = 'b6e9c3629091ecbb4d0f38d6ac70ec02334f2511bfb9fc6473f801c0ac05e15d677fceb662f1cfe2dac700f7ced5f70d22696b589e412ab55ebb4d152614ed39'
+  checksum       = '3766d662c2442c6be3995850f64118f7f655b8444fffc285e477498d8e2dff26fd4a9c71a906f14a54481ad51ed8d199fa498318293d32d6385f1dae34178ca7'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
