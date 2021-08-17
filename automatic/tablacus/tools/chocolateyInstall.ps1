3ce8e35bc861a28757a7dfe311c93c0f92a67611
Chocolatey <chocolatey@realdimensions.net>
2021-08-17 19:42:44 +0000

    AU: 2 updated - nordpass tablacus [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/47b5ff4d2eef4d1526a2a3a070bd2e5d0a9a91fd

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te210814.zip"
+  file        = "$toolsPath\te210816.zip"
   destination = "$toolsPath"
 }
 
