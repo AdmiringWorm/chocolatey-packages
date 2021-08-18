f651792311c534a562b5199e43f59e765d5ed3b4
Chocolatey <chocolatey@realdimensions.net>
2021-08-18 02:23:44 +0000

    AU: 1 updated - Qemu [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/1aeee6a5a571aef76bc94272f8d061ae8f8bb518

--- automatic/qemu/tools/chocolateyInstall.ps1
+++ automatic/qemu/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20210810.exe'
-  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20210810.exe'
+  url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20210817.exe'
+  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20210817.exe'
   softwareName   = 'QEMU'
-  checksum       = '98e9bd0aaba9871208c406949ad1c774a19e3ced3fb30a0897032369e5e3a40c690aa678dc52b1c5b260b5748e92755a6cfc2b7d50b5c3caec0733fafd2b0d50'
+  checksum       = '2b377911948ac64cef4715deca857c50051e5d1c6bf2ebc45e73405890cab74bcc5ff302ce74582fb75e05598016e59a633fc88c4c83ee66590cc5616650cd60'
   checksumType   = 'SHA512'
-  checksum64     = '1dbd0973190a6f3b8d61aee7bf20f07c4b713e3613fcb03bedcabf4a519a2e8a7843fff4007114430596ad016a0f8fc56d402d5ba587c62dcc1f4897995789ba'
+  checksum64     = 'db54619d642b06629939229338a838bf43ea19eda416267a1a2b062b9181b42f409e2b690073be2f36996c23eb6371f1f09f3014a4c8cf0a86b48397bc4455a8'
   checksumType64 = 'SHA512'
   silentArgs     = '/S'
   validExitCodes = @(0)
@@ -21,8 +21,8 @@ try {
 
   # We will just continue, as there is nothing to do when previous command succeed
 } catch {
-  $packageArgs['url'] = 'https://qemu.weilnetz.de/w32/2021/qemu-w32-setup-20210810.exe' # fallback32
-  $packageArgs['url64bit'] = 'https://qemu.weilnetz.de/w64/2021/qemu-w64-setup-20210810.exe' # fallback64
+  $packageArgs['url'] = 'https://qemu.weilnetz.de/w32/2021/qemu-w32-setup-20210817.exe' # fallback32
+  $packageArgs['url64bit'] = 'https://qemu.weilnetz.de/w64/2021/qemu-w64-setup-20210817.exe' # fallback64
 }
 
 Install-ChocolateyPackage @packageArgs
