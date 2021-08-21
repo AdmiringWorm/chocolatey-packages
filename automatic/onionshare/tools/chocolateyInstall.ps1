3e2092c81f51cff5964be965ff7412a504efba81
Chocolatey <chocolatey@realdimensions.net>
2021-08-21 02:14:59 +0000

    AU: 1 updated - onionshare [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/c0cbcee997e419ae295ff4ff90e897fc579490d4

--- automatic/onionshare/tools/chocolateyInstall.ps1
+++ automatic/onionshare/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ Report-LegacyInstalls
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file           = "$toolsPath\OnionShare-2.3.2.msi"
+  file           = "$toolsPath\OnionShare-2.3.3.msi"
   softwareName   = 'OnionShare'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1 MSIINSTALLPERUSER=0"
   validExitCodes = @(0)
