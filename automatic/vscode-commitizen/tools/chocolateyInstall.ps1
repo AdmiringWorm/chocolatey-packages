9936683de4312834790afa5eeac8246e6ad1ee5b
Chocolatey <chocolatey@realdimensions.net>
2021-08-30 07:42:35 +0000

    AU: 2 updated - powder-toy vscode-commitizen [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/d9b32e61a5147b877220c037a1fbe490b682fb7a

--- automatic/vscode-commitizen/tools/chocolateyInstall.ps1
+++ automatic/vscode-commitizen/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'KnisterPeter.vscode-commitizen@0.15.1'
+Install-VsCodeExtension -extensionId 'KnisterPeter.vscode-commitizen@1.0.1'
