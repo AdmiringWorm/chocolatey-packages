972a6305c9a3331a50dc3908fe3b2f4f3d61e4a8
Chocolatey <chocolatey@realdimensions.net>
2021-09-02 19:41:46 +0000

    AU: 2 updated - cacher nordpass [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/c0a87729bd2fe581a5a068a56a99064ba334dc8c

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.42.2.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.42.4.exe'
   softwareName   = 'Cacher*'
-  checksum       = '7b0799effd567dc12edacb622749425ba06c68dd5e8747e2e3a477d55b20777c9c4085cb73736263f483d0e66067dfc91e4bca70503a1d87a937c4f0d4acaf0e'
+  checksum       = 'd584d50b57cd473194f51336c9adaee340ea3dcd38a2f8035728bbb402b3b796277de72379d80a77a46e4dbbb4ecd282dc6fbc60ee19d964d603dc2ce50c9e22'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
