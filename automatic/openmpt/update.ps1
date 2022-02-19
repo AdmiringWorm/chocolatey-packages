7c76179324e0cb1916d0d32d1a068bbd3e4f8a98
AdmiringWorm <kim.nordmo@gmail.com>
2022-02-19 21:30:20 +0100

    (openmpt) Update to use new installer
    changelog: breaking enhancement

--- automatic/openmpt/update.ps1
+++ automatic/openmpt/update.ps1
@@ -30,16 +30,13 @@ function global:au_AfterUpdate {
 function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt"      = @{
-      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
+      "(?i)(^\s*location on\:?\s*)\<.*\>"       = "`${1}<$releases>"
+      "(?i)(\s*Download the following.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"           = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"             = "`${1} $($Latest.Checksum32)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
-      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
     }
   }
 }
@@ -50,18 +47,11 @@ function global:au_GetLatest {
   $re = 'Setup\.exe$'
   $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
 
-  $re = 'Setup-x64\.exe$'
-  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href
-
   $verRe = '[-]'
   [version]$version32 = $url32 -split "$verRe" | select -last 1 -skip 1
-  [version]$version64 = $url64 -split "$verRe" | select -last 1 -skip 2
-  if ($version32 -ne $version64) {
-    throw "32bit version do not match the 64bit version"
-  }
+
   @{
     URL32   = Get-RedirectedUrl $url32
-    URL64   = Get-RedirectedUrl $url64
     Version = $version32
   }
 }
