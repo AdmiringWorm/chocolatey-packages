079f6da6a9782cc6bb02069ef1b83e92de63b5a9
Chocolatey <chocolatey@realdimensions.net>
2022-03-01 19:38:10 +0000

    AU: 2 updated - authy-desktop FStar [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/39c59722f34c8c628b950ecc32794104c57fab3f

--- automatic/fstar/tools/chocolateyinstall.ps1
+++ automatic/fstar/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = ''
-  file64      = "$toolsPath\fstar_2021.09.04_Windows_x64.zip"
+  file64      = "$toolsPath\fstar_2022.03.01_Windows_x64.zip"
   destination = $toolsPath
 }
 
