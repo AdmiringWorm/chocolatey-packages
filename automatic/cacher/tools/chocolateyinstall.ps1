2e645f35db38e29c56fdff6441182d6e02b16e3e
Chocolatey <chocolatey@realdimensions.net>
2022-03-27 19:44:12 +0000

    AU: 2 updated - cacher IcoFX [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/367b63fe33ce7223b2f4596407ef39898971c221

--- automatic/cacher/tools/chocolateyinstall.ps1
+++ automatic/cacher/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.43.2.exe'
+  url            = 'https://s3.amazonaws.com/download.cacher.io/Cacher%20Setup%202.44.1.exe'
   softwareName   = 'Cacher*'
-  checksum       = '53028cf241218bf7db5c9386bac28f927168504d688099a09a6f3e69ecfe743c581aa4990c9912ee98ae760422d530315d099506deb55a071119617a1ad48abb'
+  checksum       = '4ea6d1b29672396d16af314422f3335ff53b3a6eaedd0afc986c09daa704bdf89ef442b702fd2fe146e80feef1dc47645f76452e74c8034dc6ec0b04fbf3f994'
   checksumType   = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/icofx/tools/ChocolateyInstall.ps1
+++ automatic/icofx/tools/ChocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.icofx.ro/files/icofxsetup.exe'
   softwareName   = 'IcoFX*'
-  checksum       = 'e33d57aadc0f88d36b5e3822192a18f0a6725af6d9dfe417205e8084eebd9d5b'
+  checksum       = '5bf43f1e815ca753a7149e666b87abe901bf693a659a80c21b2ab1000fbf37bf'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
