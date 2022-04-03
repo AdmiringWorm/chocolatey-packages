3fd75a1750817a8c7d6473833276dce86f8fda8e
Chocolatey <chocolatey@realdimensions.net>
2022-04-03 07:36:18 +0000

    AU: 1 updated - FStar [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/39b67be2b323309d22f482ce194dbda15c525c58

--- automatic/fstar/tools/chocolateyinstall.ps1
+++ automatic/fstar/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = ''
-  file64      = "$toolsPath\fstar_2022.03.24_Windows_x64.zip"
+  file64      = "$toolsPath\fstar_2022.04.02_Windows_x64.zip"
   destination = $toolsPath
 }
 
