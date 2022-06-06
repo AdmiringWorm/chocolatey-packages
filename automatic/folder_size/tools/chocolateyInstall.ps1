e326eda236b91647bc3c436d6d1dba604f416f8b
Chocolatey <chocolatey@realdimensions.net>
2022-06-06 19:44:16 +0000

    AU: 2 updated - folder_size nordvpn-teams [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/2e75199d6531db22ecd15048f2bbc9af4e23cd29

--- automatic/folder_size/tools/chocolateyInstall.ps1
+++ automatic/folder_size/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.mindgems.com/software/FolderSize.exe'
   softwareName   = 'Folder Size*'
-  checksum       = '6A570D74887B32E81C17D921D8A68508633381158BF59A0618656B78B485ECFD363797B94362D0A69383D8DFEDFB0855FBDCEEE20F134F4EEFDF42C30FC4AA30'
+  checksum       = '0033D66BB58CD5B3EAEE0260D8B111B3C12D64298E4C4EE9C879F34BFB42BA63B9783C8338A11FAD1BBCCFD38FEC14987D8383BBBF0FAD4E6AD35B1EF0C3BA8C'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
