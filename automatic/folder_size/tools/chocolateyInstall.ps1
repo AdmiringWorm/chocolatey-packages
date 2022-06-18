a87ccf698e21a8b022fb556a230bb353f4d2a10b
Chocolatey <chocolatey@realdimensions.net>
2022-06-18 13:49:14 +0000

    AU: 1 updated - folder_size [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/780a20f059f30f748407f28bd7d4ff73470691b6

--- automatic/folder_size/tools/chocolateyInstall.ps1
+++ automatic/folder_size/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.mindgems.com/software/FolderSize.exe'
   softwareName   = 'Folder Size*'
-  checksum       = '375DA9BCDC8B428A89B0A5A9D112CA50D50A64F82AA4B2E14B14795F852488657F1642669BCA25CA02619AE5C2830301E1A638BEDC3CAD77296945501304343D'
+  checksum       = '7DD141AF8298BA1357316E78AC8A7AED6BD25A0193771EC9ED6CFA0CA562C1136800207D3518F023D48A98783C12E08D0AC6FB6D4CBBAAEFE7FB4BB14968B9C5'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
