e25f7ae74735f6ac996c01aa5f61bf655aac1160
Chocolatey <chocolatey@realdimensions.net>
2022-07-04 13:55:35 +0000

    AU: 2 updated - master-pdf-editor nordvpn-teams [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/962110d6dcc71881509aa174e8dd638e56cf743e

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = 'F87AAF868721F837AC09CBFDFEFE23CA65C63A94B727512621D1B19B16D61F891C8B99E8539740F721A88F6CB41CE3F6ED5FE913F4C7330B8800F029E219CE48'
+  checksum       = '44FAAA5EAE7A54F6699138B7CE42D31EB52BBC9B69ECD35DFC41A6148CD91081E3F91A5A0F6F26FB9D03F09552FF086608E2E595795BE6CE43FB88E11CA930A4'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
