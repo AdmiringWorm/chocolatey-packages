2809466ccb53d003e5c568b8f1003cafb39770b3
Chocolatey <chocolatey@realdimensions.net>
2022-09-07 13:52:12 +0000

    AU: 2 updated - tablacus ubisoft-connect [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/4ef7408958ee0130d156596d8d573a9ac21577b0

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te220627.zip"
+  file        = "$toolsPath\te220905.zip"
   destination = "$toolsPath"
 }
 
