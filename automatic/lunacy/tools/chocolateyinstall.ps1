0c1174a9f9238f14e167129d9c290661a82dd032
Chocolatey <chocolatey@realdimensions.net>
2022-10-03 13:56:49 +0000

    AU: 1 updated - lunacy [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/23006296fd403f7699cc5fac8861188b4eee3f98

--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://lcdn.icons8.com/setup/LunacySetup_8.7.0.exe'
+  url            = 'https://lcdn.icons8.com/setup/LunacySetup_8.7.2.exe'
   softwareName   = 'Lunacy'
-  checksum       = 'af2a0349993f28362a0b675cdc0a96fee2c2af44b8a989ae5084499586f31d7dc8dbb036e661e48a08709b720da50ba841ae924eb721a0c2e04487ae2c9b7290'
+  checksum       = '232cb02799f9d487a47a5b04eeef95257ee5478005afdba96ca8ae7047d3fba3cbe51bc27f33fc3c16157b145eb6fc115236b3d6ecbc8067ba47c7d97f84eed5'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
