c76072994ccbf4a54b4fcacfdaa9d5bba5eb9d5d
Chocolatey <chocolatey@realdimensions.net>
2022-10-21 19:40:44 +0000

    AU: 1 updated - IcoFX [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/9e6d4caf99ad1d47618b0983563955f48f7e7686

--- automatic/icofx/tools/ChocolateyInstall.ps1
+++ automatic/icofx/tools/ChocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.icofx.ro/files/icofxsetup.exe'
   softwareName   = 'IcoFX*'
-  checksum       = '75eba4046fa79ec2ca45f01cb744f7f23cf5b42dbf7f651a2e4dcfe1e4545039'
+  checksum       = '2cc34d11b02a7dc5fe1edb96a95234bfca1c3bf583f657d64d38b14299ebb501'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
