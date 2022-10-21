3d279a6b02376f66051a15a2313dd69d72b8a796
Chocolatey <chocolatey@realdimensions.net>
2022-10-21 13:52:47 +0000

    AU: 1 updated - qtcreator-cdbext [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/ab65c98a896c9a4c1ce3b00b6a070bc91d91fa11

--- automatic/qtcreator-cdbext/tools/chocolateyinstall.ps1
+++ automatic/qtcreator-cdbext/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $installToolsPath = Get-ToolsLocation
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://master.qt.io/official_releases/qtcreator/8.0/8.0.0/installer_source/windows_x86/qtcreatorcdbext.7z'
-  url64bit       = 'https://master.qt.io/official_releases/qtcreator/8.0/8.0.0/installer_source/windows_x64/qtcreatorcdbext.7z'
+  url            = 'https://master.qt.io/official_releases/qtcreator/8.0/8.0.2/installer_source/windows_x86/qtcreatorcdbext.7z'
+  url64bit       = 'https://master.qt.io/official_releases/qtcreator/8.0/8.0.2/installer_source/windows_x64/qtcreatorcdbext.7z'
   destination    = "$installToolsPath\qtcreator"
-  checksum       = 'd48cdc162c17798ae9829f292b7bfd3c6ccb81c2153ee390d2d2b681744591f4'
+  checksum       = 'bddb808500d2ec0a20ed582028d7bb63ff5f2e6be8b280b30dc9e98d33a969d6'
   checksumType   = 'sha256'
-  checksum64     = '6c9ef1251cda70e20214f83f3565114e67a7af183d3b03961eefdfa4b7c79622'
+  checksum64     = 'e29a1e5c451396e7b4b908ed375ad4beb04359d89579f5f4ca417210f2c7c8e8'
   checksumType64 = 'sha256'
 
 }
