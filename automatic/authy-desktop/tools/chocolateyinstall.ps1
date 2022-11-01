0cb986dd437934e462ecc9ff6512fee369cd875d
shawnfunke <null@shawnfunke.dev>
2022-11-01 07:51:01 +0100

    [authy-desktop] advertise product to current user
    This is to ensure that installations using the Chocolatey background
    service are advertised to the current user as well, the application
    is installed for the user only

--- automatic/authy-desktop/tools/chocolateyinstall.ps1
+++ automatic/authy-desktop/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   checksumType   = ''
   checksum64     = '23b2505a2edd4d702b46307c3eadffa3105838a6c259cc66d52935c07f311c308f70218d7e061d08b331d0d7aeed7d9ee3b847ec70475d4d1b849c93f259f4f5'
   checksumType64 = 'sha512'
-  silentArgs     = '-s'
+  silentArgs     = '-s -ju'
   validExitCodes = @(0)
 }
 
