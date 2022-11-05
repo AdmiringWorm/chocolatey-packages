8451cd1ab5c4c12ff625cd82dffe7003563bd872
AdmiringWorm <kim.nordmo@gmail.com>
2022-11-05 18:12:04 +0100

    (spotlight-desktop) Fix updater to use GH API

--- automatic/spotlight-desktop/update.ps1
+++ automatic/spotlight-desktop/update.ps1
@@ -1,13 +1,11 @@
 ﻿Import-Module AU
 
-$releases = 'https://github.com/onlineth/Spotlight-Desktop/releases/latest'
-
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt"      = @{
-      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUrl)>"
       "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
       "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
       "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
@@ -29,17 +27,18 @@ function global:au_AfterUpdate($Package) {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $release = Get-LatestGithubReleases -repoUser 'onlineth' -repoName 'Spotlight-Desktop' | % latest
 
   $re = '\.msi$'
-  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
+  $url32 = $release.Assets | ? { $_ -match $re }
 
   $verRe = '\/'
   $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
   @{
     URL32        = $url32
-    Version      = $version32
-    ReleaseNotes = Get-RedirectedUrl -url $releases
+    Version      = Get-Version $release.Version
+    ReleaseNotes = $release.ReleaseUrl
+    ReleaseUrl   = $release.ReleaseUrl
   }
 }
 
