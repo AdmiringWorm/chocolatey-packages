146f51a95af572d5c56283cd8563ed17fb40345b
Chocolatey <chocolatey@realdimensions.net>
2022-11-09 13:50:39 +0000

    AU: 3 updated - master-pdf-editor nordvpn-teams ubisoft-connect [skip ci] https://gist.github.com/747b3ede98c9404e5cb6a399595e7ad1/23faa69022cb856788f6daebfc01e4ed8b9aa7fd

--- automatic/master-pdf-editor/tools/chocolateyInstall.ps1
+++ automatic/master-pdf-editor/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://code-industry.net/public/MasterPDFEditor-setup.exe'
   softwareName   = 'Master PDF Editor*'
-  checksum       = 'FF17B9C4C71F9439D3050772ECA32399BEE2A065248B7A2D5DE8D435E3CB67E6533697B360E34AB991B504947382B9E66D2F9CFAF2C0C2364013E649C1CF2A04'
+  checksum       = '6898A33187501DFEAC4AD4B96EB12B6BAA326B8AE7468E918153AF3D67E69110FB691DF5F404B4706958FD6C67228F25F0C70DD920A59EED4BC0FD376CC6A6A0'
   checksumType   = 'sha512'
   silentArgs     = "/SILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
