3bd0c6bfc39230ddd6d52a5b388b385b9c3fdc33
Chocolatey <chocolatey@realdimensions.net>
2023-06-09 07:39:06 +0000

    AU: 1 updated - lunacy [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/65543715dce31fb90b12b096634d0128e5669f9c

--- automatic/lunacy/tools/chocolateyinstall.ps1
+++ automatic/lunacy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://lcdn.icons8.com/setup/LunacySetup_9.1.0.exe'
+  url            = 'https://lcdn.icons8.com/setup/LunacySetup_9.1.1.exe'
   softwareName   = 'Lunacy'
-  checksum       = '316cdf0a77ac7135073f755e441800a44068144d11c500181e3702323ed8e325ca1d0a32becb9c8b505d60999bd934ec71c49abbb70f368f37d1ceddc498b47f'
+  checksum       = '1a6f00f4b28ea9dc5fa06388f806c3623073dd3d5eac887a667b6b7697ab2de64eb35a39ffb07b894aacf08b529fe5865b2c1931694d6171b9bdcbb1feb8e572'
   checksumType   = 'sha512'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
