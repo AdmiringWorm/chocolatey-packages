3ed7523d6629ff40b8ae2fbd04ddca82b0443447
Chocolatey <chocolatey@realdimensions.net>
2023-09-05 01:50:00 +0000

    AU: 1 updated - cmail [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/21df68d390acdce7780c41c4629d733ed0e445da

--- automatic/cmail/tools/chocolateyinstall.ps1
+++ automatic/cmail/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   fileType    = 'zip'
-  file        = "$toolsPath\CMail_0.8.8_x86.zip"
-  file64      = "$toolsPath\CMail_0.8.8_amd64.zip"
+  file        = "$toolsPath\CMail_0.8.9_x86.zip"
+  file64      = "$toolsPath\CMail_0.8.9_amd64.zip"
   destination = "$toolsPath"
 }
 
