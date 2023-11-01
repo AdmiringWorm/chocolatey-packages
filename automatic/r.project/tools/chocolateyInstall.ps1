21fbd20bdea9833ae75ad58155f129c664831d7c
Chocolatey <chocolatey@realdimensions.net>
2023-11-01 01:51:52 +0000

    AU: 2 updated - r r.project [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/e75e777811f3df040eb2688b0e26539d1756799e

--- automatic/r.project/tools/chocolateyInstall.ps1
+++ automatic/r.project/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName   = 'r.project'
   fileType      = 'exe'
   softwareName  = 'R for Windows*'
-  file          = "$toolsDir\R-4.3.1-win.exe"
+  file          = "$toolsDir\R-4.3.2-win.exe"
   silentArgs    = "/SILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0)
 }
