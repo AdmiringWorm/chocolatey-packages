ec8c0099b54397a0454d88a957612b32d368f3d3
Chocolatey <chocolatey@realdimensions.net>
2023-12-08 13:44:26 +0000

    AU: 2 updated - Qemu RssGuard [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/e875fea9c0cab4e21e4b06bfe298ca72275c0c7a

--- automatic/qemu/tools/chocolateyInstall.ps1
+++ automatic/qemu/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20221230.exe'
-  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20231207.exe'
+  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20231208.exe'
   softwareName   = 'QEMU'
   checksum       = 'e8380fc1f44264a25a6591cee881842220106c7ceee9087dd229dc56fed1415e79afebfdbf243c71853c4b2a946d2591707f06bed429eda21e31a4d9ed7b62ab'
   checksumType   = 'SHA512'
-  checksum64     = '83013fcb697de8e48a05aa60c1afc38e291bb675b74377d0a609e5c3d0cf7cc2c3435e06a513fd70141535bf8164f1b7c7e358c2df150d869c152c25606ef451'
+  checksum64     = '31d6ecaed9c057013d4ad7870a7177939631232ab83867c7bfa5d9e4a1eff9a0e6232fae30d60f5b7b2dcf18ae294d4c6e90a451df96a33489931f7c48015081'
   checksumType64 = 'SHA512'
   silentArgs     = '/S'
   validExitCodes = @(0)
@@ -22,7 +22,7 @@ try {
   # We will just continue, as there is nothing to do when previous command succeed
 } catch {
   $packageArgs['url'] = 'https://qemu.weilnetz.de/w32/2023/qemu-w32-setup-20221230.exe' # fallback32
-  $packageArgs['url64bit'] = 'https://qemu.weilnetz.de/w64/2023/qemu-w64-setup-20231207.exe' # fallback64
+  $packageArgs['url64bit'] = 'https://qemu.weilnetz.de/w64/2023/qemu-w64-setup-20231208.exe' # fallback64
 }
 
 Install-ChocolateyPackage @packageArgs
--- automatic/rssguard/tools/chocolateyinstall.ps1
+++ automatic/rssguard/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = ''
-  file64      = "$toolsPath\rssguard-4.6.0-64b739e85-lite-win7.7z"
+  file64      = "$toolsPath\rssguard-4.6.2-c1ada2fb2-lite-win7.7z"
   destination = $toolsPath
 }
 
