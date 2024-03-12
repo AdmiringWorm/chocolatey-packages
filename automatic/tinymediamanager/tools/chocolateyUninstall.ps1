02db7d157f86271500fa8c555367bb2a6ccd38ed
AdmiringWorm <kim.nordmo@gmail.com>
2024-03-12 20:29:36 +0100

    (tinymediamanager) Initial import of package

---
+++ automatic/tinymediamanager/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'tinymediamanager'
+$shortcutName = 'tinyMediaManager.lnk'
+
+Remove-Item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item -r "C:\Tools\tinyMediaManager*" -ea 0 -Force
