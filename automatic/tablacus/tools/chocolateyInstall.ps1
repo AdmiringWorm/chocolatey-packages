0865867289407b44c28ea1f72b8f11a23f8ea863
Chocolatey <chocolatey@realdimensions.net>
2024-06-16 19:37:01 +0000

    AU: 1 updated - tablacus [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/9f472c5f164acf377c392211eb277fde6e1f6082

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te240613.zip"
+  file        = "$toolsPath\te240616.zip"
   destination = "$toolsPath"
 }
 
