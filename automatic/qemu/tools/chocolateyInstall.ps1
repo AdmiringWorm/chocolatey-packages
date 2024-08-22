93eb1c0f17e62b68165eda683b524056baa18202
Chocolatey <chocolatey@realdimensions.net>
2024-08-22 13:43:58 +0000

    AU: 1 updated - Qemu [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/99752a17ba288608259d0bced4ac13bd336cb593

--- automatic/qemu/tools/chocolateyInstall.ps1
+++ automatic/qemu/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   url            = 'https://qemu.weilnetz.de/w32/qemu-w32-setup-20221230.exe'
-  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20240815.exe'
+  url64bit       = 'https://qemu.weilnetz.de/w64/qemu-w64-setup-20240822.exe'
   softwareName   = 'QEMU'
   checksum       = 'e8380fc1f44264a25a6591cee881842220106c7ceee9087dd229dc56fed1415e79afebfdbf243c71853c4b2a946d2591707f06bed429eda21e31a4d9ed7b62ab'
   checksumType   = 'SHA512'
-  checksum64     = '80cad002386fa92e03168e16d39d1946d41b1ddf90fd1e060880daf7b701f133788963696f4b84f6e0a68f5c70785ccf3fdd366c2e69181b0d82e7f89c2ad736'
+  checksum64     = '7581f7311390507a59ec72d9098bca36e02dc799fb368d31fdd357ac758a47bf0d54e9d8673087f26a238dfc54f207ee00f796c3d4f91b585e59bf8013de8ac4'
   checksumType64 = 'SHA512'
   silentArgs     = '/S'
   validExitCodes = @(0)
@@ -22,7 +22,7 @@ try {
   # We will just continue, as there is nothing to do when previous command succeed
 } catch {
   $packageArgs['url'] = 'https://qemu.weilnetz.de/w32/2024/qemu-w32-setup-20221230.exe' # fallback32
-  $packageArgs['url64bit'] = 'https://qemu.weilnetz.de/w64/2024/qemu-w64-setup-20240815.exe' # fallback64
+  $packageArgs['url64bit'] = 'https://qemu.weilnetz.de/w64/2024/qemu-w64-setup-20240822.exe' # fallback64
 }
 
 Install-ChocolateyPackage @packageArgs
