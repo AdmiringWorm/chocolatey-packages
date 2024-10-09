752ce8935ab6bae879ae732673e78679e6591618
Chocolatey <chocolatey@realdimensions.net>
2024-10-09 13:42:19 +0000

    AU: 2 updated - io-ninja tablacus [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/81b9b8eb240824a45ea21971ce2bd50a63105819

--- automatic/io-ninja/tools/chocolateyinstall.ps1
+++ automatic/io-ninja/tools/chocolateyinstall.ps1
@@ -8,12 +8,12 @@ Import-Certificate -FilePath "$toolsPath\io-ninja.cer" -CertStoreLocation "Cert:
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.0/ioninja-5.7.0-windows-x86.msi'
-  url64bit       = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.0/ioninja-5.7.0-windows-amd64.msi'
+  url            = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.1/ioninja-5.7.1-windows-x86.msi'
+  url64bit       = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.1/ioninja-5.7.1-windows-amd64.msi'
   softwareName   = 'IO Ninja*'
-  checksum       = '5314ab53f1f966311a76d414e875b05e425db905b3e624739ed1b8197200e3ecac85d2fc389c7c3e179af44ec803e4551eb5a8e35026fd161ffea0a7cc94b344'
+  checksum       = '8db00742d4474af46bb09d3a42c882c3a94f428a177088e9b2c5a3f5f606df893c37b9379f220d33b3a99353fb3b189905fdbcfb402fb1f15c306350a7e47b83'
   checksumType   = 'sha512'
-  checksum64     = '76b620b2c99df783034846a40e5e954b8e1fe4eea61188664cbaa09956b8e06d9f926db623ee68c5f9df77cc7646bff641e72f9204a920a82994700cba17ebc1'
+  checksum64     = '8495d5679c7af74c66d9b0d60efb9fc54d1abd8bf98bdb232ecd69f0d559ae1dfa4543f9d3fd009fc5094d3d7412f086be4415029f1799f627c123f4ea88f46d'
   checksumType64 = 'sha512'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = 'tablacus'
-  file        = "$toolsPath\te241004.zip"
+  file        = "$toolsPath\te241009.zip"
   destination = "$toolsPath"
 }
 
