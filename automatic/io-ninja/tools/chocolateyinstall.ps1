8c18263e9018ffa43f436ac3f1a0c3ec417516e4
Chocolatey <chocolatey@realdimensions.net>
2024-12-31 13:39:17 +0000

    AU: 1 updated - io-ninja [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/054adeca9feacf2bef95433ba873379efb8ecb51

--- automatic/io-ninja/tools/chocolateyinstall.ps1
+++ automatic/io-ninja/tools/chocolateyinstall.ps1
@@ -8,12 +8,12 @@ Import-Certificate -FilePath "$toolsPath\io-ninja.cer" -CertStoreLocation "Cert:
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.1/ioninja-5.7.1-windows-x86.msi'
-  url64bit       = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.1/ioninja-5.7.1-windows-amd64.msi'
+  url            = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.2/ioninja-5.7.2-windows-x86.msi'
+  url64bit       = 'https://tibbo.com/downloads/archive/ioninja/ioninja-5.7.2/ioninja-5.7.2-windows-amd64.msi'
   softwareName   = 'IO Ninja*'
-  checksum       = '8db00742d4474af46bb09d3a42c882c3a94f428a177088e9b2c5a3f5f606df893c37b9379f220d33b3a99353fb3b189905fdbcfb402fb1f15c306350a7e47b83'
+  checksum       = 'df4c9cc51275117900dd1f46f808543613e5ddcc3d2ae66c22b934ff50d83364a7a5fdd6bebee4dd6eee7b122fdf4118d855bf5bbf49b68c71d64bdbc4cc1986'
   checksumType   = 'sha512'
-  checksum64     = '8495d5679c7af74c66d9b0d60efb9fc54d1abd8bf98bdb232ecd69f0d559ae1dfa4543f9d3fd009fc5094d3d7412f086be4415029f1799f627c123f4ea88f46d'
+  checksum64     = 'c02a122f771abf6ee7464620647e0136bfc5f80bc1873b6f334bba48d0cf602eb9e8f4579f10bb17fcbed8782e8165768e09aebf1637e8d443daf6dd8763d47e'
   checksumType64 = 'sha512'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
