cded8480c6eb119b2ef655ed29c01ed888040530
Chocolatey <chocolatey@realdimensions.net>
2025-10-26 07:40:01 +0000

    AU: 1 updated - tinymediamanager [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/1364dad5b0a3e02138688acdc74cded328df2c26

--- automatic/tinymediamanager/tools/chocolateyInstall.ps1
+++ automatic/tinymediamanager/tools/chocolateyInstall.ps1
@@ -19,7 +19,7 @@ $destination = Get-ToolsLocation
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  file64         = "$toolsDir\tinyMediaManager-5.2.2-windows-amd64.zip"
+  file64         = "$toolsDir\tinyMediaManager-5.2.3-windows-amd64.zip"
   SpecificFolder = 'tinyMediaManager'
   Destination    = $destination
 }
