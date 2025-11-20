ea761378d03c1161b4e60101353c46371345caf5
Chocolatey <chocolatey@realdimensions.net>
2025-11-20 19:38:01 +0000

    AU: 1 updated - innosetup [skip ci] https://gist.github.com/AdmiringWorm/747b3ede98c9404e5cb6a399595e7ad1/72eabfbdf57441ceab39234b71e29452b2cfefa4

--- automatic/innosetup/tools/chocolateyInstall.ps1
+++ automatic/innosetup/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\innosetup-6.6.0.exe"
+$filePath = "$toolsDir\innosetup-6.6.1.exe"
 
 $packageArgs = @{
   packageName   = 'innosetup'
